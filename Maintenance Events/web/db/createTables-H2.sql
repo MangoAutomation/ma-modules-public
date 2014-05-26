8b54bf42293da762a4842c14fab69e75a3fab754
terrypacker <terry.packer@server.fake>
2014-05-25 15:02:06 -1000

    Adding support for H2

---
+++ Maintenance Events/web/db/createTables-H2.sql
@@ -0,0 +1,30 @@
+--
+--    Copyright (C) 2014 Infinite Automation Systems Inc. All rights reserved.
+--    @author Matthew Lohbihler
+--
+create table maintenanceEvents (
+  id int not null auto_increment,
+  xid varchar(50) not null,
+  dataSourceId int not null,
+  alias varchar(255),
+  alarmLevel int not null,
+  scheduleType int not null,
+  disabled char(1) not null,
+  activeYear int,
+  activeMonth int,
+  activeDay int,
+  activeHour int,
+  activeMinute int,
+  activeSecond int,
+  activeCron varchar(25),
+  inactiveYear int,
+  inactiveMonth int,
+  inactiveDay int,
+  inactiveHour int,
+  inactiveMinute int,
+  inactiveSecond int,
+  inactiveCron varchar(25),
+  primary key (id)
+);
+alter table maintenanceEvents add constraint maintenanceEventsUn1 unique (xid);
+alter table maintenanceEvents add constraint maintenanceEventsFk1 foreign key (dataSourceId) references dataSources(id) on delete cascade;
