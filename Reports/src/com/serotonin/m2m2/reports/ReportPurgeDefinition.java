709fc34967bc41eb2b20a0068ff882527b6716aa
terrypacker <terry.packer@server.fake>
2014-05-28 13:39:18 -1000

    Fixing testing code that was accidentally committed.

--- Reports/src/com/serotonin/m2m2/reports/ReportPurgeDefinition.java
+++ Reports/src/com/serotonin/m2m2/reports/ReportPurgeDefinition.java
@@ -12,7 +12,6 @@ import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.db.dao.SystemSettingsDao;
 import com.serotonin.m2m2.module.PurgeDefinition;
 import com.serotonin.m2m2.util.DateUtils;
-import com.serotonin.m2m2.web.dwr.SystemSettingsDwr;
 
 public class ReportPurgeDefinition extends PurgeDefinition {
     private static final Log LOG = LogFactory.getLog(ReportPurgeDefinition.class);
@@ -27,7 +26,7 @@ public class ReportPurgeDefinition extends PurgeDefinition {
                 SystemSettingsDao.getIntValue(REPORT_PURGE_PERIOD_TYPE, Common.TimePeriods.MONTHS),
                 SystemSettingsDao.getIntValue(REPORT_PURGE_PERIODS, 1));
 
-        int deleteCount = new ReportDao().purgeReportsBefore( System.currentTimeMillis()); //cutoff.getMillis());
+        int deleteCount = new ReportDao().purgeReportsBefore(cutoff.getMillis());
         if (deleteCount > 0)
             LOG.info("Report purge ended, " + deleteCount + " report instances deleted");
     }
