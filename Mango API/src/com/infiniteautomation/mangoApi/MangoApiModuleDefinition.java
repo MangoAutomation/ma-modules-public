04886a9330d7c62e31ac992f532e9b0ed0aaac4e
terrypacker <terry.packer@server.fake>
2014-08-28 14:24:43 -1000

    Initial Commit of Module

---
+++ Mango API/src/com/infiniteautomation/mangoApi/MangoApiModuleDefinition.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2014 Infinite Automation Software. All rights reserved.
+ * @author Terry Packer
+ */
+package com.infiniteautomation.mangoApi;
+
+import com.serotonin.m2m2.module.ModuleElementDefinition;
+
+/**
+ * @author Terry Packer
+ *
+ */
+public class MangoApiModuleDefinition extends ModuleElementDefinition{
+
+}
