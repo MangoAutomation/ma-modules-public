32787bf25c7154c0d4ef463697cec81bcdd652a4
Matthew Lohbihler <ml@serotoninsoftware.com>
2015-02-16 17:38:57 -0500

    Tag-based user permissions

--- Mango API/src-test/com/serotonin/m2m2/web/mvc/rest/test/data/UserTestData.java
+++ Mango API/src-test/com/serotonin/m2m2/web/mvc/rest/test/data/UserTestData.java
@@ -10,209 +10,149 @@ import java.util.List;
 
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.vo.User;
-import com.serotonin.m2m2.vo.permission.DataPointAccess;
 
 /**
  * @author Terry Packer
  *
  */
 public class UserTestData {
-	
-	//Freetext password for standard user
-	public static String standardPassword = "standard";
-	public static String adminPassword = "admin";
-	
-	
-	/**
-	 * Create a standard user that has previously loggged In
-	 * @return
-	 */
-	public static User standardUser(){
-		User user = new User();
-		user.setId(1);
-		user.setUsername("standard");
-		//Store encrypted passwords
-		user.setPassword(Common.encrypt(standardPassword));
-		user.setEmail("email@address.com");
-		user.setPhone("808-000-0000");
-		user.setAdmin(false);
-		user.setDisabled(false);
-		user.setHomeUrl("/home.shtm");
-		user.setLastLogin(new Date().getTime() - 10000);
-		user.setReceiveAlarmEmails(1);
-		user.setReceiveOwnAuditEvents(true);
-		user.setTimezone("UTC");
-		user.setMuted(false);
-		
-		//Setup Permissions
-		List<Integer> dataSourcePermissions = new ArrayList<Integer>();
-		dataSourcePermissions.add(1);
-		user.setDataSourcePermissions(dataSourcePermissions);
-		
-		List<DataPointAccess> dataPointPermissions = new ArrayList<DataPointAccess>();
-		DataPointAccess access1 = new DataPointAccess();
-		access1.setDataPointId(1);
-		access1.setPermission(DataPointAccess.READ);
-		dataPointPermissions.add(access1);
-		user.setDataPointPermissions(dataPointPermissions);
-		
-		return user;
-	}
 
-	/**
-	 * Create a standard user that has previously loggged In
-	 * @return
-	 */
-	public static User newStandardUser(){
-		User user = new User();
-		user.setId(2);
-		user.setUsername("newStandard");
-		//Store encrypted passwords
-		user.setPassword(Common.encrypt(standardPassword));
-		user.setEmail("email@address.com");
-		user.setPhone("808-000-0000");
-		user.setAdmin(false);
-		user.setDisabled(false);
-		user.setHomeUrl("/home.shtm");
-		user.setLastLogin(0); //Never logged in before
-		user.setReceiveAlarmEmails(1);
-		user.setReceiveOwnAuditEvents(true);
-		user.setTimezone("UTC");
-		user.setMuted(false);
-		
-		//Setup Permissions
-		List<Integer> dataSourcePermissions = new ArrayList<Integer>();
-		dataSourcePermissions.add(1);
-		user.setDataSourcePermissions(dataSourcePermissions);
-		
-		List<DataPointAccess> dataPointPermissions = new ArrayList<DataPointAccess>();
-		DataPointAccess access1 = new DataPointAccess();
-		access1.setDataPointId(1);
-		access1.setPermission(DataPointAccess.READ);
-		dataPointPermissions.add(access1);
-		user.setDataPointPermissions(dataPointPermissions);
-		
-		return user;
-	}
-	
-	/**
-	 * Create a standard user that has previously loggged In
-	 * @return
-	 */
-	public static User newDisabledAdminUser(){
-		User user = new User();
-		user.setId(3);
-		user.setUsername("newDisabledAdmin");
-		//Store encrypted passwords
-		user.setPassword(Common.encrypt(adminPassword));
-		user.setEmail("email@address.com");
-		user.setPhone("808-000-0000");
-		user.setAdmin(true);
-		user.setDisabled(true);
-		user.setHomeUrl("/home.shtm");
-		user.setLastLogin(0); //Never logged in before
-		user.setReceiveAlarmEmails(1);
-		user.setReceiveOwnAuditEvents(true);
-		user.setTimezone("UTC");
-		user.setMuted(false);
-		
-		//Setup Permissions
-		List<Integer> dataSourcePermissions = new ArrayList<Integer>();
-		dataSourcePermissions.add(1);
-		user.setDataSourcePermissions(dataSourcePermissions);
-		
-		List<DataPointAccess> dataPointPermissions = new ArrayList<DataPointAccess>();
-		DataPointAccess access1 = new DataPointAccess();
-		access1.setDataPointId(1);
-		access1.setPermission(DataPointAccess.READ);
-		dataPointPermissions.add(access1);
-		user.setDataPointPermissions(dataPointPermissions);
-		
-		return user;
-	}
-	
-	/**
-	 * Create a standard user that has previously loggged In
-	 * @return
-	 */
-	public static User newAdminUser(){
-		User user = new User();
-		user.setId(4);
-		user.setUsername("newAdmin");
-		//Store encrypted passwords
-		user.setPassword(Common.encrypt(adminPassword));
-		user.setEmail("email@address.com");
-		user.setPhone("808-000-0000");
-		user.setAdmin(true);
-		user.setDisabled(false);
-		user.setHomeUrl("/home.shtm");
-		user.setLastLogin(0); //Never logged in before
-		user.setReceiveAlarmEmails(1);
-		user.setReceiveOwnAuditEvents(true);
-		user.setTimezone("UTC");
-		user.setMuted(false);
-		
-		//Setup Permissions
-		List<Integer> dataSourcePermissions = new ArrayList<Integer>();
-		dataSourcePermissions.add(1);
-		user.setDataSourcePermissions(dataSourcePermissions);
-		
-		List<DataPointAccess> dataPointPermissions = new ArrayList<DataPointAccess>();
-		DataPointAccess access1 = new DataPointAccess();
-		access1.setDataPointId(1);
-		access1.setPermission(DataPointAccess.READ);
-		dataPointPermissions.add(access1);
-		user.setDataPointPermissions(dataPointPermissions);
-		
-		return user;
-	}
-	
-	/**
-	 * Create a standard user that has previously loggged In
-	 * @return
-	 */
-	public static User adminUser(){
-		User user = new User();
-		user.setId(4);
-		user.setUsername("admin");
-		//Store encrypted passwords
-		user.setPassword(Common.encrypt(adminPassword));
-		user.setEmail("email@address.com");
-		user.setPhone("808-000-0000");
-		user.setAdmin(true);
-		user.setDisabled(false);
-		user.setHomeUrl("/home.shtm");
-		user.setLastLogin(new Date().getTime() - 1000);
-		user.setReceiveAlarmEmails(1);
-		user.setReceiveOwnAuditEvents(true);
-		user.setTimezone("UTC");
-		user.setMuted(false);
-		
-		//Setup Permissions
-		List<Integer> dataSourcePermissions = new ArrayList<Integer>();
-		dataSourcePermissions.add(1);
-		user.setDataSourcePermissions(dataSourcePermissions);
-		
-		List<DataPointAccess> dataPointPermissions = new ArrayList<DataPointAccess>();
-		DataPointAccess access1 = new DataPointAccess();
-		access1.setDataPointId(1);
-		access1.setPermission(DataPointAccess.READ);
-		dataPointPermissions.add(access1);
-		user.setDataPointPermissions(dataPointPermissions);
-		
-		return user;
-	}
-	
-	public static List<User> getAllUsers(){
-		List<User> all = new ArrayList<User>();
-		all.add(standardUser());
-		all.add(newStandardUser());
-		all.add(newDisabledAdminUser());
-		all.add(newAdminUser());
-		
-		return all;
-	}
-	
-	
-	
+    //Freetext password for standard user
+    public static String standardPassword = "standard";
+    public static String adminPassword = "admin";
+
+    /**
+     * Create a standard user that has previously loggged In
+     * 
+     * @return
+     */
+    public static User standardUser() {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("standard");
+        //Store encrypted passwords
+        user.setPassword(Common.encrypt(standardPassword));
+        user.setEmail("email@address.com");
+        user.setPhone("808-000-0000");
+        user.setAdmin(false);
+        user.setDisabled(false);
+        user.setHomeUrl("/home.shtm");
+        user.setLastLogin(new Date().getTime() - 10000);
+        user.setReceiveAlarmEmails(1);
+        user.setReceiveOwnAuditEvents(true);
+        user.setTimezone("UTC");
+        user.setMuted(false);
+        user.setPermissions("myGroup");
+        return user;
+    }
+
+    /**
+     * Create a standard user that has previously loggged In
+     * 
+     * @return
+     */
+    public static User newStandardUser() {
+        User user = new User();
+        user.setId(2);
+        user.setUsername("newStandard");
+        //Store encrypted passwords
+        user.setPassword(Common.encrypt(standardPassword));
+        user.setEmail("email@address.com");
+        user.setPhone("808-000-0000");
+        user.setAdmin(false);
+        user.setDisabled(false);
+        user.setHomeUrl("/home.shtm");
+        user.setLastLogin(0); //Never logged in before
+        user.setReceiveAlarmEmails(1);
+        user.setReceiveOwnAuditEvents(true);
+        user.setTimezone("UTC");
+        user.setMuted(false);
+        user.setPermissions("myGroup");
+        return user;
+    }
+
+    /**
+     * Create a standard user that has previously loggged In
+     * 
+     * @return
+     */
+    public static User newDisabledAdminUser() {
+        User user = new User();
+        user.setId(3);
+        user.setUsername("newDisabledAdmin");
+        //Store encrypted passwords
+        user.setPassword(Common.encrypt(adminPassword));
+        user.setEmail("email@address.com");
+        user.setPhone("808-000-0000");
+        user.setAdmin(true);
+        user.setDisabled(true);
+        user.setHomeUrl("/home.shtm");
+        user.setLastLogin(0); //Never logged in before
+        user.setReceiveAlarmEmails(1);
+        user.setReceiveOwnAuditEvents(true);
+        user.setTimezone("UTC");
+        user.setMuted(false);
+        user.setPermissions("myGroup");
+        return user;
+    }
+
+    /**
+     * Create a standard user that has previously loggged In
+     * 
+     * @return
+     */
+    public static User newAdminUser() {
+        User user = new User();
+        user.setId(4);
+        user.setUsername("newAdmin");
+        //Store encrypted passwords
+        user.setPassword(Common.encrypt(adminPassword));
+        user.setEmail("email@address.com");
+        user.setPhone("808-000-0000");
+        user.setAdmin(true);
+        user.setDisabled(false);
+        user.setHomeUrl("/home.shtm");
+        user.setLastLogin(0); //Never logged in before
+        user.setReceiveAlarmEmails(1);
+        user.setReceiveOwnAuditEvents(true);
+        user.setTimezone("UTC");
+        user.setMuted(false);
+        user.setPermissions("myGroup");
+        return user;
+    }
+
+    /**
+     * Create a standard user that has previously loggged In
+     * 
+     * @return
+     */
+    public static User adminUser() {
+        User user = new User();
+        user.setId(4);
+        user.setUsername("admin");
+        //Store encrypted passwords
+        user.setPassword(Common.encrypt(adminPassword));
+        user.setEmail("email@address.com");
+        user.setPhone("808-000-0000");
+        user.setAdmin(true);
+        user.setDisabled(false);
+        user.setHomeUrl("/home.shtm");
+        user.setLastLogin(new Date().getTime() - 1000);
+        user.setReceiveAlarmEmails(1);
+        user.setReceiveOwnAuditEvents(true);
+        user.setTimezone("UTC");
+        user.setMuted(false);
+        user.setPermissions("myGroup");
+        return user;
+    }
+
+    public static List<User> getAllUsers() {
+        List<User> all = new ArrayList<>();
+        all.add(standardUser());
+        all.add(newStandardUser());
+        all.add(newDisabledAdminUser());
+        all.add(newAdminUser());
+
+        return all;
+    }
 }
--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/UserModel.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/UserModel.java
@@ -4,8 +4,6 @@
  */
 package com.serotonin.m2m2.web.mvc.rest.v1.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TimeZone;
 
 import org.springframework.http.HttpStatus;
@@ -13,19 +11,12 @@ import org.springframework.http.HttpStatus;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonSetter;
-import com.serotonin.m2m2.db.dao.DaoRegistry;
 import com.serotonin.m2m2.i18n.ProcessResult;
 import com.serotonin.m2m2.i18n.TranslatableMessage;
-import com.serotonin.m2m2.vo.DataPointVO;
 import com.serotonin.m2m2.vo.User;
-import com.serotonin.m2m2.vo.dataSource.DataSourceVO;
-import com.serotonin.m2m2.vo.permission.DataPointAccess;
 import com.serotonin.m2m2.web.mvc.rest.v1.exception.RestValidationFailedException;
 import com.serotonin.m2m2.web.mvc.rest.v1.message.RestProcessResult;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.events.AlarmLevel;
-import com.serotonin.m2m2.web.mvc.rest.v1.model.permissions.DataPointPermissionModel;
-import com.serotonin.m2m2.web.mvc.rest.v1.model.permissions.PermissionEnum;
-import com.serotonin.m2m2.web.mvc.rest.v1.model.permissions.PermissionsModel;
 import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
 
@@ -33,180 +24,140 @@ import com.wordnik.swagger.annotations.ApiModelProperty;
  * @author Terry Packer
  * 
  */
-@ApiModel(value="User", description="User Data Model", parent=AbstractRestModel.class)
-@JsonPropertyOrder({"username", "email"})
+@ApiModel(value = "User", description = "User Data Model", parent = AbstractRestModel.class)
+@JsonPropertyOrder({ "username", "email" })
 public class UserModel extends AbstractRestModel<User> {
-	
-	public UserModel(){
-		super(new User());
-		this.data.setDataPointPermissions(new ArrayList<DataPointAccess>());
-		this.data.setDataSourcePermissions(new ArrayList<Integer>());
-	}
-	
-	public UserModel(User user) {
-		super(user);
-	}
 
-	@ApiModelProperty(value = "Username of User", required = true)
-	@JsonGetter("username")
-	public String getUsername() {
-		return data.getUsername();
-	}
+    public UserModel() {
+        super(new User());
+    }
 
-	@JsonSetter("username")
-	public void setUsername(String username) {
-		data.setUsername(username);
-	}
+    public UserModel(User user) {
+        super(user);
+    }
 
-	//TODO This is not working yet
-	// the idea is that the password will only be
-	// available in the Test View
-	@JsonGetter("password")
-	public String getPassword() {
-			return ""; //data.getPassword(); 
-	}
-	@JsonSetter("password")
-	public void setPassword(String password) {
-		data.setPassword(password);
-	}
+    @ApiModelProperty(value = "Username of User", required = true)
+    @JsonGetter("username")
+    public String getUsername() {
+        return data.getUsername();
+    }
 
-	@JsonGetter("email")
-	public String getEmail() {
-		return data.getEmail();
-	}
+    @JsonSetter("username")
+    public void setUsername(String username) {
+        data.setUsername(username);
+    }
 
-	@JsonSetter("email")
-	public void setEmail(String email) {
-		data.setEmail(email);
-	}
+    //TODO This is not working yet
+    // the idea is that the password will only be
+    // available in the Test View
+    @JsonGetter("password")
+    public String getPassword() {
+        return ""; //data.getPassword(); 
+    }
 
-	@JsonGetter("disabled")
-	public Boolean getDisabled() {
-		return data.isDisabled();
-	}
+    @JsonSetter("password")
+    public void setPassword(String password) {
+        data.setPassword(password);
+    }
 
-	@JsonSetter("disabled")
-	public void setDisabled(Boolean disabled) {
-		data.setDisabled(disabled);
-	}
-	
-	@JsonGetter("permissions")
-	public PermissionsModel getPermissions() {
-		
-		List<DataPointPermissionModel> dataPointPermissions = new ArrayList<DataPointPermissionModel>();
-		List<String> dataSourceXids = new ArrayList<String>();
-		
-		for(Integer dsId : data.getDataSourcePermissions()){
-			DataSourceVO<?> vo = DaoRegistry.dataSourceDao.get(dsId);
-			if(vo != null){
-				dataSourceXids.add(vo.getXid());
-			}
-		}
-		
-		//Do For Data Points
-		for(DataPointAccess access : data.getDataPointPermissions()){
-			
-			DataPointVO vo = DaoRegistry.dataPointDao.get(access.getDataPointId());
-			if(vo != null){
-				DataPointPermissionModel model = new DataPointPermissionModel();
-				model.setDataPointXid(vo.getXid());
-				model.setPermission(PermissionEnum.convertTo(access.getPermission()));
-				dataPointPermissions.add(model);
-			}
-		}
-		
-		PermissionsModel model = new PermissionsModel(dataSourceXids, dataPointPermissions);
-		
-		return model;
-	}
+    @JsonGetter("email")
+    public String getEmail() {
+        return data.getEmail();
+    }
 
-	@JsonSetter("permissions")
-	public void setPermissions(PermissionsModel permissions) {
-		
-		for(String dsXid : permissions.getDataSourceXids()){
-			DataSourceVO<?> vo = DaoRegistry.dataSourceDao.getByXid(dsXid);
-			if(vo != null)
-				this.data.getDataSourcePermissions().add(vo.getId());
-		}
-		
-		for(DataPointPermissionModel model : permissions.getDataPointPermissions()){
-			DataPointVO vo = DaoRegistry.dataPointDao.getByXid(model.getDataPointXid());
-			if(vo != null){
-				DataPointAccess access = new DataPointAccess();
-				access.setDataPointId(vo.getId());
-				access.setPermission(PermissionEnum.convertFrom(model.getPermission()));
-				this.data.getDataPointPermissions().add(access);
-			}
-		}
-		
-	}
-	
-	@JsonGetter("homeUrl")
-	public String getHomeUrl() {
-		return data.getHomeUrl();
-	}
+    @JsonSetter("email")
+    public void setEmail(String email) {
+        data.setEmail(email);
+    }
 
-	@JsonSetter("homeUrl")
-	public void setHomeUrl(String homeUrl) {
-		data.setHomeUrl(homeUrl);
-	}
-	
-	@JsonGetter("receiveAlarmEmails")
-	public AlarmLevel getReceiveAlarmEmails() {
-		return AlarmLevel.convertTo(data.getReceiveAlarmEmails());
-	}
+    @JsonGetter("disabled")
+    public Boolean getDisabled() {
+        return data.isDisabled();
+    }
 
-	@JsonSetter("receiveAlarmEmails")
-	public void setReceiveAlarmEmails(AlarmLevel level) {
-		data.setReceiveAlarmEmails(AlarmLevel.convertFrom(level));
-	}
-	
-	@JsonGetter("timezone")
-	public String getTimezone() {
-		return data.getTimezone();
-	}
+    @JsonSetter("disabled")
+    public void setDisabled(Boolean disabled) {
+        data.setDisabled(disabled);
+    }
 
-	@JsonSetter("timezone")
-	public void setTimezone(String zone) {
-		data.setTimezone(zone);
-	}
-	
-	@JsonGetter("systemTimezone")
-	public String getSystemTimezone() {
-	    return TimeZone.getDefault().getID();
-	}
-	
-	@JsonSetter("systemTimezone")
+    @JsonGetter("permissions")
+    public String getPermissions() {
+        return data.getPermissions();
+    }
+
+    @JsonSetter("permissions")
+    public void setPermissions(String permissions) {
+        data.setPermissions(permissions);
+    }
+
+    @JsonGetter("homeUrl")
+    public String getHomeUrl() {
+        return data.getHomeUrl();
+    }
+
+    @JsonSetter("homeUrl")
+    public void setHomeUrl(String homeUrl) {
+        data.setHomeUrl(homeUrl);
+    }
+
+    @JsonGetter("receiveAlarmEmails")
+    public AlarmLevel getReceiveAlarmEmails() {
+        return AlarmLevel.convertTo(data.getReceiveAlarmEmails());
+    }
+
+    @JsonSetter("receiveAlarmEmails")
+    public void setReceiveAlarmEmails(AlarmLevel level) {
+        data.setReceiveAlarmEmails(AlarmLevel.convertFrom(level));
+    }
+
+    @JsonGetter("timezone")
+    public String getTimezone() {
+        return data.getTimezone();
+    }
+
+    @JsonSetter("timezone")
+    public void setTimezone(String zone) {
+        data.setTimezone(zone);
+    }
+
+    @JsonGetter("systemTimezone")
+    public String getSystemTimezone() {
+        return TimeZone.getDefault().getID();
+    }
+
+    @JsonSetter("systemTimezone")
     public void setSystemTimezone(String zone) {
         // no op
     }
-	
-	@JsonGetter("muted")
-	public Boolean getMuted() {
-		return data.isMuted();
-	}
 
-	@JsonSetter("muted")
-	public void setMuted(Boolean muted) {
-		data.setMuted(muted);
-	}
+    @JsonGetter("muted")
+    public Boolean getMuted() {
+        return data.isMuted();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.serotonin.m2m2.web.mvc.rest.v1.model.AbstractRestModel#validate(com.serotonin.m2m2.web.mvc.rest.v1.message.RestProcessResult)
-	 */
-	@Override
-	public void validate(RestProcessResult<?> result) throws RestValidationFailedException{
-		ProcessResult validation = new ProcessResult();
-		this.data.validate(validation);
-		
-		if(validation.getHasMessages()){
-			result.addRestMessage(HttpStatus.BAD_REQUEST, new TranslatableMessage("common.default", "Validation failed"));
-			result.addValidationMessages(validation);
-			throw new RestValidationFailedException(this, result);
-		}
-	}
+    @JsonSetter("muted")
+    public void setMuted(Boolean muted) {
+        data.setMuted(muted);
+    }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.serotonin.m2m2.web.mvc.rest.v1.model.AbstractRestModel#validate(com.serotonin.m2m2.web.mvc.rest.v1.message
+     * .RestProcessResult)
+     */
+    @Override
+    public void validate(RestProcessResult<?> result) throws RestValidationFailedException {
+        ProcessResult validation = new ProcessResult();
+        this.data.validate(validation);
 
-	
+        if (validation.getHasMessages()) {
+            result.addRestMessage(HttpStatus.BAD_REQUEST,
+                    new TranslatableMessage("common.default", "Validation failed"));
+            result.addValidationMessages(validation);
+            throw new RestValidationFailedException(this, result);
+        }
+    }
+
 }
