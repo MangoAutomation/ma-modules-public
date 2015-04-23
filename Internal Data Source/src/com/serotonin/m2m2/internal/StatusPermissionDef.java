6a9f8d2ef3c214ec82c813d5bc1b7c5c3c0ad396
terrypacker <username@users.noreply.github.com>
2015-04-22 15:05:58 -1000

    Adding in user group to permissions on module install.

--- Internal Data Source/src/com/serotonin/m2m2/internal/StatusPermissionDef.java
+++ Internal Data Source/src/com/serotonin/m2m2/internal/StatusPermissionDef.java
@@ -1,5 +1,8 @@
 package com.serotonin.m2m2.internal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.serotonin.m2m2.module.PermissionDefinition;
 
 public class StatusPermissionDef extends PermissionDefinition {
@@ -15,4 +18,14 @@ public class StatusPermissionDef extends PermissionDefinition {
     public String getPermissionTypeName() {
         return PERMISSION;
     }
+    
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.PermissionDefinition#getDefaultGroups()
+	 */
+	@Override
+	public List<String> getDefaultGroups() {
+		List<String> groups = new ArrayList<String>();
+		groups.add("user");
+		return groups;
+	}
 }
