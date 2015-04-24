ee40140e532f482701856527434c1540e7bf630b
terrypacker <username@users.noreply.github.com>
2015-04-24 12:04:46 -1000

    Removing 'users' group from Internal Data Source's default permissions.

--- Internal Data Source/src/com/serotonin/m2m2/internal/StatusPermissionDef.java
+++ Internal Data Source/src/com/serotonin/m2m2/internal/StatusPermissionDef.java
@@ -1,8 +1,5 @@
 package com.serotonin.m2m2.internal;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.serotonin.m2m2.module.PermissionDefinition;
 
 public class StatusPermissionDef extends PermissionDefinition {
@@ -18,14 +15,4 @@ public class StatusPermissionDef extends PermissionDefinition {
     public String getPermissionTypeName() {
         return PERMISSION;
     }
-    
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getDefaultGroups()
-	 */
-	@Override
-	public List<String> getDefaultGroups() {
-		List<String> groups = new ArrayList<String>();
-		groups.add("user");
-		return groups;
-	}
 }
