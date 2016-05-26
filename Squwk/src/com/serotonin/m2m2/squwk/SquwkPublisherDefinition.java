29bea01fd75953eea2b98774916f703f79a2067b
terrypacker <username@users.noreply.github.com>
2016-05-26 08:21:29 -1000

    Method stubs for publisher models.

--- Squwk/src/com/serotonin/m2m2/squwk/SquwkPublisherDefinition.java
+++ Squwk/src/com/serotonin/m2m2/squwk/SquwkPublisherDefinition.java
@@ -5,6 +5,8 @@ import com.serotonin.m2m2.squwk.pub.SquwkPublisherDwr;
 import com.serotonin.m2m2.squwk.pub.SquwkSenderVO;
 import com.serotonin.m2m2.vo.publish.PublishedPointVO;
 import com.serotonin.m2m2.vo.publish.PublisherVO;
+import com.serotonin.m2m2.web.mvc.rest.v1.model.publisher.AbstractPublishedPointModel;
+import com.serotonin.m2m2.web.mvc.rest.v1.model.publisher.AbstractPublisherModel;
 
 public class SquwkPublisherDefinition extends PublisherDefinition {
     @Override
@@ -31,4 +33,22 @@ public class SquwkPublisherDefinition extends PublisherDefinition {
     public Class<?> getDwrClass() {
         return SquwkPublisherDwr.class;
     }
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.PublisherDefinition#getPublisherModelClass()
+	 */
+	@Override
+	public Class<? extends AbstractPublisherModel<?, ?>> getPublisherModelClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.PublisherDefinition#getPublishedPointModelClass()
+	 */
+	@Override
+	public Class<? extends AbstractPublishedPointModel<?>> getPublishedPointModelClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
