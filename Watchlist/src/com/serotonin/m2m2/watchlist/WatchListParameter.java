1f8edc178c7c2404c03f2001f7f15f24730376fc
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-10-07 14:14:58 -0600

    Add missing options member

--- Watchlist/src/com/serotonin/m2m2/watchlist/WatchListParameter.java
+++ Watchlist/src/com/serotonin/m2m2/watchlist/WatchListParameter.java
@@ -4,6 +4,8 @@
  */
 package com.serotonin.m2m2.watchlist;
 
+import java.util.Map;
+
 /**
  * @author Terry Packer
  *
@@ -13,20 +15,22 @@ public class WatchListParameter {
 	private String name;
 	private String type;
 	private String label;
+	private Map<String, Object> options;
 	
 	public WatchListParameter(){ }
 	
-	public WatchListParameter(String name, String type, String label) {
+	public WatchListParameter(String name, String type, String label, Map<String, Object> options) {
 		super();
 		this.name = name;
 		this.type = type;
 		this.label = label;
+		this.options = options;
 	}
 	
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+    public void setName(String name) {
 		this.name = name;
 	}
 	public String getType() {
@@ -41,7 +45,10 @@ public class WatchListParameter {
 	public void setLabel(String label) {
 		this.label = label;
 	}
-
-	
-	
+    public Map<String, Object> getOptions() {
+        return options;
+    }
+    public void setOptions(Map<String, Object> options) {
+        this.options = options;
+    }
 }
