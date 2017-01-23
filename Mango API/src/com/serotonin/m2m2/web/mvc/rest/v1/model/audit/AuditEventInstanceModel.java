d20e9c3fc9960dbd718f7d9c0bd6e52c13413027
terrypacker <username@users.noreply.github.com>
2017-01-23 13:22:53 -1000

    Fleshing out AuditEventInstanceModel to return full event data

--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/audit/AuditEventInstanceModel.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/audit/AuditEventInstanceModel.java
@@ -4,6 +4,12 @@
  */
 package com.serotonin.m2m2.web.mvc.rest.v1.model.audit;
 
+import com.serotonin.json.type.JsonObject;
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.db.dao.UserDao;
+import com.serotonin.m2m2.i18n.TranslatableMessage;
+import com.serotonin.m2m2.rt.event.AlarmLevels;
+import com.serotonin.m2m2.vo.User;
 import com.serotonin.m2m2.vo.event.audit.AuditEventInstanceVO;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.AbstractBasicVoModel;
 
@@ -24,6 +30,65 @@ public class AuditEventInstanceModel extends AbstractBasicVoModel<AuditEventInst
 		super(new AuditEventInstanceVO());
 	}
 
+	public String getTypeName() {
+		return this.data.getTypeName();
+	}
+	public void setTypeName(String typeName) {
+		this.data.setTypeName(typeName);
+	}
+	public String getAlarmLevel() {
+		return AlarmLevels.CODES.getCode(this.data.getAlarmLevel());
+	}
+	public void setAlarmLevel(String alarmLevel) {
+		this.data.setAlarmLevel(AlarmLevels.CODES.getId(alarmLevel));
+	}
+	public String getUsername() {
+		User u = UserDao.instance.get(this.data.getUserId());
+		if(u != null)
+			return u.getUsername();
+		else
+			return null;
+	}
+	public void setUsername(String username) {
+		User u = UserDao.instance.getUser(username);
+		if(u != null)
+			this.data.setUserId(u.getId());
+	}
+	public String getChangeType() {
+		return AuditEventInstanceVO.CHANGE_TYPE_CODES.getCode(this.data.getChangeType());
+	}
+	public void setChangeType(String changeType) {
+		this.data.setChangeType(AuditEventInstanceVO.CHANGE_TYPE_CODES.getId(changeType));
+	}
+	public int getObjectId() {
+		return this.data.getObjectId();
+	}
+	public void setObjectId(int objectId) {
+		this.data.setObjectId(objectId);
+	}
+	public long getTimestamp() {
+		return this.data.getTimestamp();
+	}
+	public void setTimestamp(long timestamp) {
+		this.data.setTimestamp(timestamp);
+	}
+	public JsonObject getContext() {
+		return this.data.getContext();
+	}
+	public void setContext(JsonObject context) {
+		this.data.setContext(context);
+	}
+	public String getMessage() {
+		TranslatableMessage m = this.data.getMessage();
+		if(m != null)
+			return m.translate(Common.getTranslations());
+		else
+			return null;
+	}
+	public void setMessage(String message) {
+		this.data.setMessage(new TranslatableMessage("common.default", message));
+	}
+	
 	/* (non-Javadoc)
 	 * @see com.serotonin.m2m2.web.mvc.rest.v1.model.AbstractVoModel#getModelType()
 	 */
