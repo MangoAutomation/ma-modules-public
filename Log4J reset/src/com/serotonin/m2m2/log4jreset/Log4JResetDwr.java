eab36559ccda726b4ee1c4b328ab27b2d44d6f41
Phillip Dunlap <phillip@infiniteautomation.com>
2017-04-05 09:39:46 -0600

    Fixed for Log4J2

--- Log4J reset/src/com/serotonin/m2m2/log4jreset/Log4JResetDwr.java
+++ Log4J reset/src/com/serotonin/m2m2/log4jreset/Log4JResetDwr.java
@@ -1,13 +1,9 @@
 package com.serotonin.m2m2.log4jreset;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Enumeration;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
 
 import com.serotonin.m2m2.web.dwr.ModuleDwr;
 import com.serotonin.m2m2.web.dwr.util.DwrPermission;
@@ -18,25 +14,7 @@ public class Log4JResetDwr extends ModuleDwr {
     @DwrPermission(admin = true)
     public void resetLog4J() {
         LOG.info("Reloading Log4J configuration");
-
-        try {
-            LogManager.resetConfiguration();
-            Enumeration<URL> urls = getClass().getClassLoader().getResources("log4j.xml");
-            URL xml = null;
-            while (urls.hasMoreElements())
-                xml = urls.nextElement();
-
-            if (xml != null) {
-                DOMConfigurator.configure(xml);
-                LOG.info("Log4J configuration found at " + xml.getPath());
-            }
-            else
-                LOG.info("No Log4J configuration found");
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+    	((LoggerContext)LogManager.getContext(false)).reconfigure();;
         LOG.info("Finished reloading Log4J configuration");
     }
 
