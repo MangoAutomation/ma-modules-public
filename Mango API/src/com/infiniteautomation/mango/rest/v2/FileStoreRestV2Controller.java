aea860ea3649f9dc91e14f71aa922f7d348595b9
terrypacker <username@users.noreply.github.com>
2017-05-22 17:12:54 -1000

    Finishing File Store endpoints.

--- Mango API/src/com/infiniteautomation/mango/rest/v2/FileStoreRestV2Controller.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/FileStoreRestV2Controller.java
@@ -4,9 +4,14 @@
  */
 package com.infiniteautomation.mango.rest.v2;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -17,10 +22,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.infiniteautomation.mango.rest.v2.exception.GenericRestException;
 import com.infiniteautomation.mango.rest.v2.exception.NotFoundRestException;
@@ -39,7 +48,7 @@ import com.wordnik.swagger.annotations.ApiParam;
  */
 @Api(value="File Store", description="Allow read/write access to file storage areas")
 @RestController()
-@RequestMapping("/v2/file-store")
+@RequestMapping("/v2/file-stores")
 public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
 
 	@ApiOperation(
@@ -101,23 +110,27 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
 	
 	@ApiOperation(
 			value = "Upload a file to a store with a path",
-			notes = "Must have write access to the store"
+			notes = "Must have write access to the store, will overwrite existing files"
 			)
-	@RequestMapping(method = RequestMethod.GET, produces={"application/json"}, value="/{name}/{path}")
-    public ResponseEntity<Void> upload(
+	@RequestMapping(method = RequestMethod.POST, produces={"application/json"}, value="/{name}/**")
+    public ResponseEntity<List<String>> uploadWithPath(
        		@ApiParam(value = "Valid File Store name", required = true, allowMultiple = false)
        	 	@PathVariable("name") String name,
-       	 	@ApiParam(value = "Valid File Store path", required = false, defaultValue="", allowMultiple = false)
-       	 	@PathVariable("path") String path,
     		@AuthenticationPrincipal User user,
-    		HttpServletRequest request) {
+    		MultipartHttpServletRequest multipartRequest,
+    		HttpServletRequest request) throws IOException {
 		
 		FileStoreDefinition def = ModuleRegistry.getFileStoreDefinition(name);
 		if(def == null)
 			throw new NotFoundRestException();
 
+		//Check Permissions
+		def.ensureStoreWritePermission(user);
+
+		String pathInStore = parsePath(request);
+		
 		File root = def.getRoot();
-		String[] parts = path.split("/");
+		String[] parts = pathInStore.split("/");
 		File toSave = new File(root.getAbsolutePath());
 		for(String part : parts)
 			toSave = new File(toSave, part);
@@ -126,11 +139,66 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
 				throw new GenericRestException(HttpStatus.INTERNAL_SERVER_ERROR, new TranslatableMessage("filestore.cannotCreateDir", removeToRoot(root, toSave), name));
 		}
 		
-		//TODO Put the file where it belongs
-		
-		return new ResponseEntity<>(null, HttpStatus.OK);
+		//Put the file where it belongs
+		List<String> filenames = new ArrayList<String>();
+		Iterator<String> itr =  multipartRequest.getFileNames();
+		while(itr.hasNext()){
+            MultipartFile file = multipartRequest.getFile(itr.next());
+    		if (!file.isEmpty()) {
+    			File newFile = new File(toSave, file.getName());
+    			filenames.add(removeToRoot(root, newFile));
+            	byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(newFile, false));
+                stream.write(bytes);
+                stream.close();
+    		}
+		}
+		return new ResponseEntity<>(filenames, HttpStatus.OK);
 	}
 	
+	@ApiOperation(
+			value = "Download a file from a store",
+			notes = "Must have write access to the store"
+			)
+	@RequestMapping(method = RequestMethod.GET, produces={"application/octet-stream", "application/json"}, value="/{name}/**")
+    public ResponseEntity<byte[]> download(
+       		@ApiParam(value = "Valid File Store name", required = true, allowMultiple = false)
+       	 	@PathVariable("name") String name,
+    		@AuthenticationPrincipal User user,
+    		HttpServletRequest request) throws IOException {
+    	
+		FileStoreDefinition def = ModuleRegistry.getFileStoreDefinition(name);
+		if(def == null)
+			throw new NotFoundRestException();
+		
+		//Check permissions
+		def.ensureStoreReadPermission(user);
+		
+		File f = new File(def.getRoot(), parsePath(request));
+		//TODO Allow downloading directory as a zip
+		if(!f.exists())
+			throw new NotFoundRestException();
+		if(!f.isFile())
+			throw new GenericRestException(HttpStatus.INTERNAL_SERVER_ERROR, new TranslatableMessage("rest.fileStore.notAFile"));
+		
+		return new ResponseEntity<byte[]>(Files.readAllBytes(f.toPath()), HttpStatus.OK);
+	}
+	
+    /**
+     * Get the path within the store off the URL
+     * @param request
+     * @return
+     */
+    protected String parsePath(HttpServletRequest request){
+	    String path = (String) request.getAttribute(
+	            HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+	    String bestMatchPattern = (String ) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+
+	    AntPathMatcher apm = new AntPathMatcher();
+	    return apm.extractPathWithinPattern(bestMatchPattern, path);
+    }
+    
 	/**
 	 * Remove the path up to the root folder
 	 * @param root
