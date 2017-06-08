6710546d90589ea3610a2a0fae778ee3d82f7452
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-06-08 12:31:50 -0600

    Simplify mime handling, correctly upload multiple files, dont overwrite

--- Mango API/src/com/infiniteautomation/mango/rest/v2/FileStoreRestV2Controller.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/FileStoreRestV2Controller.java
@@ -4,10 +4,11 @@
  */
 package com.infiniteautomation.mango.rest.v2;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -15,7 +16,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,19 +32,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.HandlerMapping;
 
+import com.google.common.collect.Sets;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Files;
 import com.infiniteautomation.mango.rest.v2.exception.GenericRestException;
 import com.infiniteautomation.mango.rest.v2.exception.NotFoundRestException;
 import com.infiniteautomation.mango.rest.v2.exception.ResourceNotFoundException;
@@ -100,7 +102,7 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
 			value = "Upload a file to a store with a path",
 			notes = "Must have write access to the store, will overwrite existing files"
 			)
-	@RequestMapping(method = RequestMethod.POST, produces={"application/json"}, value="/{name}/**")
+	@RequestMapping(method = RequestMethod.POST, consumes=MediaType.MULTIPART_FORM_DATA_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE, value="/{name}/**")
     public ResponseEntity<List<FileModel>> uploadWithPath(
        		@ApiParam(value = "Valid File Store name", required = true, allowMultiple = false)
        	 	@PathVariable("name") String name,
@@ -131,25 +133,44 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
 		
 		//Put the file where it belongs
 		List<FileModel> fileModels = new ArrayList<>();
-		Iterator<String> itr =  multipartRequest.getFileNames();
-		while(itr.hasNext()){
-            MultipartFile file = multipartRequest.getFile(itr.next());
-            String filename = file.getOriginalFilename();
-            if (filename == null) {
-                filename = file.getName();
-            }
-			File newFile = new File(toSave, filename);
-			fileModels.add(fileToModel(newFile, root, request.getServletContext()));
-        	byte[] bytes = file.getBytes();
-            try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(newFile, false))) {
-                stream.write(bytes);
-            }
+		
+		MultiValueMap<String, MultipartFile> filemap = multipartRequest.getMultiFileMap();
+		for (String nameField : filemap.keySet()) {
+		    for (MultipartFile file : filemap.get(nameField)) {
+	            String filename = file.getOriginalFilename();
+	            File newFile = findUniqueFileName(toSave, filename);
+	            try (OutputStream output = new FileOutputStream(newFile, false)) {
+	                try (InputStream input  = file.getInputStream()) {
+	                    ByteStreams.copy(input, output);
+	                }
+	            }
+                fileModels.add(fileToModel(newFile, toSave, request.getServletContext()));
+		    }
 		}
+
 		return new ResponseEntity<>(fileModels, HttpStatus.OK);
 	}
 	
+	private File findUniqueFileName(File directory, String filename) {
+	    File file = new File(directory, filename);
+	    
+        String originalName = Files.getNameWithoutExtension(filename);
+        String extension = Files.getFileExtension(filename);
+	    int i = 1;
+        
+	    while (file.exists()) {
+	        if (extension.isEmpty()) {
+	            file = new File(directory, String.format("%s_%03d", originalName, i++));
+	        } else {
+	            file = new File(directory, String.format("%s_%03d.%s", originalName, i++, extension));
+	        }
+	    }
+	    
+	    return file;
+	}
+
 	@ApiOperation(value = "List a directory or download a file from a store")
-	@RequestMapping(method = RequestMethod.GET, produces={"*/*"}, value="/{name}/**")
+	@RequestMapping(method = RequestMethod.GET, produces={}, value="/{name}/**")
     public ResponseEntity<?> download(
        		@ApiParam(value = "Valid File Store name", required = true, allowMultiple = false)
        	 	@PathVariable("name") String name,
@@ -184,11 +205,10 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
         
         for (File file : files)
             found.add(fileToModel(file, directory, request.getServletContext()));
-        
-        @SuppressWarnings("unchecked")
-        Set<MediaType> mediaTypes = (Set<MediaType>) request.getAttribute(HandlerMapping.PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE);
-        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        
+
+        Set<MediaType> mediaTypes = Sets.newHashSet(MediaType.APPLICATION_JSON_UTF8);
+        request.setAttribute(HandlerMapping.PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE, mediaTypes);
+
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
         
@@ -199,45 +219,16 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(HttpHeaders.CONTENT_DISPOSITION, download ? "attachment" : "inline");
 
-        // We need to do our own custom MIME handling as the Spring implementation doesnt handle this correctly:
-        // - Wildcards like image/* dont work
-        // - Does not trigger HTTP 406 Not Acceptable if file MIME does not match the Accept header
-        
         MediaType mediaType = null;
-        
-        @SuppressWarnings("unchecked")
-        Set<MediaType> mediaTypes = (Set<MediaType>) request.getAttribute(HandlerMapping.PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE);
-        mediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+
+        Set<MediaType> mediaTypes = Sets.newHashSet(MediaType.APPLICATION_OCTET_STREAM);
+        request.setAttribute(HandlerMapping.PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE, mediaTypes);
         
         // ResourceHttpMessageConverter uses ActivationMediaTypeFactory.getMediaType(resource) but this is not visible
         String mimeType = request.getServletContext().getMimeType(file.getName());
         if (StringUtils.hasText(mimeType)) {
             try {
                 mediaType = MediaType.parseMediaType(mimeType);
-                MediaType compatibleType = null;
-
-                HeaderContentNegotiationStrategy strategy = new HeaderContentNegotiationStrategy();
-                List<MediaType> requestedMediaTypes = strategy.resolveMediaTypes(new ServletWebRequest(request));
-
-                if (requestedMediaTypes.isEmpty()) {
-                    compatibleType = mediaType;
-                } else {
-                    for (MediaType requestedType : requestedMediaTypes) {
-                        if (requestedType.isCompatibleWith(mediaType) || requestedType.isCompatibleWith(MediaType.APPLICATION_OCTET_STREAM)) {
-                            compatibleType = mediaType;
-                            break;
-                        }
-                    }
-                }
-
-                if (compatibleType != null) {
-                    // setting this request attribute essentially modifies the @RequestMapping(produces=xxx) property
-                    // we need to do this because otherwise AbstractMessageConverterMethodProcessor fails on partial wildcard Accept headers like image/*
-                    // and causes HTTP 406 Not Acceptable errors
-                    mediaTypes.add(mediaType);
-                } else {
-                    throw new HttpMediaTypeNotAcceptableException(Arrays.asList(mediaType, MediaType.APPLICATION_OCTET_STREAM));
-                }
             } catch (InvalidMediaTypeException e) {
                 // Shouldn't happen - ServletContext.getMimeType() should return valid mime types
             }
@@ -245,7 +236,13 @@ public class FileStoreRestV2Controller extends AbstractMangoRestV2Controller{
 
         // always set the content type header or AbstractHttpMessageConverter.addDefaultHeaders() will set the Content-Type
         // to whatever the Accept header was
-        responseHeaders.setContentType(mediaType != null ? mediaType : MediaType.APPLICATION_OCTET_STREAM);
+        if (mediaType == null) {
+            mediaTypes.add(MediaType.ALL);
+            responseHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        } else {
+            mediaTypes.add(mediaType);
+            responseHeaders.setContentType(mediaType);
+        }
         
         return new ResponseEntity<>(new FileSystemResource(file), responseHeaders, HttpStatus.OK);
 	}
