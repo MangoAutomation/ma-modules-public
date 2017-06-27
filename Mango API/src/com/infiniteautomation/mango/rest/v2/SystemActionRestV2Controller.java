f28b026d93c9c5f71f5ac6677191a8cea8b28855
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-06-27 17:22:56 -0600

    Fix system action REST controller

--- Mango API/src/com/infiniteautomation/mango/rest/v2/SystemActionRestV2Controller.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/SystemActionRestV2Controller.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,14 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.infiniteautomation.mango.rest.v2.exception.GenericRestException;
 import com.infiniteautomation.mango.rest.v2.exception.NotFoundRestException;
 import com.infiniteautomation.mango.rest.v2.util.MangoRestTemporaryResourceContainer;
 import com.infiniteautomation.mango.rest.v2.util.SystemActionTemporaryResource;
 import com.serotonin.m2m2.module.ModuleRegistry;
 import com.serotonin.m2m2.module.SystemActionDefinition;
 import com.serotonin.m2m2.vo.User;
-import com.serotonin.m2m2.web.mvc.rest.v1.message.RestProcessResult;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
@@ -38,11 +37,12 @@ import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
 
 /**
- * Class to provide server information
+ * Controller for triggering system actions and retrieving their status
  * 
  * @author Terry Packer
  */
 @Api(value="System Actions", description="Ask Mango to perform a pre-defined action.  Admin Only.")
+@PreAuthorize("isAdmin()")
 @RestController
 @RequestMapping("/v2/actions")
 public class SystemActionRestV2Controller extends AbstractMangoRestV2Controller{
@@ -59,24 +59,24 @@ public class SystemActionRestV2Controller extends AbstractMangoRestV2Controller{
 			response=List.class,
 			responseContainer="Array"
 			)
-	@RequestMapping(method = RequestMethod.GET, produces={"application/json"})
+	@RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<String>> list() {
-		RestProcessResult<List<String>> result = new RestProcessResult<>(HttpStatus.OK);
+
 		Map<String, SystemActionDefinition> defs = ModuleRegistry.getSystemActionDefinitions();
 		List<String> model = new ArrayList<String>();
 		for(SystemActionDefinition def : defs.values())
 			model.add(def.getKey());
-		return result.createResponseEntity(model);
+
+        return new ResponseEntity<>(model, HttpStatus.OK);
 	}
 
-	@PreAuthorize("isAdmin()")
 	@ApiOperation(value = "Perform an Action", notes="Kicks off action and returns temporary URL for status")
 	@ApiResponses({
 		@ApiResponse(code = 500, message = "Internal error", response=ResponseEntity.class),
 		@ApiResponse(code = 404, message = "Not Found", response=ResponseEntity.class),
 	})
-	@RequestMapping(method = RequestMethod.PUT, consumes={"application/json"}, produces={"application/json"}, value = "/{action}")
-    public ResponseEntity<Void> performAction(
+	@RequestMapping(method = RequestMethod.PUT, value = "/trigger/{action}")
+    public ResponseEntity<SystemActionTemporaryResource> performAction(
     		@ApiParam(value = "Valid System Action", required = true, allowMultiple = false)
 			@PathVariable String action,
 			@ApiParam(value = "Input for task", required = false, allowMultiple = false)
@@ -84,64 +84,53 @@ public class SystemActionRestV2Controller extends AbstractMangoRestV2Controller{
     		JsonNode input,
     		@AuthenticationPrincipal User user,
     		UriComponentsBuilder builder) {
-		try{
-			RestProcessResult<Void> result = new RestProcessResult<Void>(HttpStatus.OK);
-			
-			//Kick off action
-			SystemActionDefinition def = ModuleRegistry.getSystemActionDefinition(action);
-			if(def == null)
-				throw new NotFoundRestException();
-			
-			String resourceId = resources.generateResourceId();
-			SystemActionTemporaryResource resource = new SystemActionTemporaryResource(resourceId, def.getTask(user, input));
-			
-			//Resource can live for up to 10 minutes (TODO Configurable?)
-			resources.put(resourceId, resource, System.currentTimeMillis() + 600000);
-	    	URI location = builder.path("/v2/actions/status/{resourceId}").buildAndExpand(resourceId).toUri();
-	    	result.addHeader("Location", location.toString());
-	    	
-			return result.createResponseEntity();
-		}catch(Exception e){
-			throw new GenericRestException(HttpStatus.INTERNAL_SERVER_ERROR, e);
-		}
+	    //Kick off action
+        SystemActionDefinition def = ModuleRegistry.getSystemActionDefinition(action);
+        if(def == null)
+            throw new NotFoundRestException();
+        
+        String resourceId = resources.generateResourceId();
+        SystemActionTemporaryResource resource = new SystemActionTemporaryResource(resourceId, def.getTask(user, input));
+        
+        //Resource can live for up to 10 minutes (TODO Configurable?)
+        resources.put(resourceId, resource, System.currentTimeMillis() + 600000);
+        URI location = builder.path("/v2/actions/status/{resourceId}").buildAndExpand(resourceId).toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
 	}
-	
-	@PreAuthorize("isAdmin()")
+
 	@ApiOperation(value = "Get Action Progress", notes = "Polls temporary resource for results.")
 	@ApiResponses({
 		@ApiResponse(code = 401, message = "Unauthorized user access", response=ResponseEntity.class),
 		@ApiResponse(code = 404, message = "No resource exists with given id", response=ResponseEntity.class),
 		@ApiResponse(code = 500, message = "Error processing request", response=ResponseEntity.class)
 	})
-	@RequestMapping( method = {RequestMethod.GET}, value = {"/status/{resourceId}"}, produces = {"application/json"} )
+	@RequestMapping( method = {RequestMethod.GET}, value = {"/status/{resourceId}"})
 	public ResponseEntity<SystemActionTemporaryResource> getStatus(HttpServletRequest request, 
 			@ApiParam(value="Resource id", required=true, allowMultiple=false) @PathVariable String resourceId,
 			@AuthenticationPrincipal User user) {
-		RestProcessResult<SystemActionTemporaryResource> result = new RestProcessResult<>(HttpStatus.OK);
-		
+	    
 		SystemActionTemporaryResource resource = resources.get(resourceId);
-		
-		return result.createResponseEntity(resource);
+		return new ResponseEntity<>(resource, HttpStatus.OK);
 	}
-	
-	@PreAuthorize("isAdmin()")
+
 	@ApiOperation(value = "Cancel Action", notes = "No Guarantees that the cancel will work, this is task dependent.")
 	@ApiResponses({
 		@ApiResponse(code = 401, message = "Unauthorized user access", response=ResponseEntity.class),
 		@ApiResponse(code = 404, message = "No resource exists with given id", response=ResponseEntity.class),
 		@ApiResponse(code = 500, message = "Error processing request", response=ResponseEntity.class)
 	})
-	@RequestMapping( method = {RequestMethod.PUT}, value = {"/import/{resourceId}"}, produces = {"application/json"} )
+	@RequestMapping( method = {RequestMethod.PUT}, value = {"/cancel/{resourceId}"})
 	public ResponseEntity<SystemActionTemporaryResource> cancel(HttpServletRequest request, 
 			@ApiParam(value="Resource id", required=true, allowMultiple=false) @PathVariable String resourceId,
 			@AuthenticationPrincipal User user) {
-		RestProcessResult<SystemActionTemporaryResource> result = new RestProcessResult<>(HttpStatus.OK);
-		
+
 		SystemActionTemporaryResource resource = resources.get(resourceId);
-		
 		resource.cancel();
-		
-		return result.createResponseEntity(resource);
+        return new ResponseEntity<>(resource, HttpStatus.OK);
 	}
-	
+
 }
