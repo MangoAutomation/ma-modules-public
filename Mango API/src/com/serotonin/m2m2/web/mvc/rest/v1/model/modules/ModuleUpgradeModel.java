d4bd079d5a9a8268e8ac2da1d82e0a3d98b9eb4f
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-09-05 13:08:39 -0600

    Fixes as per infiniteautomation/m2m2Store#14

--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/modules/ModuleUpgradeModel.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/modules/ModuleUpgradeModel.java
@@ -4,7 +4,14 @@
  */
 package com.serotonin.m2m2.web.mvc.rest.v1.model.modules;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.serotonin.json.type.JsonArray;
+import com.serotonin.json.type.JsonObject;
 import com.serotonin.json.type.JsonValue;
+import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.module.Module;
 
 /**
@@ -15,6 +22,7 @@ public class ModuleUpgradeModel extends ModuleModel {
 
 	String newVersion;
     String releaseNotes;
+    Map<String, String> dependencyVersions;
     
 	public ModuleUpgradeModel() {
 	}
@@ -29,10 +37,15 @@ public class ModuleUpgradeModel extends ModuleModel {
 	}
 
 	public void loadFromStoreJson(JsonValue v) {
+        this.dependencyVersions = new HashMap<>();
+        
         JsonValue sub = v.getJsonValue("name");
         if(sub != null)
             this.name = sub.toString();
         sub = v.getJsonValue("version");
+        if(sub != null)
+            this.newVersion = sub.toString();
+        sub = v.getJsonValue("fullVersion");
         if(sub != null)
             this.newVersion = sub.toString();
         sub = v.getJsonValue("shortDescription");
@@ -50,6 +63,29 @@ public class ModuleUpgradeModel extends ModuleModel {
         sub = v.getJsonValue("releaseNotes");
         if(sub != null)
             this.releaseNotes = sub.toString();
+        
+        JsonValue dependencies = v.getJsonValue("dependencies");
+        JsonValue fullDependencies = v.getJsonValue("fullDependencies");
+        
+        if (dependencies instanceof JsonArray && !(fullDependencies instanceof JsonObject)) {
+            dependencies.toList().forEach((obj) -> {
+                String moduleName = obj.toString();
+                String versionRange = Common.getVersion().getMajorVersion() + "." + Common.getVersion().getMinorVersion();
+                this.dependencyVersions.put(moduleName, versionRange);
+            });
+        }
+        
+        if (fullDependencies instanceof JsonObject) {
+            fullDependencies.toMap().forEach((key, value) -> {
+                this.dependencyVersions.put(key, value.toString());
+            });
+        }
+        
+        this.dependencies = this.dependencyVersions.entrySet().stream()
+            .map((entry) -> {
+                return entry.getKey() + ":" + entry.getValue();
+            })
+            .collect(Collectors.joining(","));
 	}
 	
 	public String getReleaseNotes() {
@@ -67,4 +103,13 @@ public class ModuleUpgradeModel extends ModuleModel {
     public void setNewVersion(String newVersion) {
         this.newVersion = newVersion;
     }
+
+    public Map<String, String> getDependencyVersions() {
+        return dependencyVersions;
+    }
+
+    public void setDependencyVersions(Map<String, String> dependencyVersions) {
+        this.dependencyVersions = dependencyVersions;
+    }
+
 }
