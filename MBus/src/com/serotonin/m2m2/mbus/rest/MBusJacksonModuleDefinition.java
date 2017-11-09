1fe3370055971161f3ebc39920ea73406a22df88
terrypacker <terrypacker@users.noreply.github.com>
2017-11-09 11:20:30 -1000

    Consolidate object mapper use as per infiniteautomation/ma-core-public#1075

--- MBus/src/com/serotonin/m2m2/mbus/rest/MBusJacksonModuleDefinition.java
+++ MBus/src/com/serotonin/m2m2/mbus/rest/MBusJacksonModuleDefinition.java
@@ -5,20 +5,27 @@
 package com.serotonin.m2m2.mbus.rest;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.serotonin.m2m2.module.JsonRestJacksonModuleDefinition;
+import com.serotonin.m2m2.module.JacksonModuleDefinition;
 
 /**
  * 
  * @author Terry Packer
  */
-public class MBusJacksonModuleDefinition extends JsonRestJacksonModuleDefinition{
+public class MBusJacksonModuleDefinition extends JacksonModuleDefinition{
 
 	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.JsonRestJacksonModuleDefinition#getJacksonModule()
+	 * @see com.serotonin.m2m2.module.JacksonModuleDefinition#getJacksonModule()
 	 */
 	@Override
 	public SimpleModule getJacksonModule() {
 		return new MBusJacksonModule();
 	}
 
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.JacksonModuleDefinition#getSourceMapperType()
+	 */
+	@Override
+	public ObjectMapperSource getSourceMapperType() {
+	    return ObjectMapperSource.REST;
+	}
 }
--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/mapping/MangoApiJacksonModuleDefinition.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/mapping/MangoApiJacksonModuleDefinition.java
@@ -5,13 +5,15 @@
 package com.serotonin.m2m2.web.mvc.rest.v1.mapping;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.serotonin.m2m2.module.JsonRestJacksonModuleDefinition;
+import com.serotonin.m2m2.module.JacksonModuleDefinition;
 
 /**
+ * Module for REST jackson mapper
+ * 
  * @author Terry Packer
  *
  */
-public class MangoApiJacksonModuleDefinition extends JsonRestJacksonModuleDefinition{
+public class MangoApiJacksonModuleDefinition extends JacksonModuleDefinition{
 
 	/* (non-Javadoc)
 	 * @see com.serotonin.m2m2.module.JsonRestJacksonModuleDefinition#getJacksonModule()
@@ -19,6 +21,14 @@ public class MangoApiJacksonModuleDefinition extends JsonRestJacksonModuleDefini
 	@Override
 	public SimpleModule getJacksonModule() {
 		return new MangoApiJacksonModule();
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.JacksonModuleDefinition#getSourceMapperType()
+	 */
+	@Override
+	public ObjectMapperSource getSourceMapperType() {
+	    return ObjectMapperSource.REST;
 	}
 
 }
