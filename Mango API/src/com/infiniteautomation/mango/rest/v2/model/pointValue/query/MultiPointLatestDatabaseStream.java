9ae66f4a36d0a2e630550ce779ba4b75e69709b9
terrypacker <terrypacker@users.noreply.github.com>
2017-12-22 10:36:04 -1000

    Fix for using 1 data point on the multiple-arrays point values endpoint

--- Mango API/src/com/infiniteautomation/mango/rest/v2/model/pointValue/query/MultiPointLatestDatabaseStream.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/model/pointValue/query/MultiPointLatestDatabaseStream.java
@@ -92,7 +92,7 @@ public class MultiPointLatestDatabaseStream <T, INFO extends LatestQueryInfo> ex
             if(currentValues.size() > 0)
                 writer.writeMultiplePointValuesAtSameTime(currentValues, currentValues.get(0).getPvt().getTime());
         }else {
-            if(voMap.size() > 1) {
+            if(!info.isSingleArray() && voMap.size() > 1) {
                 if(contentType == StreamContentType.JSON)
                     writer.writeEndArray();
             }
