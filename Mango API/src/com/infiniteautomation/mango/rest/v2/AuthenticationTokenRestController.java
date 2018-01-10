e3e1654533f9d6bf9a75922d26726d7b87ba2862
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-01-10 15:02:26 -0700

    Move creating auth tokens to a single reset end point

--- Mango API/src/com/infiniteautomation/mango/rest/v2/AuthenticationTokenRestController.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/AuthenticationTokenRestController.java
@@ -8,22 +8,24 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.infiniteautomation.mango.rest.v2.exception.AccessDeniedException;
+import com.infiniteautomation.mango.rest.v2.exception.BadRequestException;
 import com.infiniteautomation.mango.rest.v2.exception.NotFoundRestException;
 import com.infiniteautomation.mango.rest.v2.model.jwt.HeaderClaimsModel;
 import com.infiniteautomation.mango.rest.v2.model.jwt.TokenModel;
 import com.serotonin.m2m2.db.dao.UserDao;
+import com.serotonin.m2m2.i18n.TranslatableMessage;
 import com.serotonin.m2m2.vo.User;
 import com.serotonin.m2m2.web.mvc.rest.v1.MangoRestController;
 import com.serotonin.m2m2.web.mvc.spring.components.TokenAuthenticationService;
@@ -47,8 +49,6 @@ import io.jsonwebtoken.Jws;
 @RequestMapping("/v2/auth-tokens")
 public class AuthenticationTokenRestController extends MangoRestController {
 
-    private static final int DEFAULT_EXPIRY = 5 * 60 * 1000; // 5 minutes
-    
     private final TokenAuthenticationService tokenAuthService;
     private final MangoSessionRegistry sessionRegistry;
 
@@ -58,18 +58,28 @@ public class AuthenticationTokenRestController extends MangoRestController {
         this.sessionRegistry = sessionRegistry;
     }
 
-    @ApiOperation(value = "Create token", notes = "Creates a token for the current user")
+    @ApiOperation(value = "Create auth token", notes = "Creates an authentication token for the current user or for the username specified (admin only)")
     @RequestMapping(path="/create", method = RequestMethod.POST)
     @PreAuthorize("isAuthenticated() and isPasswordAuthenticated()")
     public ResponseEntity<TokenModel> createToken(
-            @RequestParam(required = false)
-            @DateTimeFormat(iso = ISO.DATE_TIME)
-            Date expiry,
-            
-            @AuthenticationPrincipal User user) {
+            @RequestBody
+            CreateTokenRequest requestBody,
 
-        if (expiry == null) {
-            expiry = new Date(System.currentTimeMillis() + DEFAULT_EXPIRY);
+            @AuthenticationPrincipal User currentUser) {
+
+        Date expiry = requestBody.getExpiry();
+        String username = requestBody.getUsername();
+
+        User user = currentUser;
+        if (username != null && !username.equals(currentUser.getUsername())) {
+            if (!currentUser.isAdmin()) {
+                throw new AccessDeniedException(new TranslatableMessage("rest.error.onlyAdminsCanCreateTokens"));
+            }
+            
+            user = UserDao.instance.getUser(username);
+            if (user == null) {
+                throw new BadRequestException(new TranslatableMessage("rest.error.unknownUser", username));
+            }
         }
 
         String token = tokenAuthService.generateToken(user, expiry);
@@ -89,30 +99,7 @@ public class AuthenticationTokenRestController extends MangoRestController {
         
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-    
-    @ApiOperation(value = "Create token for user", notes = "Creates a token for a given user")
-    @RequestMapping(path="/create/{username}", method = RequestMethod.POST)
-    @PreAuthorize("isAdmin() and isPasswordAuthenticated()")
-    public ResponseEntity<TokenModel> createTokenForUser(
-            @PathVariable String username,
-            
-            @RequestParam(required = false)
-            @DateTimeFormat(iso = ISO.DATE_TIME)
-            Date expiry) {
 
-        if (expiry == null) {
-            expiry = new Date(System.currentTimeMillis() + DEFAULT_EXPIRY);
-        }
-
-        User user = UserDao.instance.getUser(username);
-        if (user == null) {
-            throw new NotFoundRestException();
-        }
-        
-        String token = tokenAuthService.generateToken(user, expiry);
-        return new ResponseEntity<>(new TokenModel(token), HttpStatus.CREATED);
-    }
-    
     @ApiOperation(value = "Revoke all tokens for user", notes = "Revokes all tokens for a given user")
     @RequestMapping(path="/revoke/{username}", method = RequestMethod.POST)
     @PreAuthorize("isAdmin() and isPasswordAuthenticated()")
@@ -154,5 +141,35 @@ public class AuthenticationTokenRestController extends MangoRestController {
     public ResponseEntity<Void> resetKeys() {
         tokenAuthService.resetKeys();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    
+    public static class CreateTokenRequest {
+        private String password;
+        private String username;
+        private Date expiry;
+        
+        public String getPassword() {
+            return password;
+        }
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        public String getUsername() {
+            return username;
+        }
+        public void setUsername(String username) {
+            this.username = username;
+        }
+        public Date getExpiry() {
+            return expiry;
+        }
+        public void setExpiry(Date expiry) {
+            this.expiry = expiry;
+        }
+        @Override
+        public String toString() {
+            return "CreateTokenRequest [password=<redacted>, username=" + username
+                    + ", expiry=" + expiry + "]";
+        }
     }
 }
