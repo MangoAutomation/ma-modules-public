21a42b4cf1c8eb51e497746ee41b0da4660b6966
Phillip Dunlap <phillip@infiniteautomation.com>
2018-01-16 14:13:05 -0700

    Fixing issue with deleting handled maintenance event

--- Maintenance Events/src/com/serotonin/m2m2/maintenanceEvents/MaintenanceEventDao.java
+++ Maintenance Events/src/com/serotonin/m2m2/maintenanceEvents/MaintenanceEventDao.java
@@ -135,7 +135,7 @@ public class MaintenanceEventDao extends BaseDao {
             getTransactionTemplate().execute(new TransactionCallbackWithoutResult() {
                 @Override
                 protected void doInTransactionWithoutResult(TransactionStatus status) {
-                    ejt2.update("delete from eventHandlers where eventTypeName=? and eventTypeRef1=?", new Object[] {
+                    ejt2.update("delete from eventHandlersMapping where eventTypeName=? and eventTypeRef1=?", new Object[] {
                             MaintenanceEventType.TYPE_NAME, maintenanceEventId });
                     ejt2.update("delete from maintenanceEvents where id=?", new Object[] { maintenanceEventId });
                 }
