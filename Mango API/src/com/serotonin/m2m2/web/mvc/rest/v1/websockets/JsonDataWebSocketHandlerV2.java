2c0413d0d76fde383afd568e273132fee30accfe
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-09-24 11:29:03 -0600

    Revert v1 json data websocket back to previous behavior, add v2

--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/websockets/JsonDataWebSocketHandlerV2.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/websockets/JsonDataWebSocketHandlerV2.java
@@ -7,21 +7,21 @@ package com.serotonin.m2m2.web.mvc.rest.v1.websockets;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import com.infiniteautomation.mango.rest.v2.websocket.dao.SubscriptionDaoWebSocketHandler;
 import com.infiniteautomation.mango.spring.events.DaoEvent;
 import com.serotonin.m2m2.vo.User;
 import com.serotonin.m2m2.vo.json.JsonDataVO;
 import com.serotonin.m2m2.vo.permission.Permissions;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.jsondata.JsonDataModel;
 import com.serotonin.m2m2.web.mvc.spring.WebSocketMapping;
-import com.serotonin.m2m2.web.mvc.websocket.DaoNotificationWebSocketHandler;
 
 /**
  * @author Terry Packer
  *
  */
 @Component
-@WebSocketMapping("/v1/websocket/json-data")
-public class JsonDataWebSocketHandler extends DaoNotificationWebSocketHandler<JsonDataVO> {
+@WebSocketMapping("/v2/websocket/json-data")
+public class JsonDataWebSocketHandlerV2 extends SubscriptionDaoWebSocketHandler<JsonDataVO> {
 
     @Override
     protected boolean hasPermission(User user, JsonDataVO vo) {
