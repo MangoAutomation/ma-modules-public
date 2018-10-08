9b22619d2de56b2b3124662ad9793a56ff72211b
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-10-08 17:14:28 -0600

    Update webpack config

--- SQL console/webpack.config.js
+++ SQL console/webpack.config.js
@@ -3,21 +3,5 @@
  * @author Jared Wiltshire
  */
 
-const path = require('path');
 const moduleConfig = require('@infinite-automation/mango-module-tools');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-module.exports = moduleConfig.then(config => {
-    config.optimization = {
-        splitChunks: false
-    };
-    
-    config.output.path = path.resolve('web', 'angular');
-    config.output.publicPath = config.output.publicPath + 'angular/';
-    
-    config.plugins.push(new CleanWebpackPlugin(['web/angular'], {
-        root: __dirname
-    }));
-
-    return config;
-});
+module.exports = moduleConfig();
