b373e517c97844c0c0685b8d079a8b59a7770758
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-10-29 12:22:57 -0600

    Fix validation bug

--- Reports/src/com/serotonin/m2m2/reports/handler/ReportEventHandlerVO.java
+++ Reports/src/com/serotonin/m2m2/reports/handler/ReportEventHandlerVO.java
@@ -21,108 +21,108 @@ import com.serotonin.m2m2.vo.event.AbstractEventHandlerVO;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.events.handlers.AbstractEventHandlerModel;
 
 /**
- * 
+ *
  * @author Terry Packer
  */
 public class ReportEventHandlerVO extends AbstractEventHandlerVO<ReportEventHandlerVO>{
 
-	private int activeReportId = Common.NEW_ID;
-	private int inactiveReportId = Common.NEW_ID;
-	
-	public int getActiveReportId() {
-		return activeReportId;
-	}
+    private int activeReportId = Common.NEW_ID;
+    private int inactiveReportId = Common.NEW_ID;
 
-	public void setActiveReportId(int activeReportId) {
-		this.activeReportId = activeReportId;
-	}
+    public int getActiveReportId() {
+        return activeReportId;
+    }
 
-	public int getInactiveReportId() {
-		return inactiveReportId;
-	}
+    public void setActiveReportId(int activeReportId) {
+        this.activeReportId = activeReportId;
+    }
 
-	public void setInactiveReportId(int inactiveReportId) {
-		this.inactiveReportId = inactiveReportId;
-	}
+    public int getInactiveReportId() {
+        return inactiveReportId;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#createRuntime()
-	 */
-	@Override
-	public EventHandlerRT<?> createRuntime() {
-		return new ReportEventHandlerRT(this);
-	}
+    public void setInactiveReportId(int inactiveReportId) {
+        this.inactiveReportId = inactiveReportId;
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#asModel()
-	 */
-	@Override
-	public AbstractEventHandlerModel<?> asModel() {
-		return new ReportEventHandlerModel(this);
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#validate(com.serotonin.m2m2.i18n.ProcessResult)
-	 */
-	@Override
-	public void validate(ProcessResult response) {
-		super.validate(response);
-		
-		if(activeReportId != Common.NEW_ID){
-			ReportVO vo = ReportDao.getInstance().get(activeReportId);
-			if(vo == null)
-				response.addContextualMessage("activeReportId", "validate.invalidValue");
-		}
-		if(inactiveReportId != Common.NEW_ID){
-			ReportVO vo = ReportDao.getInstance().get(inactiveReportId);
-			if(vo == null)
-				response.addContextualMessage("inactiveReportId", "validate.invalidValue");
-		}
-		
-		if((inactiveReportId != Common.NEW_ID)&&(activeReportId != Common.NEW_ID)){
-			response.addContextualMessage("activeReportId", "validate.atLeast1");
-			response.addContextualMessage("inactiveReportId", "validate.atLeast1");
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#jsonRead(com.serotonin.json.JsonReader, com.serotonin.json.type.JsonObject)
-	 */
-	@Override
-	public void jsonRead(JsonReader reader, JsonObject jsonObject) throws JsonException {
-		super.jsonRead(reader, jsonObject);
-		String text = jsonObject.getString("activeReportXid");
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#createRuntime()
+     */
+    @Override
+    public EventHandlerRT<?> createRuntime() {
+        return new ReportEventHandlerRT(this);
+    }
+
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#asModel()
+     */
+    @Override
+    public AbstractEventHandlerModel<?> asModel() {
+        return new ReportEventHandlerModel(this);
+    }
+
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#validate(com.serotonin.m2m2.i18n.ProcessResult)
+     */
+    @Override
+    public void validate(ProcessResult response) {
+        super.validate(response);
+
+        if(activeReportId != Common.NEW_ID){
+            ReportVO vo = ReportDao.getInstance().get(activeReportId);
+            if(vo == null)
+                response.addContextualMessage("activeReportId", "validate.invalidValue");
+        }
+        if(inactiveReportId != Common.NEW_ID){
+            ReportVO vo = ReportDao.getInstance().get(inactiveReportId);
+            if(vo == null)
+                response.addContextualMessage("inactiveReportId", "validate.invalidValue");
+        }
+
+        if((inactiveReportId == Common.NEW_ID)&&(activeReportId == Common.NEW_ID)){
+            response.addContextualMessage("activeReportId", "validate.atLeast1");
+            response.addContextualMessage("inactiveReportId", "validate.atLeast1");
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#jsonRead(com.serotonin.json.JsonReader, com.serotonin.json.type.JsonObject)
+     */
+    @Override
+    public void jsonRead(JsonReader reader, JsonObject jsonObject) throws JsonException {
+        super.jsonRead(reader, jsonObject);
+        String text = jsonObject.getString("activeReportXid");
         if (text != null){
-        	ReportVO report = ReportDao.getInstance().getByXid(text);
-        	if(report != null)
-        		this.activeReportId = report.getId();
+            ReportVO report = ReportDao.getInstance().getByXid(text);
+            if(report != null)
+                this.activeReportId = report.getId();
         }else
-        	this.activeReportId = Common.NEW_ID;
-        
-		text = jsonObject.getString("inActiveReportXid");
-        if (text != null){
-        	ReportVO report = ReportDao.getInstance().getByXid(text);
-        	if(report != null)
-        		this.inactiveReportId = report.getId();
-        }else
-        	this.inactiveReportId = Common.NEW_ID;
-        	
-	}
+            this.activeReportId = Common.NEW_ID;
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#jsonWrite(com.serotonin.json.ObjectWriter)
-	 */
-	@Override
-	public void jsonWrite(ObjectWriter writer) throws IOException, JsonException {
-		super.jsonWrite(writer);
-		ReportVO report = ReportDao.getInstance().get(activeReportId);
-		if(report != null)
-			writer.writeEntry("activeReportXid", report.getXid());
-		report = ReportDao.getInstance().get(inactiveReportId);
-		if(report != null)
-			writer.writeEntry("inActiveReportXid", report.getXid());		
-	}
-	
+        text = jsonObject.getString("inActiveReportXid");
+        if (text != null){
+            ReportVO report = ReportDao.getInstance().getByXid(text);
+            if(report != null)
+                this.inactiveReportId = report.getId();
+        }else
+            this.inactiveReportId = Common.NEW_ID;
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.serotonin.m2m2.vo.event.AbstractEventHandlerVO#jsonWrite(com.serotonin.json.ObjectWriter)
+     */
+    @Override
+    public void jsonWrite(ObjectWriter writer) throws IOException, JsonException {
+        super.jsonWrite(writer);
+        ReportVO report = ReportDao.getInstance().get(activeReportId);
+        if(report != null)
+            writer.writeEntry("activeReportXid", report.getXid());
+        report = ReportDao.getInstance().get(inactiveReportId);
+        if(report != null)
+            writer.writeEntry("inActiveReportXid", report.getXid());
+    }
+
     //
     //
     // Serialization
@@ -141,8 +141,8 @@ public class ReportEventHandlerVO extends AbstractEventHandlerVO<ReportEventHand
 
         // Switch on the version of the class so that version changes can be elegantly handled.
         if (ver == 1) {
-        	activeReportId = in.readInt();
-        	inactiveReportId = in.readInt();
+            activeReportId = in.readInt();
+            inactiveReportId = in.readInt();
         }
     }
 }
