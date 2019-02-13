b05d7260c5d5dd51e3a187271823e1674eb81b43
Luis Güette <luis@infiniteautomation.com>
2019-02-13 14:09:50 -0400

    change cards

--- Maintenance Events/web-src/components/maintenanceEvents.js
+++ Maintenance Events/web-src/components/maintenanceEvents.js
@@ -1,11 +1,33 @@
 /**
- * @copyright 2018 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
+ * @copyright 2019 {@link http://infiniteautomation.com|Infinite Automation Systems, Inc.} All rights reserved.
  * @author Luis Güette
  */
 
 import componentTemplate from './maintenanceEvents.html';
 
+const $inject = Object.freeze(['$scope', '$mdMedia']);
+
+class maintenanceEventsController {
+
+    static get $inject() { return $inject; }
+    static get $$ngIsClass() { return true; }
+
+    constructor($scope, $mdMedia) {
+        this.$scope = $scope;
+        this.$mdMedia = $mdMedia;
+   }
+
+    $onInit() {
+        
+    }
+
+}
+
 export default {
     bindings: {},
+    require: {
+
+    },
+    controller: maintenanceEventsController,
     template: componentTemplate
 };
