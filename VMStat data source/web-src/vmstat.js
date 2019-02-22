3c1b7a0eefa5cc44f6cec723e3d3cd723ffe4836
Luis Güette <luis@infiniteautomation.com>
2019-02-22 14:57:52 -0400

    data source fields

--- VMStat data source/web-src/vmstat.js
+++ VMStat data source/web-src/vmstat.js
@@ -14,40 +14,45 @@ const vmStatDataSourceModule = angular.module('maVmStatDataSource', ['maUiApp'])
 .component('maVmStatDataPointEditor', vmStatDataPointEditor)
 .config(['maDataSourceProvider', 'maPointProvider', 'maUiMenuProvider', function(maDataSourceProvider, maPointProvider, maUiMenuProvider) {
     maDataSourceProvider.registerType({
-        type: 'SCRIPTING',
-        description: 'dsEdit.vmStat',
-        template: `<ma-scripting-data-source-editor data-source="$ctrl.dataSource"></ma-scripting-data-source-editor>`,
+        type: 'VMSTAT',
+        description: 'dsEdit.vmstat',
+        template: `<ma-vm-stat-data-source-editor data-source="$ctrl.dataSource"></ma-vm-stat-data-source-editor>`,
         polling: true,
         defaultDataSource: {
-            connectionDescription: '* * * * * ?',
-            context: [],
-            cronPattern: '* * * * * ?',
-            description: 'Scripting',
-            descriptionKey: 'dsEdit.vmStat',
+            connectionDescription: 'Poll seconds: 60',
+            descriptionKey: 'dsEdit.vmstat',
             editPermission: [],
-            enabled: true,
-            eventAlarmLevels: [],
-            executionDelaySeconds: 0,
-            historicalSetting: false,
-            logCount: 5,
-            logLevel: 'NONE',
-            logSize: 1,
-            modelType: 'SCRIPTING',
-            purgeSettings: {override: true, frequency: {periods: 1, type: 'YEARS'}},
-            script: '',
-            scriptPermissions: ['superadmin'],
-            updateEvent: 'UPDATE',
-            quantize: false,
-            useCron: false
+            enabled: false,
+            eventAlarmLevels: [
+                {
+                    description: 'Data source exception',
+                    descriptionKey: 'event.ds.dataSource',
+                    duplicateHandling: 'IGNORE_SAME_MESSAGE',
+                    eventType: null,
+                    level: 'URGENT'
+                },
+                {
+                    description: 'Point data parse exception',
+                    descriptionKey: 'event.ds.dataParse',
+                    duplicateHandling: 'IGNORE',
+                    eventType: null,
+                    level: 'URGENT'
+                }
+            ],
+            modelType: 'VMSTAT',
+            name: '',
+            outputScale: 'NONE',
+            pollSeconds: 60,
+            purgeSettings: {override: false, frequency: {periods: 1, type: 'YEARS'}}
         },
         defaultDataPoint: {
             dataSourceTypeName: 'SCRIPTING',
             pointLocator: {
-                dataType: 'BINARY',
-                modelType: 'PL.SCRIPTING',
+                attribute: 'CPU_ID',
+                dataType: 'NUMERIC',
+                modelType: 'PL.VMSTAT',
                 relinquishable: false,
-                settable: true,
-                varName: '',
+                settable: false
             }
         },
         bulkEditorColumns: []
@@ -55,21 +60,21 @@ const vmStatDataSourceModule = angular.module('maVmStatDataSource', ['maUiApp'])
 
     maPointProvider.registerType({
         type: 'SCRIPTING',
-        description: 'dsEdit.vmStatPoint',
-        template: `<ma-scripting-data-point-editor data-point="$ctrl.dataPoint"></ma-scripting-data-point-editor>`
+        description: 'dsEdit.vmstatPoint',
+        template: `<ma-vm-stat-data-point-editor data-point="$ctrl.dataPoint"></ma-vm-stat-data-point-editor>`
     });
 
     maUiMenuProvider.registerMenuItems([
         {
             name: 'ui.help.vmStatDataSource',
             url: '/vm-stat-data-source',
-            menuTr: 'dsEdit.vmStat',
+            menuTr: 'dsEdit.vmstat',
             template: dsHelpTemplate
         },
         {
             name: 'ui.help.vmStatDataPoint',
             url: '/vm-stat-data-point',
-            menuTr: 'dsEdit.vmStatPoint',
+            menuTr: 'dsEdit.vmstatPoint',
             template: dpHelpTemplate
         }
     ]);
