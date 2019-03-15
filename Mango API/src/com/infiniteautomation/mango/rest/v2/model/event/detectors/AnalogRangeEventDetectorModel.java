dcbeb5f6728b6dbe8398ee6a772fb4b2bcfbd0ec
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-03-15 12:18:08 -0600

    Fix model, add getters and setters

--- Mango API/src/com/infiniteautomation/mango/rest/v2/model/event/detectors/AnalogRangeEventDetectorModel.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/model/event/detectors/AnalogRangeEventDetectorModel.java
@@ -8,41 +8,65 @@ import com.serotonin.m2m2.module.definitions.event.detectors.AnalogRangeEventDet
 import com.serotonin.m2m2.vo.event.detector.AnalogRangeDetectorVO;
 
 /**
- * 
+ *
  * @author Terry Packer
  */
 public class AnalogRangeEventDetectorModel extends TimeoutDetectorModel<AnalogRangeDetectorVO>{
-	
+
     private double low;
     private double high;
     private boolean withinRange;
-    
-	public AnalogRangeEventDetectorModel(AnalogRangeDetectorVO data) {
-		fromVO(data);
-	}
-	
-	public AnalogRangeEventDetectorModel() { } 
-	
-	@Override
-	public void fromVO(AnalogRangeDetectorVO vo) {
-	    super.fromVO(vo);
-	    this.low = vo.getLow();
-	    this.high = vo.getHigh();
-	    this.withinRange = vo.isWithinRange();
-	}
-	
-	@Override
-	public AnalogRangeDetectorVO toVO() {
-	    AnalogRangeDetectorVO vo = super.toVO();
-	    vo.setLow(low);
-	    vo.setHigh(high);
-	    vo.setWithinRange(withinRange);
-	    return vo;
-	}
+
+    public AnalogRangeEventDetectorModel(AnalogRangeDetectorVO data) {
+        fromVO(data);
+    }
+
+    public AnalogRangeEventDetectorModel() { }
+
+    @Override
+    public void fromVO(AnalogRangeDetectorVO vo) {
+        super.fromVO(vo);
+        this.low = vo.getLow();
+        this.high = vo.getHigh();
+        this.withinRange = vo.isWithinRange();
+    }
+
+    @Override
+    public AnalogRangeDetectorVO toVO() {
+        AnalogRangeDetectorVO vo = super.toVO();
+        vo.setLow(low);
+        vo.setHigh(high);
+        vo.setWithinRange(withinRange);
+        return vo;
+    }
 
     @Override
     public String getDetectorType() {
         return AnalogRangeEventDetectorDefinition.TYPE_NAME;
+    }
+
+    public double getLow() {
+        return low;
+    }
+
+    public void setLow(double low) {
+        this.low = low;
+    }
+
+    public double getHigh() {
+        return high;
+    }
+
+    public void setHigh(double high) {
+        this.high = high;
+    }
+
+    public boolean isWithinRange() {
+        return withinRange;
+    }
+
+    public void setWithinRange(boolean withinRange) {
+        this.withinRange = withinRange;
     }
 
 }
