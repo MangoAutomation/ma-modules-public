f12b366060f9126413adc2623557d6b2ee3e2b4c
Craig <craig@infiniteautomation.com>
2019-03-18 14:03:04 +0200

    report settings help

--- Reports/web-src/reports.js
+++ Reports/web-src/reports.js
@@ -10,37 +10,50 @@ import reportSelect from './components/reportSelect/reportSelect';
 import reportService from './services/reportService';
 
 const reportsModule = angular.module('maReports', ['maUiApp'])
-.component('maReportsSettings', reportsSettings)
-.component('maReportSelect', reportSelect)
-.component('maReportEventHandlerEditor', reportEventHandlerEditor)
-.factory('maReport', reportService)
-.config(['maSystemSettingsProvider', 'maUiMenuProvider', '$injector', function(SystemSettingsProvider, maUiMenuProvider, $injector) {
-    maUiMenuProvider.registerMenuItems([{
-        name: 'ui.settings.system.reports',
-        url: '/reports',
-        template: '<ma-reports-settings></ma-reports-settings>',
-        menuTr: 'header.reports',
-        menuIcon: 'book',
-        menuHidden: true
-    }]);
-
-    SystemSettingsProvider.addAuditAlarmLevelSettings([
+    .component('maReportsSettings', reportsSettings)
+    .component('maReportSelect', reportSelect)
+    .component('maReportEventHandlerEditor', reportEventHandlerEditor)
+    .factory('maReport', reportService)
+    .config(['maSystemSettingsProvider', 'maUiMenuProvider', '$injector', function (SystemSettingsProvider, maUiMenuProvider, $injector) {
+        maUiMenuProvider.registerMenuItems([{
+            name: 'ui.settings.system.reports',
+            url: '/reports',
+            template: '<ma-reports-settings></ma-reports-settings>',
+            menuTr: 'header.reports',
+            menuIcon: 'book',
+            menuHidden: true,
+            params: {
+                helpPage: 'ui.help.reportSettings'
+            },
+        },
         {
-            key: 'auditEventAlarmLevel.REPORT',
-            translation: 'event.audit.report'
-        }
+            url: '/report-settings',
+            name: 'ui.help.reportSettings',
+            resolve: {
+                viewTemplate: function() {
+                    return import(/* webpackMode: "lazy", webpackChunkName: "ui.help" */
+                            './help/reportSettings.html');
+                }
+            },
+            menuTr: 'dox.reportSettings'
+        },
     ]);
 
-    if ($injector.has('maEventHandlerProvider')) {
-        const maEventHandlerProvider = $injector.get('maEventHandlerProvider');
-        if (typeof maEventHandlerProvider.registerEventHandlerType === 'function') {
-            maEventHandlerProvider.registerEventHandlerType({
-                type: 'REPORT',
-                description: 'reports.handler',
-                template: `<ma-report-event-handler-editor event-handler="$ctrl.eventHandler"></ma-report-event-handler-editor>`
-            });
+        SystemSettingsProvider.addAuditAlarmLevelSettings([{
+            key: 'auditEventAlarmLevel.REPORT',
+            translation: 'event.audit.report'
+        }]);
+
+        if ($injector.has('maEventHandlerProvider')) {
+            const maEventHandlerProvider = $injector.get('maEventHandlerProvider');
+            if (typeof maEventHandlerProvider.registerEventHandlerType === 'function') {
+                maEventHandlerProvider.registerEventHandlerType({
+                    type: 'REPORT',
+                    description: 'reports.handler',
+                    template: `<ma-report-event-handler-editor event-handler="$ctrl.eventHandler"></ma-report-event-handler-editor>`
+                });
+            }
         }
-    }
-}]);
+    }]);
 
 export default reportsModule;
