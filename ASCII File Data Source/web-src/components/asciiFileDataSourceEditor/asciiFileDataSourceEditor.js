13276d136bad0176f4399ecc7067ba392d756541
Luis Güette <luis@infiniteautomation.com>
2019-03-22 11:30:50 -0400

    check if file path exists

--- ASCII File Data Source/web-src/components/asciiFileDataSourceEditor/asciiFileDataSourceEditor.js
+++ ASCII File Data Source/web-src/components/asciiFileDataSourceEditor/asciiFileDataSourceEditor.js
@@ -5,24 +5,43 @@
 
 import componentTemplate from './asciiFileDataSourceEditor.html';
 
-const $inject = Object.freeze(['$scope', 'maAsciiFile']);
+const $inject = Object.freeze(['$scope', 'maAsciiFile', 'maDialogHelper']);
 
 class asciiFileDataSourceEditorController {
 
     static get $inject() { return $inject; }
     static get $$ngIsClass() { return true; }
 
-    constructor($scope, maAsciiFile) {
+    constructor($scope, maAsciiFile, maDialogHelper) {
         this.$scope = $scope;
         this.maAsciiFile = maAsciiFile;
+        this.maDialogHelper = maDialogHelper;
     }
 
     $onInit() {
         this.asciiFile = new this.maAsciiFile();
     }
 
-    validateFile(){
-        this.asciiFile.validate(this.dataSource.xid, this.file);
+    validateFile() {
+        this.asciiFile.validate(this.dataSource.xid, this.file).then(response => {
+            console.log(response);
+        });
+    }
+
+    validateFileExists() {
+        this.asciiFile.validateFileExists(this.dataSource.filePath).then(response => {
+            if (response.status === 200) {
+                this.maDialogHelper.toast(['dsEdit.file.canRead']);
+            }
+        }, error => {
+            if (error.status === 400) {
+                this.maDialogHelper.errorToast(['dsEdit.file.pathIsNotCanonical']);
+            }
+            
+            if (error.status === 403) {
+                this.maDialogHelper.errorToast(['dsEdit.file.cannotRead']);
+            }
+        });
     }
 
 }
