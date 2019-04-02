9bc366305d19137993354fa776d08df3b9d24897
terrypacker <terrypacker@users.noreply.github.com>
2019-04-02 10:15:13 -1000

    Expose factory method to create a text renderer from its model.

--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/dataPoint/textRenderer/TextRendererFactory.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/dataPoint/textRenderer/TextRendererFactory.java
@@ -85,52 +85,60 @@ public class TextRendererFactory {
 	 */
 	public static void updateDataPoint(DataPointVO vo,
 			BaseTextRendererModel<?> baseRendererModel) {
-		
-		if(baseRendererModel.getType().equals(AnalogRenderer.getDefinition().getName())){
-			AnalogTextRendererModel model = (AnalogTextRendererModel)baseRendererModel;
-			AnalogRenderer r = new AnalogRenderer();
-			r.setUseUnitAsSuffix(model.isUseUnitAsSuffix());
-			r.setFormat(model.getFormat());
-			r.setSuffix(model.getSuffix());
-			vo.setTextRenderer(r);
-		}else if(baseRendererModel.getType().equals(BinaryTextRenderer.getDefinition().getName())){
-			BinaryTextRendererModel model = (BinaryTextRendererModel)baseRendererModel;
-			BinaryTextRenderer btr = new BinaryTextRenderer();
-			btr.setOneColour(model.getOneColour());
-			btr.setOneLabel(model.getOneLabel());
-			btr.setZeroColour(model.getZeroColour());
-			btr.setZeroLabel(model.getZeroLabel());
-			vo.setTextRenderer(btr);
-		}else if(baseRendererModel.getType().equals(MultistateRenderer.getDefinition().getName())){
-			MultistateRenderer r = new MultistateRenderer();
-			MultistateTextRendererModel model = (MultistateTextRendererModel)baseRendererModel;
-			r.setMultistateValues(model.getMultistateValues());
-			vo.setTextRenderer(r);
-		}else if(baseRendererModel.getType().equals(NoneRenderer.getDefinition().getName())){
-			NoneRenderer r = new NoneRenderer();
-			vo.setTextRenderer(r);
-		}else if(baseRendererModel.getType().equals(PlainRenderer.getDefinition().getName())){
-			PlainTextRendererModel model = (PlainTextRendererModel)baseRendererModel;
-			PlainRenderer r = new PlainRenderer();
-			r.setUseUnitAsSuffix(model.isUseUnitAsSuffix());
-			r.setSuffix(model.getSuffix());
-			vo.setTextRenderer(r);
-		}else if(baseRendererModel.getType().equals(RangeRenderer.getDefinition().getName())){
-			RangeTextRendererModel model = (RangeTextRendererModel)baseRendererModel;
-			RangeRenderer r = new RangeRenderer();
-			r.setUseUnitAsSuffix(model.isUseUnitAsSuffix());
-			r.setFormat(model.getFormat());
-			r.setRangeValues(model.getRangeValues());
-			vo.setTextRenderer(r);
-		}else if(baseRendererModel.getType().equals(TimeRenderer.getDefinition().getName())){
-			TimeRenderer r = new TimeRenderer();
-			TimeTextRendererModel model = (TimeTextRendererModel)baseRendererModel;
-			r.setFormat(model.getFormat());
-			r.setConversionExponent(model.getConversionExponent());
-			vo.setTextRenderer(r);
-		}else{
-			throw new ShouldNeverHappenException("Unknown Text Renderer: " + baseRendererModel.getType());
-		}
+	    vo.setTextRenderer(createTextRenderer(baseRendererModel));
+	}
+	
+	/**
+	 * Create a renderer from a model
+	 * @param baseRendererModel
+	 * @return
+	 */
+	public static TextRenderer createTextRenderer(BaseTextRendererModel<?> baseRendererModel) {
+	       if(baseRendererModel.getType().equals(AnalogRenderer.getDefinition().getName())){
+	            AnalogTextRendererModel model = (AnalogTextRendererModel)baseRendererModel;
+	            AnalogRenderer r = new AnalogRenderer();
+	            r.setUseUnitAsSuffix(model.isUseUnitAsSuffix());
+	            r.setFormat(model.getFormat());
+	            r.setSuffix(model.getSuffix());
+	            return r;
+	        }else if(baseRendererModel.getType().equals(BinaryTextRenderer.getDefinition().getName())){
+	            BinaryTextRendererModel model = (BinaryTextRendererModel)baseRendererModel;
+	            BinaryTextRenderer btr = new BinaryTextRenderer();
+	            btr.setOneColour(model.getOneColour());
+	            btr.setOneLabel(model.getOneLabel());
+	            btr.setZeroColour(model.getZeroColour());
+	            btr.setZeroLabel(model.getZeroLabel());
+	            return btr;
+	        }else if(baseRendererModel.getType().equals(MultistateRenderer.getDefinition().getName())){
+	            MultistateRenderer r = new MultistateRenderer();
+	            MultistateTextRendererModel model = (MultistateTextRendererModel)baseRendererModel;
+	            r.setMultistateValues(model.getMultistateValues());
+	            return r;
+	        }else if(baseRendererModel.getType().equals(NoneRenderer.getDefinition().getName())){
+	            NoneRenderer r = new NoneRenderer();
+	            return r;
+	        }else if(baseRendererModel.getType().equals(PlainRenderer.getDefinition().getName())){
+	            PlainTextRendererModel model = (PlainTextRendererModel)baseRendererModel;
+	            PlainRenderer r = new PlainRenderer();
+	            r.setUseUnitAsSuffix(model.isUseUnitAsSuffix());
+	            r.setSuffix(model.getSuffix());
+	            return r;
+	        }else if(baseRendererModel.getType().equals(RangeRenderer.getDefinition().getName())){
+	            RangeTextRendererModel model = (RangeTextRendererModel)baseRendererModel;
+	            RangeRenderer r = new RangeRenderer();
+	            r.setUseUnitAsSuffix(model.isUseUnitAsSuffix());
+	            r.setFormat(model.getFormat());
+	            r.setRangeValues(model.getRangeValues());
+	            return r;
+	        }else if(baseRendererModel.getType().equals(TimeRenderer.getDefinition().getName())){
+	            TimeRenderer r = new TimeRenderer();
+	            TimeTextRendererModel model = (TimeTextRendererModel)baseRendererModel;
+	            r.setFormat(model.getFormat());
+	            r.setConversionExponent(model.getConversionExponent());
+	            return r;
+	        }else{
+	            throw new ShouldNeverHappenException("Unknown Text Renderer: " + baseRendererModel.getType());
+	        }
 	}
 
 }
