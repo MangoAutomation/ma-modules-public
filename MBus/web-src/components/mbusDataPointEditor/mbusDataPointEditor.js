cb1527f91dcff9421312b2c7581c7dc68d5576da
Luis Güette <luis@infiniteautomation.com>
2019-04-30 13:28:46 -0400

    update difCode

--- MBus/web-src/components/mbusDataPointEditor/mbusDataPointEditor.js
+++ MBus/web-src/components/mbusDataPointEditor/mbusDataPointEditor.js
@@ -20,6 +20,31 @@ class mbusDataPointEditorController {
         
     }
 
+    difCodeChange() {
+        const numericDataTypes = [
+            '12 digit BCD',
+            '16 bit integer',
+            '24 bit integer',
+            '2 digit BCD',
+            '32 bit integer',
+            '32 bit real',
+            '48 bit integer',
+            '4 digit BCD',
+            '64 bit integer',
+            '6 digit BCD',
+            '8 bit integer',
+            '8 digit BCD',
+        ];
+
+        if (numericDataTypes.includes(this.dataPoint.pointLocator.difCode)) {
+            this.dataPoint.dataType = 'NUMERIC';
+        } else if (this.dataPoint.pointLocator.difCode === 'variable length') {
+            this.dataPoint.dataType = 'ALPHANUMERIC';
+        } else {
+            this.dataPoint.dataType = 'UNKNOWN';
+        }
+    }
+
 }
 
 export default {
