9e73f7ac94da776a69c87e925ae776b9b1ef15a8
Jared Wiltshire <jazdw@users.noreply.github.com>
2019-06-14 12:33:07 -0600

    Fix permission definitions

--- Graphical views/src/com/serotonin/m2m2/gviews/GraphicalViewPermissionDefinition.java
+++ Graphical views/src/com/serotonin/m2m2/gviews/GraphicalViewPermissionDefinition.java
@@ -4,10 +4,11 @@
  */
 package com.serotonin.m2m2.gviews;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.serotonin.m2m2.module.PermissionDefinition;
+import com.serotonin.m2m2.vo.permission.Permissions;
 
 /**
  * @author Terry Packer
@@ -15,31 +16,20 @@ import com.serotonin.m2m2.module.PermissionDefinition;
  */
 public class GraphicalViewPermissionDefinition extends PermissionDefinition{
 
-	public static final String PERMISSION = "graphical.view";
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getPermissionKey()
-	 */
-	@Override
-	public String getPermissionKey() {
-		return "graphic.permission.view";
-	}
+    public static final String PERMISSION = "graphical.view";
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getPermissionTypeName()
-	 */
-	@Override
-	public String getPermissionTypeName() {
-		return PERMISSION;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getDefaultGroups()
-	 */
-	@Override
-	public List<String> getDefaultGroups() {
-		List<String> groups = new ArrayList<String>();
-		groups.add("user");
-		return groups;
-	}
+    @Override
+    public String getPermissionKey() {
+        return "graphic.permission.view";
+    }
+
+    @Override
+    public String getPermissionTypeName() {
+        return PERMISSION;
+    }
+
+    @Override
+    public List<String> getDefaultGroups() {
+        return Collections.singletonList(Permissions.USER_DEFAULT);
+    }
 }
--- Log4J reset/src/com/serotonin/m2m2/log4jreset/Log4JResetActionPermissionDefinition.java
+++ Log4J reset/src/com/serotonin/m2m2/log4jreset/Log4JResetActionPermissionDefinition.java
@@ -4,40 +4,24 @@
  */
 package com.serotonin.m2m2.log4jreset;
 
-import java.util.List;
-
 import com.serotonin.m2m2.module.PermissionDefinition;
 
 /**
- * 
+ *
  * @author Terry Packer
  */
 public class Log4JResetActionPermissionDefinition extends PermissionDefinition {
 
-	public static final String PERMISSION = "action.log4jUtil";
+    public static final String PERMISSION = "action.log4jUtil";
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getPermissionKey()
-	 */
-	@Override
-	public String getPermissionKey() {
-		return "log4JReset.settings.header";
-	}
+    @Override
+    public String getPermissionKey() {
+        return "log4JReset.settings.header";
+    }
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getPermissionTypeName()
-	 */
-	@Override
-	public String getPermissionTypeName() {
-		return PERMISSION;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.module.PermissionDefinition#getDefaultGroups()
-	 */
-	@Override
-	public List<String> getDefaultGroups() {
-		return null;
-	}
+    @Override
+    public String getPermissionTypeName() {
+        return PERMISSION;
+    }
 
 }
