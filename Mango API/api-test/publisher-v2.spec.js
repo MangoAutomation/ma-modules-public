ab9ed920b0772078fc3e45cb6c88bdfecf9c229e
terrypacker <terrypacker@users.noreply.github.com>
2019-07-02 13:21:23 -1000

    Add publisher-v2 websocket api tests.

---
+++ Mango API/api-test/publisher-v2.spec.js
@@ -0,0 +1,277 @@
+/**
+ * Copyright 2017 Infinite Automation Systems Inc.
+ * http://infiniteautomation.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const config = require('@infinite-automation/mango-client/test/setup');
+
+describe('Publishers v2 service', () => {
+    before('Login', config.login);
+
+    const httpPublisher = {
+            enabled : false,
+            dateFormat : "DATE_FORMAT_BASIC",
+            url : "http://www.terrypacker.com",
+            usePost : false,
+            useJSON : false,
+            staticHeaders : [ {
+              key : "test",
+              value : "value"
+            } ],
+            staticParameters : [ {
+              key : "test",
+              value : "value"
+            } ],
+            raiseResultWarning : true,
+            points : [],
+            publishType : "ALL",
+            cacheWarningSize : 100,
+            cacheDiscardSize : 1000,
+            sendSnapshot : false,
+            snapshotSendPeriodType : "MINUTES",
+            snapshotSendPeriods : 5,
+            validationMessages : [ ],
+            name : "HTTP",
+            modelType : "HTTP_SENDER"
+    };
+    
+    before('Gets websocket notifications for publisher create', function() {
+
+        let ws;
+        const subscription = {
+            eventTypes: ['add', 'delete', 'update']
+        };
+        
+        const socketOpenDeferred = config.defer();
+        const listUpdatedDeferred = config.defer();
+
+        return Promise.resolve().then(() => {
+            ws = client.openWebSocket({
+                path: '/rest/v2/websocket/publishers'
+            });
+
+            ws.on('open', () => {
+                socketOpenDeferred.resolve();
+            });
+            
+            ws.on('error', error => {
+                const msg = new Error(`WebSocket error, error: ${error}`);
+                socketOpenDeferred.reject(msg);
+                listUpdatedDeferred.reject(msg);
+            });
+            
+            ws.on('close', (code, reason) => {
+                const msg = new Error(`WebSocket closed, code: ${code}, reason: ${reason}`);
+                socketOpenDeferred.reject(msg);
+                listUpdatedDeferred.reject(msg);
+            });
+
+            ws.on('message', msgStr => {
+                try{
+                    assert.isString(msgStr);
+                    const msg = JSON.parse(msgStr);
+                    assert.strictEqual(msg.status, 'OK');
+                    assert.strictEqual(msg.payload.action, 'add');
+                    assert.strictEqual(msg.payload.object.name, httpPublisher.name);
+                    listUpdatedDeferred.resolve();   
+                }catch(e){
+                    listUpdatedDeferred.reject(e);
+                }
+            });
+
+            return socketOpenDeferred.promise;
+        }).then(() => {
+            const send = config.defer();
+            ws.send(JSON.stringify(subscription), error => {
+                if (error != null) {
+                    send.reject(error);
+                } else {
+                    send.resolve();
+                }
+            });
+            return send.promise;
+            
+        }).then(() => config.delay(1000)).then(() => {
+            //TODO Fix DaoNotificationWebSocketHandler so we can remove this delay, only required for cold start
+            return client.restRequest({
+                path: `/rest/v2/publishers-v2`,
+                method: 'POST',
+                data: httpPublisher
+            }).then(response => {
+                assert.isNumber(response.data.id);
+                httpPublisher.id = response.data.id;
+                httpPublisher.xid = response.data.xid;
+            });
+        }).then(() => listUpdatedDeferred.promise).then((r)=>{
+            ws.close();
+            return r;
+        },e => {
+            ws.close();
+            return Promise.reject(e);
+        });
+    });
+
+    it('Gets websocket notifications for publisher update', function() {
+
+        let ws;
+        const subscription = {
+            eventTypes: ['add', 'delete', 'update']
+        };
+        
+        const socketOpenDeferred = config.defer();
+        const listUpdatedDeferred = config.defer();
+
+        return Promise.resolve().then(() => {
+            ws = client.openWebSocket({
+                path: '/rest/v2/websocket/publishers'
+            });
+
+            ws.on('open', () => {
+                socketOpenDeferred.resolve();
+            });
+            
+            ws.on('error', error => {
+                const msg = new Error(`WebSocket error, error: ${error}`);
+                socketOpenDeferred.reject(msg);
+                listUpdatedDeferred.reject(msg);
+            });
+            
+            ws.on('close', (code, reason) => {
+                const msg = new Error(`WebSocket closed, code: ${code}, reason: ${reason}`);
+                socketOpenDeferred.reject(msg);
+                listUpdatedDeferred.reject(msg);
+            });
+
+            ws.on('message', msgStr => {
+                try{
+                    assert.isString(msgStr);
+                    const msg = JSON.parse(msgStr);
+                    assert.strictEqual(msg.status, 'OK');
+                    assert.strictEqual(msg.payload.action, 'update');
+                    assert.strictEqual(msg.payload.object.id, httpPublisher.id);
+                    listUpdatedDeferred.resolve();   
+                }catch(e){
+                    listUpdatedDeferred.reject(e);
+                }
+            });
+
+            return socketOpenDeferred.promise;
+        }).then(() => {
+            const send = config.defer();
+            ws.send(JSON.stringify(subscription), error => {
+                if (error != null) {
+                    send.reject(error);
+                } else {
+                    send.resolve();
+                }
+            });
+            return send.promise;
+            
+        }).then(() => config.delay(1000)).then(() => {
+            //TODO Fix DaoNotificationWebSocketHandler so we can remove this delay, only required for cold start
+            httpPublisher.name = "new name";
+            return client.restRequest({
+                path: `/rest/v2/publishers-v2/${httpPublisher.xid}`,
+                method: 'PUT',
+                data: httpPublisher
+            }).then(response => {
+                assert.isNumber(response.data.id);
+                assert.strictEqual(response.data.name, httpPublisher.name);
+            });
+        }).then(() => listUpdatedDeferred.promise).then((r)=>{
+            ws.close();
+            return r;
+        },e => {
+            ws.close();
+            return Promise.reject(e);
+        });
+    });
+    
+    after('Gets websocket notifications for publisher delete', function() {
+
+        let ws;
+        const subscription = {
+            eventTypes: ['add', 'delete', 'update']
+        };
+        
+        const socketOpenDeferred = config.defer();
+        const listUpdatedDeferred = config.defer();
+
+        return Promise.resolve().then(() => {
+            ws = client.openWebSocket({
+                path: '/rest/v2/websocket/publishers'
+            });
+
+            ws.on('open', () => {
+                socketOpenDeferred.resolve();
+            });
+            
+            ws.on('error', error => {
+                const msg = new Error(`WebSocket error, error: ${error}`);
+                socketOpenDeferred.reject(msg);
+                listUpdatedDeferred.reject(msg);
+            });
+            
+            ws.on('close', (code, reason) => {
+                const msg = new Error(`WebSocket closed, code: ${code}, reason: ${reason}`);
+                socketOpenDeferred.reject(msg);
+                listUpdatedDeferred.reject(msg);
+            });
+
+            ws.on('message', msgStr => {
+                try{
+                    assert.isString(msgStr);
+                    const msg = JSON.parse(msgStr);
+                    assert.strictEqual(msg.status, 'OK');
+                    assert.strictEqual(msg.payload.action, 'delete');
+                    assert.strictEqual(msg.payload.object.id, httpPublisher.id);
+                    listUpdatedDeferred.resolve();   
+                }catch(e){
+                    listUpdatedDeferred.reject(e);
+                }
+            });
+
+            return socketOpenDeferred.promise;
+        }).then(() => {
+            const send = config.defer();
+            ws.send(JSON.stringify(subscription), error => {
+                if (error != null) {
+                    send.reject(error);
+                } else {
+                    send.resolve();
+                }
+            });
+            return send.promise;
+            
+        }).then(() => config.delay(1000)).then(() => {
+            //TODO Fix DaoNotificationWebSocketHandler so we can remove this delay, only required for cold start
+            httpPublisher.name = "new name";
+            return client.restRequest({
+                path: `/rest/v2/publishers-v2/${httpPublisher.xid}`,
+                method: 'DELETE',
+                data: {}
+            }).then(response => {
+                assert.isNumber(response.data.id);
+                assert.strictEqual(response.data.name, httpPublisher.name);
+            });
+        }).then(() => listUpdatedDeferred.promise).then((r)=>{
+            ws.close();
+            return r;
+        },e => {
+            ws.close();
+            return Promise.reject(e);
+        });
+    });
+});
