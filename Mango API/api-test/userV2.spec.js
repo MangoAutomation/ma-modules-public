4924de0b35f6fd0602f4ca512b24d66e5620369f
terrypacker <terrypacker@users.noreply.github.com>
2019-09-24 10:29:39 +0100

    Finish to-do user v2 REST tests

--- Mango API/api-test/userV2.spec.js
+++ Mango API/api-test/userV2.spec.js
@@ -57,6 +57,19 @@ function userV2Factory(client) {
                 return (new UserV2()).updateSelf(response);
             });
         }
+
+        static lockPassword(username) {
+            return client.restRequest({
+               path: this.baseUrl + '/' + encodeURIComponent(`${username}`) + '/lock-password',
+               method: 'PUT'
+            });
+        }
+        
+        static patch(id, values) {
+            var options = {};
+            options[this.idProperty] = id;
+            return (new this(options).patch(values));
+        }
         
         patch(values) {
             return client.restRequest({
@@ -68,10 +81,25 @@ function userV2Factory(client) {
             });
         }
         
-        lockPassword(username) {
+        updateHomeUrl(url) {
             return client.restRequest({
-               path: this.constructor.baseUrl + '/' + encodeURIComponent(`${username}`) + '/lock-password',
-               method: 'PUT'
+                path: this.constructor.baseUrl + '/' + encodeURIComponent(this[this.constructor.idProperty]) + '/homepage?url=' + encodeURIComponent(url),
+                method: 'PUT'
+            }).then(response => {
+                return this.updateSelf(response);
+            });
+        }
+        
+        toggleMuted(muted) {
+            let path = this.constructor.baseUrl + '/' + encodeURIComponent(this[this.constructor.idProperty])  + '/mute';
+            if(muted) {
+                path += '?mute=' + muted;
+            }
+            return client.restRequest({
+                path: path,
+                method: 'PUT'
+            }).then(response => {
+                return this.updateSelf(response);
             });
         }
     };
@@ -86,49 +114,49 @@ describe('User V2 endpoint tests', function() {
             this.timeout(localConfig.loginRetries * localConfig.loginRetryDelay + 5000);
             return LocalUser.login(localConfig.username, localConfig.password, localConfig.loginRetries, localConfig.loginRetryDelay);
         };
-        //TODO Remove?
-        this.UserV2 = userV2Factory(client);
         this.clients = {};
         this.configs = {};
     });
     
     beforeEach('Create test User', function() {
         const username = uuidV4();
-        this.testUserPassword = uuidV4();
-        this.testUser = new UserV2({
-            name: 'name',
-            username: username,
-            password: this.testUserPassword,
-            email: `${username}@example.com`,
-            phone: '808-888-8888',
-            disabled: false,
-            locale: '',
-            homeUrl: 'www.google.com',
-            receiveAlarmEmails: 'IGNORE',
-            receiveOwnAuditEvents: false,
-            muted: false,
-            permissions: ['testuser', 'test'],
-            sessionExpirationOverride: true,
-            sessionExpirationPeriod: {
-                periods: 1,
-                type: 'SECONDS'
-            },
-            organization: 'Infinite Automation Systems',
-            organizationalRole: 'test engineer',
-            data: {
-                stringField: 'some random string',
-                numberField: 123,
-                booleanField: true
-            }
-                
-        });
+        const testUserPassword = uuidV4();
+        this.testUserSettings = {
+                name: 'name',
+                username: username,
+                password: testUserPassword,
+                email: `${username}@example.com`,
+                phone: '808-888-8888',
+                disabled: false,
+                locale: '',
+                homeUrl: 'www.google.com',
+                receiveAlarmEmails: 'IGNORE',
+                receiveOwnAuditEvents: false,
+                muted: false,
+                permissions: ['testuser', 'test'],
+                sessionExpirationOverride: true,
+                sessionExpirationPeriod: {
+                    periods: 1,
+                    type: 'SECONDS'
+                },
+                organization: 'Infinite Automation Systems',
+                organizationalRole: 'test engineer',
+                data: {
+                    stringField: 'some random string',
+                    numberField: 123,
+                    booleanField: true
+                }
+                    
+            };
+        
+        const testUser = new UserV2(this.testUserSettings);
         
         //Save user and setup a session user for it
-        return this.testUser.save().then(user => {
+        return testUser.save().then(user => {
             assert.equal(user.username, username);
             this.configs.user = Object.assign({}, config, {
                     username: username,
-                    password: this.testUserPassword
+                    password: testUserPassword
             });
             this.clients.user = new MangoClient(this.configs.user);
             this.clients.user.User = userV2Factory(this.clients.user);
@@ -139,34 +167,35 @@ describe('User V2 endpoint tests', function() {
     
     beforeEach('Create test admin User', function() {
         const username = uuidV4();
-        this.testAdminUserPassword = uuidV4();
-        this.testAdminUser = new UserV2({
-            name: 'admin name',
-            username: username,
-            password: this.testAdminUserPassword,
-            email: `${username}@example.com`,
-            phone: '808-888-8888',
-            disabled: false,
-            locale: '',
-            homeUrl: 'www.google.com',
-            receiveAlarmEmails: 'IGNORE',
-            receiveOwnAuditEvents: false,
-            muted: false,
-            permissions: ['superadmin'],
-            sessionExpirationOverride: true,
-            sessionExpirationPeriod: {
-                periods: 1,
-                type: 'HOURS'
-            },
-            organization: 'Infinite Automation Systems'
-        });
+        const testAdminUserPassword = uuidV4();
+        this.adminUserSettings = {
+                name: 'admin name',
+                username: username,
+                password: testAdminUserPassword,
+                email: `${username}@example.com`,
+                phone: '808-888-8888',
+                disabled: false,
+                locale: '',
+                homeUrl: 'www.google.com',
+                receiveAlarmEmails: 'IGNORE',
+                receiveOwnAuditEvents: false,
+                muted: false,
+                permissions: ['superadmin'],
+                sessionExpirationOverride: true,
+                sessionExpirationPeriod: {
+                    periods: 1,
+                    type: 'HOURS'
+                },
+                organization: 'Infinite Automation Systems'
+            };
+        const testAdminUser = new UserV2(this.adminUserSettings);
         
         //Save user and setup a session user for it
-        return this.testAdminUser.save().then(user => {
+        return testAdminUser.save().then(user => {
             assert.equal(user.username, username);
             this.configs.admin = Object.assign({}, config, {
                     username: username,
-                    password: this.testAdminUserPassword
+                    password: testAdminUserPassword
             });
             this.clients.admin = new MangoClient(this.configs.admin);
             this.clients.admin.User = userV2Factory(this.clients.admin);
@@ -176,14 +205,14 @@ describe('User V2 endpoint tests', function() {
     });
     
     afterEach('Deletes the test user', function() {
-        return this.testUser.delete().then(user => {
-            assert.equal(user.username, this.testUser.username);
+        return client.User.delete(this.clients.user.user.username).then(user => {
+            assert.equal(user.username, this.clients.user.user.username);
         });
     });
     
     afterEach('Deletes the test admin user', function() {
-        return this.testAdminUser.delete().then(user => {
-            assert.equal(user.username, this.testAdminUser.username);
+        return client.User.delete(this.clients.admin.user.username).then(user => {
+            assert.equal(user.username, this.clients.admin.user.username);
         });
     });
     
@@ -193,12 +222,12 @@ describe('User V2 endpoint tests', function() {
     
     it('Gets self as non admin', function() {
         return this.clients.user.user.get().then(user => {
-            assert.equal(user.username, this.testUser.username);
-            assert.strictEqual(user.organization, this.testUser.organization);
-            assert.strictEqual(user.organizationalRole, this.testUser.organizationalRole);
-            assert.strictEqual(user.data.stringField, this.testUser.data.stringField);
-            assert.strictEqual(user.data.numberField, this.testUser.data.numberField);
-            assert.strictEqual(user.data.booleanField, this.testUser.data.booleanField);
+            assert.equal(user.username, this.testUserSettings.username);
+            assert.strictEqual(user.organization, this.testUserSettings.organization);
+            assert.strictEqual(user.organizationalRole, this.testUserSettings.organizationalRole);
+            assert.strictEqual(user.data.stringField, this.testUserSettings.data.stringField);
+            assert.strictEqual(user.data.numberField, this.testUserSettings.data.numberField);
+            assert.strictEqual(user.data.booleanField, this.testUserSettings.data.booleanField);
             assert.isNull(user.emailVerified);
             assert.isString(user.created);
             assert.isAbove(new Date(user.created).valueOf(), 0);
@@ -230,7 +259,7 @@ describe('User V2 endpoint tests', function() {
     });
     
     it('Can lock other users password as admin ', function() {
-        return this.clients.admin.user.lockPassword(this.clients.user.user.username).then(response => {
+        return this.clients.admin.User.lockPassword(this.clients.user.user.username).then(response => {
             assert.strictEqual(response.status, 200);
             return this.login(this.configs.user, this.clients.user.User).then(response => {
                 throw new Error('Should not have logged in');
@@ -241,7 +270,7 @@ describe('User V2 endpoint tests', function() {
     });
     
     it('Can\'t lock other users password as user ', function() {
-        return this.clients.user.user.lockPassword(this.clients.admin.user.username).then(response => {
+        return this.clients.user.User.lockPassword(this.clients.admin.user.username).then(response => {
             throw new Error('Should not have locked password');
         }, error => {
             assert.strictEqual(error.status, 403); 
@@ -249,7 +278,7 @@ describe('User V2 endpoint tests', function() {
     });
     
     it('Can\'t lock own password', function() {
-        return this.clients.admin.user.lockPassword(this.clients.admin.user.username).then(response => {
+        return this.clients.admin.User.lockPassword(this.clients.admin.user.username).then(response => {
             throw new Error('Should not have locked password');
         }, error => {
             assert.strictEqual(error.status, 403); 
@@ -322,34 +351,33 @@ describe('User V2 endpoint tests', function() {
             username: 'Iamnew'
         }).then(user => {
             assert.strictEqual(user.username, 'Iamnew');
-            this.testUser.username = 'Iamnew';
         });
     });
     
     it('Queries to match all user permissions as admin', function() {
-        return this.clients.admin.User.query('permissionsContainsAll(permissions,testuser,test)').then(result => {
+        return this.clients.admin.User.query(`permissionsContainsAll(permissions,testuser,test)&username=${this.clients.user.user.username}`).then(result => {
             assert.equal(result.total, 1);
-            assert.equal(result[0].username, this.testUser.username);
+            assert.equal(result[0].username, this.testUserSettings.username);
         });
     });
     
     it('Queries to match one user permission as admin', function() {
-        return this.clients.admin.User.query('permissionsContainsAny(permissions,testuser)').then(result => {
+        return this.clients.admin.User.query(`permissionsContainsAny(permissions,testuser)&username=${this.clients.user.user.username}`).then(result => {
             assert.equal(result.total, 1);
-            assert.equal(result[0].username, this.testUser.username);
+            assert.equal(result[0].username, this.testUserSettings.username);
         });
     });
     
     it('Queries for disabled users as admin', function() {
         //First disable the test user
-        return this.testUser.patch({
+        return this.clients.admin.User.patch(this.clients.user.user.username, {
             disabled: true
         }).then(user => {
-            assert.equal(user.username, this.testUser.username);
+            assert.equal(user.username, this.testUserSettings.username);
             assert.equal(user.disabled, true);
             return this.clients.admin.User.query('disabled=true').then(result => {
                 assert.equal(result.total, 1);
-                assert.equal(result[0].username, this.testUser.username);
+                assert.equal(result[0].username, this.testUserSettings.username);
             });
         });
     });
@@ -358,7 +386,7 @@ describe('User V2 endpoint tests', function() {
         return this.clients.user.user.patch({
             name: 'test user'
         }).then(user => {
-            assert.equal(user.username, this.testUser.username);
+            assert.equal(user.username, this.testUserSettings.username);
             assert.equal(user.name, 'test user');
         });
     });
@@ -366,7 +394,7 @@ describe('User V2 endpoint tests', function() {
     it('User session timeout override expires session', function() {
         this.timeout(5000);
         const loginClient = new MangoClient(config);
-        return loginClient.User.login(this.testUser.username, this.testUserPassword).then(() => {
+        return loginClient.User.login(this.testUserSettings.username, this.testUserSettings.password).then(() => {
             return config.delay(2000);
         }).then(() => {
             return loginClient.User.current().then(response => {
@@ -395,18 +423,91 @@ describe('User V2 endpoint tests', function() {
             });
         });
     });
-    it.skip('Can\'t manually set the emailVerified or created property when creating user');
-    it.skip('Can\'t manually update the emailVerified or created property');
-    it.skip('Can\'t set email to a used email address');
+    
+    it('Can\'t manually set the emailVerified or created property when creating user', function() {
+    
+        const username = uuidV4();
+        const dateCreated = new Date(100000).toISOString();
+        const emailVerified = new Date(100000).toISOString();
+        
+        const invalidUser = new UserV2({
+            name: 'name',
+            username: username,
+            password: uuidV4(),
+            email: `${username}@example.com`,
+            emailVerified: emailVerified,
+            created: dateCreated,
+            phone: '808-888-8888',
+            disabled: false,
+            locale: '',
+            homeUrl: 'www.google.com',
+            receiveAlarmEmails: 'IGNORE',
+            receiveOwnAuditEvents: false,
+            muted: false,
+            permissions: ['testuser', 'test'],
+            sessionExpirationOverride: true,
+            sessionExpirationPeriod: {
+                periods: 1,
+                type: 'SECONDS'
+            },
+            organization: 'Infinite Automation Systems',
+            organizationalRole: 'test engineer',
+            data: {
+                stringField: 'some random string',
+                numberField: 123,
+                booleanField: true
+            }
+        });
+        
+        return invalidUser.save().then(user => {
+            assert.notStrictEqual(user.created, dateCreated);
+            assert.notStrictEqual(user.emailVerified, emailVerified);
+        });
+        
+    });
+    
+    it('Can\'t manually update the emailVerified or created property', function() {
 
-    it.skip('Can update own home url as non admin');
-    it.skip('Can toggle audio mute setting as non admin', function() {
-        return client.restRequest({
-            path: `/rest/v2/users/${this.testUser.username}/mute`,
-            method: 'PUT'
-        }).then(response => {
-            assert.equal(response.data.mute, !this.testUser.mute);
+        const dateCreated = new Date(100000).toISOString();
+        const emailVerified = new Date(100000).toISOString();
+        this.clients.admin.user.created = dateCreated;
+        this.clients.admin.user.emailVerified = emailVerified;
+        
+        return this.clients.admin.user.save().then(user => {
+            assert.notStrictEqual(user.created, dateCreated);
+            assert.notStrictEqual(user.emailVerified, emailVerified);
+        });
+        
+    });
+    
+    it('Can\'t set email to a used email address', function() {
+        this.clients.admin.user.email = this.clients.user.user.email;
+        return this.clients.admin.user.save().then(user => {
+            throw new Error('Should not have saved user');
+        }, error => {
+            assert.strictEqual(error.status, 422);
+            assert.strictEqual(error.data.result.messages.length, 1);
+            assert.strictEqual(error.data.result.messages[0].property, 'email');
         });
     });
 
+    it('Can update own home url as non admin', function() {
+        return this.clients.user.user.updateHomeUrl('/test-url').then(user => {
+            assert.strictEqual(user.homeUrl, '/test-url');
+        });
+    });
+    
+    it('Can set audio mute setting as non admin', function() {
+        return this.clients.user.user.toggleMuted(true).then(user => {
+            assert.strictEqual(user.muted, true);
+        });
+    });
+
+    it('Can toggle audio mute setting as non admin', function() {
+        assert.strictEqual(this.clients.user.user.muted, false);
+        return this.clients.user.user.toggleMuted().then(user => {
+            assert.strictEqual(user.muted, true);
+        });
+    });
+    
 });
