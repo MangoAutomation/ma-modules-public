280c3141397d8dd5501996c1117325929427a06e
terrypacker <terrypacker@users.noreply.github.com>
2019-12-20 09:04:23 -1000

    Watchlist - remove legacy UI

--- Watchlist/src/com/serotonin/m2m2/watchlist/AuditEvent.java
+++ Watchlist/src/com/serotonin/m2m2/watchlist/AuditEvent.java
@@ -4,10 +4,7 @@
  */
 package com.serotonin.m2m2.watchlist;
 
-import com.serotonin.m2m2.Constants;
-import com.serotonin.m2m2.i18n.Translations;
 import com.serotonin.m2m2.module.AuditEventTypeDefinition;
-import com.serotonin.web.taglib.Functions;
 
 /**
  * @author Terry Packer
@@ -26,17 +23,4 @@ public class AuditEvent extends AuditEventTypeDefinition {
         return "event.audit.watchlist";
     }
 
-    @Override
-    public String getEventListLink(int ref1, int ref2, Translations translations) {
-        String alt = Functions.quotEncode(translations.translate("events.editPointLink"));
-        StringBuilder sb = new StringBuilder();
-        sb.append("<a href='watchlist.shtm?wlid=");
-        sb.append(ref1);
-        sb.append("'><img src='");
-        sb.append("/" + Constants.DIR_MODULES + "/" + getModule().getName()).append("/web/link.png");
-        sb.append("' alt='").append(alt);
-        sb.append("' title='").append(alt);
-        sb.append("'/></a>");
-        return sb.toString();
-    }
 }
--- Watchlist/web/mobile/logout.jsp
+++
@@ -1,6 +0,0 @@
-<%--
-    Copyright (C) 2014 Infinite Automation Systems Inc. All rights reserved.
-    @author Matthew Lohbihler
---%>
-<%@ include file="/WEB-INF/jsp/include/tech.jsp" %>
-<c:redirect url="/mobile/login.htm"/>
