5b1e7f3a31d9110361c3d348d9685c2709c85de6
terrypacker <terrypacker@users.noreply.github.com>
2019-12-20 06:51:49 -1000

    ASCII File - Remove legacy UI support

--- ASCII File Data Source/src/com/infiniteautomation/asciifile/AsciiFileDataSourceDefinition.java
+++ ASCII File Data Source/src/com/infiniteautomation/asciifile/AsciiFileDataSourceDefinition.java
@@ -1,7 +1,6 @@
 package com.infiniteautomation.asciifile;
 
 import com.infiniteautomation.asciifile.vo.AsciiFileDataSourceVO;
-import com.infiniteautomation.asciifile.web.AsciiFileEditDwr;
 import com.serotonin.m2m2.module.DataSourceDefinition;
 import com.serotonin.m2m2.vo.dataSource.DataSourceVO;
 
@@ -26,15 +25,5 @@ public class AsciiFileDataSourceDefinition extends DataSourceDefinition{
 	@Override
 	protected DataSourceVO<?> createDataSourceVO() {
 		return new AsciiFileDataSourceVO();
-	}
-
-	@Override
-	public String getEditPagePath() {
-		return "web/editFile.jsp";
-	}
-
-	@Override
-	public Class<?> getDwrClass() {
-		return AsciiFileEditDwr.class;
 	}
 }
