f9b9cc4fa4571292d77a61a85518db2471fc9ad9
terrypacker <terrypacker@users.noreply.github.com>
2020-01-08 14:27:32 -1000

    Fix User events controller

--- Mango API/src/com/infiniteautomation/mango/rest/v2/UserEventsV2Controller.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/UserEventsV2Controller.java
@@ -21,27 +21,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infiniteautomation.mango.db.query.QueryAttribute;
-import com.infiniteautomation.mango.db.query.TableModel;
 import com.infiniteautomation.mango.db.query.pojo.RQLToObjectListQuery;
+import com.infiniteautomation.mango.rest.v2.model.ListWithTotal;
+import com.infiniteautomation.mango.rest.v2.model.query.TableModel;
 import com.infiniteautomation.mango.util.RQLUtils;
 import com.serotonin.m2m2.Common;
 import com.serotonin.m2m2.rt.event.EventInstance;
 import com.serotonin.m2m2.vo.User;
-import com.serotonin.m2m2.web.mvc.rest.v1.model.PageQueryResultModel;
 import com.serotonin.m2m2.web.mvc.rest.v1.model.events.EventInstanceModel;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
 import net.jazdw.rql.parser.ASTNode;
 
 /**
- * REST Endpoints for User Event Cache access, All un-acknowledged user events are held in the cache 
+ * REST Endpoints for User Event Cache access, All un-acknowledged user events are held in the cache
  * on a per user basis.
- * 
- * Note that querying an object is different to querying the database.  If a property is queried on 
- * that does not exist in a given object, it will not fail and simply not match that criteria.  Since 
+ *
+ * Note that querying an object is different to querying the database.  If a property is queried on
+ * that does not exist in a given object, it will not fail and simply not match that criteria.  Since
  * the list of user events contains various event types, each item in the list can have different properties.
  *
  * @author Terry Packer
@@ -51,7 +51,7 @@ import net.jazdw.rql.parser.ASTNode;
 @RequestMapping("/user-events")
 public class UserEventsV2Controller extends AbstractMangoRestV2Controller{
 
-    
+
     @ApiOperation(
             value = "Query User Events",
             notes = "Query via rql in url against events for the current user",
@@ -59,10 +59,10 @@ public class UserEventsV2Controller extends AbstractMangoRestV2Controller{
             responseContainer="Array"
             )
     @RequestMapping(method = RequestMethod.GET, value = "")
-    public ResponseEntity<PageQueryResultModel<EventInstanceModel>> query(
+    public ListWithTotal<EventInstanceModel> query(
             @AuthenticationPrincipal User user,
             HttpServletRequest request) {
-        
+
         //Parse the RQL Query
         ASTNode query = RQLUtils.parseRQLtoAST(request.getQueryString());
         List<EventInstance> results;
@@ -76,23 +76,34 @@ public class UserEventsV2Controller extends AbstractMangoRestV2Controller{
         for(EventInstance event : results) {
             models.add(new EventInstanceModel(event));
         }
-        
-        //Query the models
-        return new ResponseEntity<>(new PageQueryResultModel<>(models, events.size()), HttpStatus.OK);
+
+        return new ListWithTotal<EventInstanceModel>() {
+
+            @Override
+            public List<EventInstanceModel> getItems() {
+                return models;
+            }
+
+            @Override
+            public int getTotal() {
+                return events.size();
+            }
+
+        };
     }
-    
+
     @ApiOperation(
             value = "Get Explaination For Query",
             notes = "What is Query-able on this model"
             )
-    @ApiResponses(value = { 
+    @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Ok"),
             @ApiResponse(code = 403, message = "User does not have access")
-        })
+    })
     @RequestMapping(method = RequestMethod.GET, value = "/explain-query")
     public ResponseEntity<TableModel> getTableModel(HttpServletRequest request) {
         Map<String, QueryAttribute> attributeMap = new HashMap<>();
-        
+
         Field[] inherited = EventInstance.class.getFields();
         for(Field field : inherited) {
             QueryAttribute qa = new QueryAttribute(field.getName(), null, getJdbcTypeCode(field.getGenericType().getTypeName()));
@@ -103,7 +114,7 @@ public class UserEventsV2Controller extends AbstractMangoRestV2Controller{
             QueryAttribute qa = new QueryAttribute(field.getName(), null, getJdbcTypeCode(field.getGenericType().getTypeName()));
             attributeMap.put(field.getName(), qa);
         }
-        
+
         return new ResponseEntity<>(new TableModel("EventInstance.class", new ArrayList<>(attributeMap.values())), HttpStatus.OK);
     }
 
@@ -127,13 +138,13 @@ public class UserEventsV2Controller extends AbstractMangoRestV2Controller{
             case "long":
             case "Long":
                 return Types.BIGINT;
-            default: 
+            default:
                 if(typeName.startsWith("com") || typeName.startsWith("java"))
                     return Types.JAVA_OBJECT;
                 else
                     return Types.OTHER;
         }
     }
-    
-    
+
+
 }
