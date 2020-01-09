9b0261f46603c6bd1cf1561e5bfa8379650c5adc
terrypacker <terrypacker@users.noreply.github.com>
2020-01-09 13:33:46 -1000

    Add log rest controller models

--- Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/logging/LogQueryArrayStream.java
+++ Mango API/src/com/serotonin/m2m2/web/mvc/rest/v1/model/logging/LogQueryArrayStream.java
@@ -9,9 +9,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.infiniteautomation.mango.rest.v2.model.JSONStreamedArray;
 import com.serotonin.m2m2.Common;
-import com.serotonin.m2m2.web.mvc.rest.v1.csv.CSVPojoWriter;
-import com.serotonin.m2m2.web.mvc.rest.v1.model.QueryArrayStream;
 
 import net.jazdw.rql.parser.ASTNode;
 
@@ -19,70 +18,51 @@ import net.jazdw.rql.parser.ASTNode;
  * @author Terry Packer
  *
  */
-public class LogQueryArrayStream implements QueryArrayStream<LogMessageModel>{
-	
-	public static final String LOGFILE_REGEX = ".*ma.log";
-	private String filename;
-	private ASTNode query;
-	
-	public LogQueryArrayStream(String filename, ASTNode query){
-		this.filename = filename;
-		this.query = query;
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.web.mvc.rest.v1.model.QueryArrayStream#streamData(com.fasterxml.jackson.core.JsonGenerator)
-	 */
-	@Override
-	public void streamData(JsonGenerator jgen) throws IOException {
-		
-		if(filename == null)
-			return;
-		
-		if(filename.matches(LOGFILE_REGEX)){
-			processLog4j(jgen);
-		}else{
-			throw new IOException("Cannot query non Mango Log4J Files");
-		}
-		
+public class LogQueryArrayStream implements JSONStreamedArray {
 
-	}
+    public static final String LOGFILE_REGEX = ".*ma.log";
+    private String filename;
+    private ASTNode query;
 
-	/**
-	 * Specifically Process a Log4J file
-	 * 
-	 * @param jgen
-	 * @throws IOException 
-	 */
-	private void processLog4j(JsonGenerator jgen) throws IOException {
-		MangoLogFilePatternReceiver receiver = new MangoLogFilePatternReceiver(query, jgen);
+    public LogQueryArrayStream(String filename, ASTNode query){
+        this.filename = filename;
+        this.query = query;
+    }
 
-		try {
-			File logsDir = Common.getLogsDir();
-			File logFile = new File(logsDir, filename);
-			if(!logFile.exists())
-				return;
-			
-		    receiver.setLogFormat("LEVEL TIMESTAMP (CLASS.METHOD:LINE) - MESSAGE"); //"%-5p %d{ISO8601} (%C.%M:%L) - %m %n"
-		    receiver.setFileURL(logFile.toURI().toURL().toExternalForm());
-		    receiver.setUseCurrentThread(true);
+    /**
+     * Specifically Process a Log4J file
+     *
+     * @param jgen
+     * @throws IOException
+     */
+    @Override
+    public void writeArrayValues(JsonGenerator jgen) throws IOException {
+        if(filename == null)
+            return;
 
-		    //Start the parsing
-		    receiver.activateOptions();
-	    } catch (MalformedURLException e) {
-			throw new IOException(e);
-		} finally {
-	        receiver.shutdown();
-	    }
-	}
+        if(filename.matches(LOGFILE_REGEX)){
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.m2m2.web.mvc.rest.v1.model.QueryArrayStream#streamData(com.serotonin.m2m2.web.mvc.rest.v1.csv.CSVPojoWriter)
-	 */
-	@Override
-	public void streamData(CSVPojoWriter<LogMessageModel> writer)
-			throws IOException {
-		//No Op For now
-	}
+            MangoLogFilePatternReceiver receiver = new MangoLogFilePatternReceiver(query, jgen);
 
+            try {
+                File logsDir = Common.getLogsDir();
+                File logFile = new File(logsDir, filename);
+                if(!logFile.exists())
+                    return;
+
+                receiver.setLogFormat("LEVEL TIMESTAMP (CLASS.METHOD:LINE) - MESSAGE"); //"%-5p %d{ISO8601} (%C.%M:%L) - %m %n"
+                receiver.setFileURL(logFile.toURI().toURL().toExternalForm());
+                receiver.setUseCurrentThread(true);
+
+                //Start the parsing
+                receiver.activateOptions();
+            } catch (MalformedURLException e) {
+                throw new IOException(e);
+            } finally {
+                receiver.shutdown();
+            }
+        }else{
+            throw new IOException("Cannot query non Mango Log4J Files");
+        }
+    }
 }
