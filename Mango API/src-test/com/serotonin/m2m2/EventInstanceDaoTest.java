d1ad8ad086556a54e20338e66d6f237e626e35ca
terrypacker <terrypacker@users.noreply.github.com>
2020-01-10 10:04:07 -1000

    Fixing failing test

--- Mango API/src-test/com/serotonin/m2m2/EventInstanceDaoTest.java
+++ Mango API/src-test/com/serotonin/m2m2/EventInstanceDaoTest.java
@@ -9,11 +9,9 @@ import org.apache.commons.lang3.mutable.MutableInt;
 import org.junit.Test;
 
 import com.infiniteautomation.mango.db.query.ConditionSortLimit;
-import com.serotonin.m2m2.db.dao.EventDao;
 import com.serotonin.m2m2.db.dao.EventInstanceDao;
 import com.serotonin.m2m2.i18n.TranslatableMessage;
 import com.serotonin.m2m2.rt.event.AlarmLevels;
-import com.serotonin.m2m2.rt.event.EventInstance;
 import com.serotonin.m2m2.rt.event.type.SystemEventType;
 import com.serotonin.m2m2.vo.event.EventInstanceVO;
 
@@ -26,30 +24,39 @@ import net.jazdw.rql.parser.RQLParser;
  */
 public class EventInstanceDaoTest extends MangoTestBase {
 
+    public EventInstanceDaoTest() {
+        super(true, 9000);
+    }
+
     /**
      * Ensure the extra fields property doesn't cause problems with JOOQ
      */
     @Test
     public void testJooq() {
-        
+
         SystemEventType type = new SystemEventType(SystemEventType.TYPE_SYSTEM_STARTUP);
         long timestamp = Common.timer.currentTimeMillis();
         for(int i=0; i<100; i++) {
-            EventInstance evt = new EventInstance(type, timestamp, false,
-                    AlarmLevels.CRITICAL, new TranslatableMessage("common.default", "testing"), null);
-            EventDao.getInstance().saveEvent(evt);
+            Common.eventManager.raiseEvent(type, timestamp, false, AlarmLevels.CRITICAL, new TranslatableMessage("common.default", "testing"), null);
             timestamp++;
         }
-        
+
         ASTNode rql = new RQLParser().parse("lt(activeTs, " + timestamp + ")&limit(100");
         ConditionSortLimit conditions = EventInstanceDao.getInstance().rqlToCondition(rql);
         MutableInt count = new MutableInt();
         EventInstanceDao.getInstance().customizedQuery(conditions, (EventInstanceVO item, int index) -> {
             count.increment();
         });
-        
+
         assertEquals(100, (int)count.getValue());
-        
+
+    }
+
+    @Override
+    protected MockMangoLifecycle getLifecycle() {
+        MockMangoLifecycle lifecycle = super.getLifecycle();
+        lifecycle.setEventManager(new MockEventManager(true));
+        return lifecycle;
     }
 
 }
