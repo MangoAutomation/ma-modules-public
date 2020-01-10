bc55918da8392cbe44cef63f5c07f24b4b33e0e3
terrypacker <terrypacker@users.noreply.github.com>
2020-01-10 13:09:37 -1000

    Add JSONStreamedArraySerializer to v2 context

--- Mango API/src/com/infiniteautomation/mango/rest/v2/mapping/MangoRestV2JacksonModule.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/mapping/MangoRestV2JacksonModule.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.infiniteautomation.mango.io.serial.virtual.VirtualSerialPortConfig;
 import com.infiniteautomation.mango.rest.v2.exception.ExceptionMixin;
+import com.infiniteautomation.mango.rest.v2.model.JSONStreamedArray;
 import com.serotonin.m2m2.i18n.TranslatableMessage;
 
 /**
@@ -16,18 +17,19 @@ import com.serotonin.m2m2.i18n.TranslatableMessage;
  */
 public class MangoRestV2JacksonModule extends SimpleModule {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public MangoRestV2JacksonModule() {
-		super("MangoCoreRestV2", new Version(0, 0, 1, "SNAPSHOT", "com.infiniteautomation",
-				"mango"));
+    public MangoRestV2JacksonModule() {
+        super("MangoCoreRestV2", new Version(0, 0, 1, "SNAPSHOT", "com.infiniteautomation",
+                "mango"));
         this.setMixInAnnotation(Exception.class, ExceptionMixin.class);
-	}
-	
-	@Override
-	public void setupModule(SetupContext context) {
-		this.addSerializer(TranslatableMessage.class, new TranslatableMessageSerializer());
-		this.addDeserializer(VirtualSerialPortConfig.class, new VirtualSerialPortConfigDeserializer());
-		super.setupModule(context);
-	}
+    }
+
+    @Override
+    public void setupModule(SetupContext context) {
+        this.addSerializer(JSONStreamedArray.class, new JSONStreamedArraySerializer());
+        this.addSerializer(TranslatableMessage.class, new TranslatableMessageSerializer());
+        this.addDeserializer(VirtualSerialPortConfig.class, new VirtualSerialPortConfigDeserializer());
+        super.setupModule(context);
+    }
 }
