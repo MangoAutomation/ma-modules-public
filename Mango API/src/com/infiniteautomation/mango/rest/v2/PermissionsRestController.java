604fc59ad86806bce34fa8529bab89013e857c11
terrypacker <terrypacker@users.noreply.github.com>
2020-02-26 07:05:19 -1000

    Improve system permission endpoints

--- Mango API/src/com/infiniteautomation/mango/rest/v2/PermissionsRestController.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/PermissionsRestController.java
@@ -3,46 +3,92 @@
  */
 package com.infiniteautomation.mango.rest.v2;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.infiniteautomation.mango.rest.v2.model.permissions.PermissionDefinitionModel;
+import com.infiniteautomation.mango.spring.service.RoleService;
 import com.serotonin.m2m2.module.ModuleRegistry;
 import com.serotonin.m2m2.module.PermissionDefinition;
+import com.serotonin.m2m2.vo.User;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
- * List of permissions and their system setting keys
+ * List of permissions and their keys
  *
  * @author Jared Wiltshire
  */
-@Api(value="Lists permissions and their system setting keys")
+@Api(value="Lists permissions and their keys")
 @PreAuthorize("isAdmin()")
 @RestController
 @RequestMapping("/permissions")
 public class PermissionsRestController {
 
+    private final RoleService service;
+
+    @Autowired
+    public PermissionsRestController(RoleService service){
+        this.service = service;
+    }
+
     @ApiOperation(
-            value = "List permissions and their system setting keys"
+            value = "List permissions, their keys and roles"
             )
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<PermissionDefinitionModel>> listPermissions() {
         List<PermissionDefinitionModel> permissions = new ArrayList<>();
 
-        for (PermissionDefinition def : ModuleRegistry.getDefinitions(PermissionDefinition.class)) {
+        for (PermissionDefinition def : ModuleRegistry.getPermissionDefinitions().values()) {
             permissions.add(new PermissionDefinitionModel(def));
         }
 
         return new ResponseEntity<>(permissions, HttpStatus.OK);
+    }
+
+    @ApiOperation(
+            value = "Update all of a Permission's Roles",
+            notes = "If no roles are supplied then all existing assigned roles are removed",
+            response=PermissionDefinitionModel.class
+            )
+    @RequestMapping(method = RequestMethod.PUT, value="/{key}")
+    public ResponseEntity<PermissionDefinitionModel> update(
+            @PathVariable String key,
+            @ApiParam(value="Permission", required=true)
+            @RequestBody(required=true)
+            PermissionDefinitionModel model,
+            @AuthenticationPrincipal User user,
+            HttpServletRequest request,
+            UriComponentsBuilder builder,
+            Authentication authentication){
+
+
+        //Replace the roles
+        service.replaceAllRolesOnPermission(model.getRoles(), key);
+
+        URI location = builder.path("/permissions/{key}").buildAndExpand(key).toUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+        return new ResponseEntity<>(new PermissionDefinitionModel(ModuleRegistry.getPermissionDefinition(key)), headers, HttpStatus.OK);
     }
 
 }
--- Mango API/src/com/infiniteautomation/mango/rest/v2/model/permissions/PermissionDefinitionModel.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/model/permissions/PermissionDefinitionModel.java
@@ -4,8 +4,12 @@
 
 package com.infiniteautomation.mango.rest.v2.model.permissions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.serotonin.m2m2.i18n.TranslatableMessage;
 import com.serotonin.m2m2.module.PermissionDefinition;
+import com.serotonin.m2m2.vo.role.Role;
 
 /**
  * @author Terry Packer
@@ -13,17 +17,24 @@ import com.serotonin.m2m2.module.PermissionDefinition;
 public class PermissionDefinitionModel {
     String systemSettingName;
     TranslatableMessage description;
+    Set<String> roles;
 
     public PermissionDefinitionModel() {}
 
-    public PermissionDefinitionModel(String systemSettingName, TranslatableMessage description) {
+    public PermissionDefinitionModel(String systemSettingName, TranslatableMessage description, Set<String> roles) {
         this.systemSettingName = systemSettingName;
         this.description = description;
+        this.roles = roles;
     }
 
     public PermissionDefinitionModel(PermissionDefinition def) {
         this.systemSettingName = def.getPermissionTypeName();
         this.description = def.getDescription();
+        Set<Role> defRoles = def.getRoles();
+        this.roles = new HashSet<>();
+        for(Role role : defRoles) {
+            this.roles.add(role.getXid());
+        }
     }
 
     public String getSystemSettingName() {
@@ -40,5 +51,13 @@ public class PermissionDefinitionModel {
 
     public void setDescription(TranslatableMessage description) {
         this.description = description;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
     }
 }
