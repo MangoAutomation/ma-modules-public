5c549f513be9408ad2ea8ba7da153d4703719252
terrypacker <terrypacker@users.noreply.github.com>
2020-04-08 12:53:57 -1000

    Add replacement /v2/realtime query endpoint

---
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/RealTimeDataRestController.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (C) 2018 Infinite Automation Software. All rights reserved.
+ */
+package com.infiniteautomation.mango.rest.v2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.BiFunction;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.infiniteautomation.mango.rest.v2.model.FilteredStreamWithTotal;
+import com.infiniteautomation.mango.rest.v2.model.RestModelMapper;
+import com.infiniteautomation.mango.rest.v2.model.StreamWithTotal;
+import com.infiniteautomation.mango.rest.v2.model.realtime.RealTimeDataPointValueModel;
+import com.infiniteautomation.mango.spring.service.PermissionService;
+import com.infiniteautomation.mango.util.RQLUtils;
+import com.serotonin.m2m2.Common;
+import com.serotonin.m2m2.rt.dataImage.DataPointRT;
+import com.serotonin.m2m2.rt.dataImage.PointValueTime;
+import com.serotonin.m2m2.rt.dataImage.types.ImageValue;
+import com.serotonin.m2m2.rt.dataSource.DataSourceRT;
+import com.serotonin.m2m2.view.text.TextRenderer;
+import com.serotonin.m2m2.vo.User;
+import com.serotonin.m2m2.vo.permission.PermissionHolder;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import net.jazdw.rql.parser.ASTNode;
+
+/**
+ *
+ * @author Terry Packer
+ */
+@Api(value="Access to current values combined with data point information for all currently running data points.")
+@RestController()
+@RequestMapping("/realtime")
+public class RealTimeDataRestController {
+
+    private final String OK = "OK";
+    private final String UNRELIABLE = "UNRELIABLE";
+    private final UriComponentsBuilder imageServletBuilder = UriComponentsBuilder.fromPath("/imageValue/{ts}_{id}.jpg");
+
+    private final PermissionService permissionService;
+    private final BiFunction<DataPointRT, PermissionHolder, RealTimeDataPointValueModel> map;
+
+
+    @Autowired
+    public RealTimeDataRestController(PermissionService permissionService, RestModelMapper modelMapper){
+        this.permissionService = permissionService;
+        this.map = (rt, user) -> {
+            RealTimeDataPointValueModel model = new RealTimeDataPointValueModel();
+
+            model.setXid(rt.getVO().getXid());
+            model.setDeviceName(rt.getVO().getDeviceName());
+            model.setName(rt.getVO().getName());
+
+            PointValueTime pvt = rt.getPointValue();
+            if(pvt != null) {
+                if(pvt.getValue() instanceof ImageValue) {
+                    model.setValue(imageServletBuilder.buildAndExpand(pvt.getTime(), rt.getId()).toUri());
+                }else {
+                    model.setValue(pvt.getValue().getObjectValue());
+                }
+                model.setTimestamp(pvt.getTime());
+                model.setRenderedValue(rt.getVO().getTextRenderer().getText(pvt, TextRenderer.HINT_FULL));
+            }
+
+            if(user instanceof User) {
+                model.setType(rt.getVO().getPointLocator().getDataTypeMessage().translate(((User)user).getTranslations()));
+            }else {
+                model.setType(rt.getVO().getPointLocator().getDataTypeMessage().translate(Common.getTranslations()));
+            }
+
+            model.setAttributes(new HashMap<>(rt.getAttributes()));
+
+            Object unreliable = rt.getAttribute(DataSourceRT.ATTR_UNRELIABLE_KEY);
+            if ((unreliable instanceof Boolean) && ((Boolean) unreliable)) {
+                model.setStatus(UNRELIABLE);
+            }else {
+                model.setStatus(OK);
+            }
+
+            model.setTags(rt.getVO().getTags());
+            return model;
+        };
+    }
+
+    /**
+     * Query the User's Real Time Data
+     * @param request
+     * @param limit
+     * @return
+     */
+    @ApiOperation(value = "Query realtime values",
+            notes = "Check the status member to ensure the point is OK not DISABLED or UNRELIABLE")
+    @RequestMapping(method = RequestMethod.GET)
+    public StreamWithTotal<RealTimeDataPointValueModel> query(
+            @AuthenticationPrincipal User user,
+            HttpServletRequest request) {
+        //First build all the models
+        ASTNode query = RQLUtils.parseRQLtoAST(request.getQueryString());
+        List<DataPointRT> points = Common.runtimeManager.getRunningDataPoints();
+        List<RealTimeDataPointValueModel> models = new ArrayList<>();
+        for(DataPointRT rt : points) {
+            if(permissionService.hasDataPointReadPermission(user, rt.getVO())) {
+                models.add(map.apply(rt, user));
+            }
+        }
+
+        //Query the results
+        return new FilteredStreamWithTotal<>(models, query);
+    }
+
+}
---
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/model/realtime/RealTimeDataPointValueModel.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (C) 2020 Infinite Automation Software. All rights reserved.
+ */
+package com.infiniteautomation.mango.rest.v2.model.realtime;
+
+import java.util.Map;
+
+
+/**
+ *
+ * @author Terry Packer
+ */
+public class RealTimeDataPointValueModel {
+
+    private String xid;
+    private String deviceName;
+    private String name;
+    private Object value;
+    private String renderedValue;
+    private String type;
+    private long timestamp;
+    private String status;
+    private Map<String,String> tags;
+    private Map<String, Object> attributes;
+
+    public String getXid() {
+        return xid;
+    }
+    public void setXid(String xid) {
+        this.xid = xid;
+    }
+    public String getDeviceName() {
+        return deviceName;
+    }
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Object getValue() {
+        return value;
+    }
+    public void setValue(Object value) {
+        this.value = value;
+    }
+    public String getRenderedValue() {
+        return renderedValue;
+    }
+    public void setRenderedValue(String renderedValue) {
+        this.renderedValue = renderedValue;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Map<String, String> getTags() {
+        return tags;
+    }
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+}
