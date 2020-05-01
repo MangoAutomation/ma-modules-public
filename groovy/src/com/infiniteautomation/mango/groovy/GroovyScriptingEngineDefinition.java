c06b8a0cb7743a7953d75fb5f5ef75c34f9cea6f
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-05-01 13:00:03 -0600

    Add support for script stack trace

--- groovy/src/com/infiniteautomation/mango/groovy/GroovyScriptingEngineDefinition.java
+++ groovy/src/com/infiniteautomation/mango/groovy/GroovyScriptingEngineDefinition.java
@@ -3,13 +3,25 @@
  */
 package com.infiniteautomation.mango.groovy;
 
-import javax.script.ScriptEngineFactory;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Optional;
 
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptException;
+
+import org.codehaus.groovy.control.ErrorCollector;
+import org.codehaus.groovy.control.MultipleCompilationErrorsException;
+import org.codehaus.groovy.control.messages.Message;
+import org.codehaus.groovy.control.messages.SyntaxErrorMessage;
 import org.codehaus.groovy.jsr223.GroovyScriptEngineFactory;
+import org.codehaus.groovy.runtime.StackTraceUtils;
+import org.codehaus.groovy.syntax.SyntaxException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.infiniteautomation.mango.permission.MangoPermission;
 import com.serotonin.m2m2.module.ScriptEngineDefinition;
+import com.serotonin.m2m2.module.SourceLocation;
 
 /**
  * @author Jared Wiltshire
@@ -29,4 +41,37 @@ public class GroovyScriptingEngineDefinition extends ScriptEngineDefinition {
         return permission.getPermission();
     }
 
+    @Override
+    public SourceLocation extractSourceLocation(ScriptException e) {
+        Throwable root = StackTraceUtils.extractRootCause(e);
+
+        if (root instanceof MultipleCompilationErrorsException) {
+            ErrorCollector errorCollector = ((MultipleCompilationErrorsException) root).getErrorCollector();
+            Optional<? extends Message> syntaxErrorMessage = errorCollector.getErrors().stream().filter(m -> m instanceof SyntaxErrorMessage).findFirst();
+            if (syntaxErrorMessage.isPresent()) {
+                SyntaxErrorMessage syntaxMessage = (SyntaxErrorMessage) syntaxErrorMessage.get();
+                SyntaxException syntaxException = syntaxMessage.getCause();
+                return new SourceLocation(syntaxException.getSourceLocator(), syntaxException.getStartLine(), syntaxException.getStartColumn(), "");
+            }
+        }
+
+        StringWriter writer = new StringWriter();
+        StackTraceUtils.printSanitizedStackTrace(root, new PrintWriter(writer));
+        String stackTraceStr = writer.toString();
+
+        Integer lineNumber = null;
+        String scriptFileName = null;
+        StackTraceElement[] stackTrace = root.getStackTrace();
+        for (int i = 0; i < stackTrace.length; i++) {
+            StackTraceElement frame = stackTrace[i];
+            String fileName = frame.getFileName();
+            if (fileName != null && fileName.endsWith(".groovy")) {
+                scriptFileName = fileName;
+                lineNumber = frame.getLineNumber() >= 0 ? frame.getLineNumber() : null;
+                break;
+            }
+        }
+
+        return new SourceLocation(scriptFileName, lineNumber, null, stackTraceStr);
+    }
 }
