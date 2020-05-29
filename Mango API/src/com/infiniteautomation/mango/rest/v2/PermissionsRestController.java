37627c74282a4220187eff61c2681e6fbfac73bf
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-05-29 14:06:21 -0600

    Add get method

--- Mango API/src/com/infiniteautomation/mango/rest/v2/PermissionsRestController.java
+++ Mango API/src/com/infiniteautomation/mango/rest/v2/PermissionsRestController.java
@@ -3,32 +3,23 @@
  */
 package com.infiniteautomation.mango.rest.v2;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import com.infiniteautomation.mango.permission.MangoPermission;
 import com.infiniteautomation.mango.rest.v2.exception.NotFoundRestException;
 import com.infiniteautomation.mango.rest.v2.model.permissions.PermissionDefinitionModel;
 import com.infiniteautomation.mango.spring.service.SystemPermissionService;
 import com.serotonin.m2m2.module.ModuleRegistry;
 import com.serotonin.m2m2.module.PermissionDefinition;
-import com.serotonin.m2m2.vo.User;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -39,7 +30,7 @@ import io.swagger.annotations.ApiParam;
  *
  * @author Jared Wiltshire
  */
-@Api(value="Lists permissions and their keys")
+@Api(value = "Lists permissions and their keys")
 @PreAuthorize("isAdmin()")
 @RestController
 @RequestMapping("/system-permissions")
@@ -52,48 +43,41 @@ public class PermissionsRestController {
         this.service = service;
     }
 
-    @ApiOperation(
-            value = "List permissions, their keys and roles"
-            )
+    @ApiOperation(value = "List permissions, their keys and roles")
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<PermissionDefinitionModel>> listPermissions() {
+    public List<PermissionDefinitionModel> listPermissions() {
         List<PermissionDefinitionModel> permissions = new ArrayList<>();
 
         for (PermissionDefinition def : ModuleRegistry.getPermissionDefinitions().values()) {
             permissions.add(new PermissionDefinitionModel(def));
         }
 
-        return new ResponseEntity<>(permissions, HttpStatus.OK);
+        return permissions;
     }
 
-    @ApiOperation(
-            value = "Update all of a Permission's Roles",
-            notes = "If no roles are supplied then all existing assigned roles are removed",
-            response=PermissionDefinitionModel.class
-            )
-    @RequestMapping(method = RequestMethod.PUT, value="/{key}")
-    public ResponseEntity<PermissionDefinitionModel> update(
-            @PathVariable String key,
-            @ApiParam(value="Permission", required=true)
-            @RequestBody(required=true)
-            PermissionDefinitionModel model,
-            @AuthenticationPrincipal User user,
-            HttpServletRequest request,
-            UriComponentsBuilder builder,
-            Authentication authentication){
+    @ApiOperation(value = "Update all of a Permission's Roles", notes = "If no roles are supplied then all existing assigned roles are removed")
+    @RequestMapping(method = RequestMethod.PUT, value = "/{key}")
+    public PermissionDefinitionModel update(@PathVariable String key,
+            @ApiParam(value = "Permission", required = true) @RequestBody(required = true) PermissionDefinitionModel model) {
 
         PermissionDefinition def = ModuleRegistry.getPermissionDefinition(key);
 
-        if(def == null) {
+        if (def == null) {
             throw new NotFoundRestException();
         }
 
-        service.update(model.getPermission() != null ? model.getPermission().getPermission() : null, def);
-
-        URI location = builder.path("/permissions/{key}").buildAndExpand(key).toUri();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(location);
-        return new ResponseEntity<>(new PermissionDefinitionModel(def), headers, HttpStatus.OK);
+        MangoPermission permission = model.getPermission() != null ? model.getPermission().getPermission() : null;
+        service.update(permission, def);
+        return new PermissionDefinitionModel(def);
     }
 
+    @ApiOperation(value = "Get a permission")
+    @RequestMapping(method = RequestMethod.GET, value = "/{key}")
+    public PermissionDefinitionModel get(@PathVariable String key) {
+        PermissionDefinition def = ModuleRegistry.getPermissionDefinition(key);
+        if (def == null) {
+            throw new NotFoundRestException();
+        }
+        return new PermissionDefinitionModel(def);
+    }
 }
