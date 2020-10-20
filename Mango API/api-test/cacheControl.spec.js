5509bbd15918be8ce71e730bc3a3deb8d1983750
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-10-20 10:22:36 -0600

    Update logos

--- Mango API/api-test/cacheControl.spec.js
+++ Mango API/api-test/cacheControl.spec.js
@@ -20,79 +20,79 @@ const client = createClient();
 
 describe('Cache control verification', function() {
     before('Login', function() { return login.call(this, client); });
-    
+
     it('Gets default cache-control header for core resource request', function() {
         return client.restRequest({
-            path: '/images/logo.gif',
+            path: '/images/logo.png',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }    
+            }
         }).then(response => {
             assert.strictEqual(response.headers['cache-control'], 'max-age=86400');
             assert.exists(response.headers['last-modified']);
         });
     });
-    
+
     it('Gets default cache-control header for module resource request', function() {
         return client.restRequest({
-            path: '/modules/mangoUI/web/img/logo.png',
+            path: '/modules/mangoUI/web/img/icon16.png',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }    
+            }
         }).then(response => {
             assert.strictEqual(response.headers['cache-control'], 'max-age=86400');
             assert.exists(response.headers['last-modified']);
         });
     });
-    
+
     it('Gets default cache-control header for ui resource request', function() {
         return client.restRequest({
-            path: '/ui/img/logo.png',
+            path: '/ui/img/icon16.png',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }    
+            }
         }).then(response => {
             assert.strictEqual(response.headers['cache-control'], 'max-age=86400');
             assert.exists(response.headers['last-modified']);
         });
     });
-    
+
     it('Gets default resource cache-control header for ui/login request', function() {
         return client.restRequest({
             path: '/ui/login',
             dataType: 'buffer',
             headers: {
                 'Accept': 'text/html'
-            }    
+            }
         }).then(response => {
             assert.strictEqual(response.headers['cache-control'], 'max-age=0');
             assert.exists(response.headers['last-modified']);
         });
     });
-    
+
     it('Gets no cache for REST request', function() {
         return client.restRequest({
             path: '/rest/latest/users/current',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }    
+            }
         }).then(response => {
             assert.strictEqual(response.headers['cache-control'], 'no-store');
             assert.notExists(response.headers['last-modified']);
         });
     });
-    
+
     it('Gets 304 response for resource request with same modified time', function() {
         return client.restRequest({
-            path: '/images/logo.gif',
+            path: '/images/logo.png',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }     
+            }
         }).then(response => {
             assert.exists(response.headers['cache-control']);
             assert.exists(response.headers['last-modified']);
@@ -103,7 +103,7 @@ describe('Cache control verification', function() {
                 headers: {
                     'Accept': '*/*',
                     'if-modified-since': lastModified
-                }     
+                }
             }).then(response => {
                 assert.strictEqual(response.status, 304);
             });
@@ -112,11 +112,11 @@ describe('Cache control verification', function() {
 
     it('Gets 200 response for core resource request modified since', function() {
         return client.restRequest({
-            path: '/images/logo.gif',
+            path: '/images/logo.png',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }     
+            }
         }).then(response => {
             assert.exists(response.headers['cache-control']);
             assert.exists(response.headers['last-modified']);
@@ -128,25 +128,25 @@ describe('Cache control verification', function() {
                 headers: {
                     'Accept': '*/*',
                     'if-modified-since': beforeLastModified.toUTCString()
-                }     
+                }
             }).then(response => {
                assert.strictEqual(response.status, 200);
             });
         });
     });
-    
+
     it('Handles not found ok', function() {
         return client.restRequest({
             path: '/images/nada',
             dataType: 'buffer',
             headers: {
                 'Accept': '*/*'
-            }     
+            }
         }).then(response => {
            throw new Error('Should not have gotten ok response');
         }, error => {
             assert.strictEqual(error.status, 404);
         });
     });
-    
+
 });
