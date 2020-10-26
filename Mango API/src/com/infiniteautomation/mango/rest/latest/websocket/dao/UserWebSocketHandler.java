fcc4e332e886c914689f3f3002c781ffff801106
terrypacker <terrypacker@users.noreply.github.com>
2020-10-26 07:52:38 -1000

    Use username for xid and originalXid fields in user websocket mesages
    Fixes infiniteautomation/ma-core-public#1655

--- Mango API/src/com/infiniteautomation/mango/rest/latest/websocket/dao/UserWebSocketHandler.java
+++ Mango API/src/com/infiniteautomation/mango/rest/latest/websocket/dao/UserWebSocketHandler.java
@@ -4,11 +4,15 @@
  */
 package com.infiniteautomation.mango.rest.latest.websocket.dao;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import com.infiniteautomation.mango.rest.latest.model.user.UserModel;
+import com.infiniteautomation.mango.rest.latest.websocket.DaoNotificationModel;
 import com.infiniteautomation.mango.rest.latest.websocket.DaoNotificationWebSocketHandler;
+import com.infiniteautomation.mango.rest.latest.websocket.MangoWebSocketResponseModel;
+import com.infiniteautomation.mango.rest.latest.websocket.MangoWebSocketResponseStatus;
 import com.infiniteautomation.mango.rest.latest.websocket.WebSocketMapping;
 import com.infiniteautomation.mango.spring.events.DaoEvent;
 import com.serotonin.m2m2.vo.User;
@@ -43,4 +47,25 @@ public class UserWebSocketHandler extends DaoNotificationWebSocketHandler<User>{
         this.notify(event);
     }
 
+    /*
+     * Overriden to use username instead of xid
+     */
+    @Override
+    protected Object createNotification(String action, User vo, User originalVo, PermissionHolder user) {
+        Integer id = vo.getId();
+        String xid = vo.getUsername();
+        String originalXid = (originalVo != null) ? originalVo.getUsername() : null;
+
+        DaoNotificationModel payload;
+        if(StringUtils.equals(action, "delete")) {
+            payload = new DaoNotificationModel(action, id, xid, null, originalXid);
+        }else {
+            Object model = createModel(vo, user);
+            if (model == null) {
+                return null;
+            }
+            payload = new DaoNotificationModel("create".equals(action) ? "add" : action, id, xid, model, originalXid);
+        }
+        return new MangoWebSocketResponseModel(MangoWebSocketResponseStatus.OK, payload);
+    }
 }
