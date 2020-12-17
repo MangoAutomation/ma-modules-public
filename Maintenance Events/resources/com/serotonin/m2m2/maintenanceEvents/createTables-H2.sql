687342e91e9e9bbc860c83c5b058340d90a31a92
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-12-17 13:27:22 -0700

    Format and quote identifiers

--- Maintenance Events/resources/com/serotonin/m2m2/maintenanceEvents/createTables-H2.sql
+++ Maintenance Events/resources/com/serotonin/m2m2/maintenanceEvents/createTables-H2.sql
@@ -2,46 +2,55 @@
 --    Copyright (C) 2014 Infinite Automation Systems Inc. All rights reserved.
 --    @author Matthew Lohbihler
 --
-create table maintenanceEvents (
-  id int not null auto_increment,
-  xid varchar(100) not null,
-  alias varchar(255),
-  alarmLevel int not null,
-  scheduleType int not null,
-  disabled char(1) not null,
-  activeYear int,
-  activeMonth int,
-  activeDay int,
-  activeHour int,
-  activeMinute int,
-  activeSecond int,
-  activeCron varchar(25),
-  inactiveYear int,
-  inactiveMonth int,
-  inactiveDay int,
-  inactiveHour int,
-  inactiveMinute int,
-  inactiveSecond int,
-  inactiveCron varchar(25),
-  timeoutPeriods int,
-  timeoutPeriodType int,
-  togglePermissionId INT NOT NULL,
-  primary key (id)
+create table "maintenanceEvents"
+(
+    "id"                 int          not null auto_increment,
+    "xid"                varchar(100) not null,
+    "alias"              varchar(255),
+    "alarmLevel"         int          not null,
+    "scheduleType"       int          not null,
+    "disabled"           char(1)      not null,
+    "activeYear"         int,
+    "activeMonth"        int,
+    "activeDay"          int,
+    "activeHour"         int,
+    "activeMinute"       int,
+    "activeSecond"       int,
+    "activeCron"         varchar(25),
+    "inactiveYear"       int,
+    "inactiveMonth"      int,
+    "inactiveDay"        int,
+    "inactiveHour"       int,
+    "inactiveMinute"     int,
+    "inactiveSecond"     int,
+    "inactiveCron"       varchar(25),
+    "timeoutPeriods"     int,
+    "timeoutPeriodType"  int,
+    "togglePermissionId" INT          NOT NULL,
+    primary key ("id")
 );
-alter table maintenanceEvents add constraint maintenanceEventsUn1 unique (xid);
-ALTER TABLE maintenanceEvents ADD CONSTRAINT maintenanceEventsFk1 FOREIGN KEY (togglePermissionId) REFERENCES permissions(id) ON DELETE RESTRICT;
+alter table "maintenanceEvents"
+    add constraint "maintenanceEventsUn1" unique ("xid");
+ALTER TABLE "maintenanceEvents"
+    ADD CONSTRAINT "maintenanceEventsFk1" FOREIGN KEY ("togglePermissionId") REFERENCES "permissions" ("id") ON DELETE RESTRICT;
 
-CREATE TABLE maintenanceEventDataPoints (
-  maintenanceEventId int NOT NULL,
-  dataPointId int NOT NULL
-) ;
-ALTER TABLE maintenanceEventDataPoints add constraint maintenanceEventDataPointsFk1 foreign key (maintenanceEventId) references maintenanceEvents(id) on delete cascade;
-ALTER TABLE maintenanceEventDataPoints add constraint maintenanceEventDataPointsFk2 foreign key (dataPointId) references dataPoints(id) on delete cascade;
+CREATE TABLE "maintenanceEventDataPoints"
+(
+    "maintenanceEventId" int NOT NULL,
+    "dataPointId"        int NOT NULL
+);
+ALTER TABLE "maintenanceEventDataPoints"
+    add constraint "maintenanceEventDataPointsFk1" foreign key ("maintenanceEventId") references "maintenanceEvents" ("id") on delete cascade;
+ALTER TABLE "maintenanceEventDataPoints"
+    add constraint "maintenanceEventDataPointsFk2" foreign key ("dataPointId") references "dataPoints" ("id") on delete cascade;
 
-CREATE TABLE maintenanceEventDataSources (
-  maintenanceEventId int NOT NULL,
-  dataSourceId int NOT NULL
-) ;
-ALTER TABLE maintenanceEventDataSources add constraint maintenanceEventDataSourcesFk1 foreign key (maintenanceEventId) references maintenanceEvents(id) on delete cascade;
-ALTER TABLE maintenanceEventDataSources add constraint maintenanceEventDataSourcesFk2 foreign key (dataSourceId) references dataSources(id) on delete cascade;
+CREATE TABLE "maintenanceEventDataSources"
+(
+    "maintenanceEventId" int NOT NULL,
+    "dataSourceId"       int NOT NULL
+);
+ALTER TABLE "maintenanceEventDataSources"
+    add constraint "maintenanceEventDataSourcesFk1" foreign key ("maintenanceEventId") references "maintenanceEvents" ("id") on delete cascade;
+ALTER TABLE "maintenanceEventDataSources"
+    add constraint "maintenanceEventDataSourcesFk2" foreign key ("dataSourceId") references "dataSources" ("id") on delete cascade;
 
