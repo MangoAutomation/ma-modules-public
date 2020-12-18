ddb7bffefd2542a5786ad13795be920985ae9a4d
Jared Wiltshire <jazdw@users.noreply.github.com>
2020-12-18 16:23:32 -0700

    Roll back quoted identifiers

--- Maintenance Events/resources/com/serotonin/m2m2/maintenanceEvents/createTables-H2.sql
+++ Maintenance Events/resources/com/serotonin/m2m2/maintenanceEvents/createTables-H2.sql
@@ -2,61 +2,55 @@
 --    Copyright (C) 2014 Infinite Automation Systems Inc. All rights reserved.
 --    @author Matthew Lohbihler
 --
-CREATE TABLE "maintenanceEvents"
+CREATE TABLE maintenanceEvents
 (
-    "id"                 INT          NOT NULL AUTO_INCREMENT,
-    "xid"                VARCHAR(100) NOT NULL,
-    "alias"              VARCHAR(255),
-    "alarmLevel"         INT          NOT NULL,
-    "scheduleType"       INT          NOT NULL,
-    "disabled"           CHAR(1)      NOT NULL,
-    "activeYear"         INT,
-    "activeMonth"        INT,
-    "activeDay"          INT,
-    "activeHour"         INT,
-    "activeMinute"       INT,
-    "activeSecond"       INT,
-    "activeCron"         VARCHAR(25),
-    "inactiveYear"       INT,
-    "inactiveMonth"      INT,
-    "inactiveDay"        INT,
-    "inactiveHour"       INT,
-    "inactiveMinute"     INT,
-    "inactiveSecond"     INT,
-    "inactiveCron"       VARCHAR(25),
-    "timeoutPeriods"     INT,
-    "timeoutPeriodType"  INT,
-    "togglePermissionId" INT          NOT NULL,
-    PRIMARY KEY ("id")
+    id                 INT          NOT NULL AUTO_INCREMENT,
+    xid                VARCHAR(100) NOT NULL,
+    alias              VARCHAR(255),
+    alarmLevel         INT          NOT NULL,
+    scheduleType       INT          NOT NULL,
+    disabled           CHAR(1)      NOT NULL,
+    activeYear         INT,
+    activeMonth        INT,
+    activeDay          INT,
+    activeHour         INT,
+    activeMinute       INT,
+    activeSecond       INT,
+    activeCron         VARCHAR(25),
+    inactiveYear       INT,
+    inactiveMonth      INT,
+    inactiveDay        INT,
+    inactiveHour       INT,
+    inactiveMinute     INT,
+    inactiveSecond     INT,
+    inactiveCron       VARCHAR(25),
+    timeoutPeriods     INT,
+    timeoutPeriodType  INT,
+    togglePermissionId INT          NOT NULL,
+    PRIMARY KEY (id)
 );
-ALTER TABLE "maintenanceEvents"
-    ADD CONSTRAINT "maintenanceEventsUn1" UNIQUE ("xid");
--- [jooq ignore start]
-ALTER TABLE "maintenanceEvents"
-    ADD CONSTRAINT "maintenanceEventsFk1" FOREIGN KEY ("togglePermissionId") REFERENCES "permissions" ("id") ON DELETE RESTRICT;
--- [jooq ignore stop]
+ALTER TABLE maintenanceEvents
+    ADD CONSTRAINT maintenanceEventsUn1 UNIQUE (xid);
+ALTER TABLE maintenanceEvents
+    ADD CONSTRAINT maintenanceEventsFk1 FOREIGN KEY (togglePermissionId) REFERENCES permissions (id) ON DELETE RESTRICT;
 
-CREATE TABLE "maintenanceEventDataPoints"
+CREATE TABLE maintenanceEventDataPoints
 (
-    "maintenanceEventId" INT NOT NULL,
-    "dataPointId"        INT NOT NULL
+    maintenanceEventId INT NOT NULL,
+    dataPointId        INT NOT NULL
 );
-ALTER TABLE "maintenanceEventDataPoints"
-    ADD CONSTRAINT "maintenanceEventDataPointsFk1" FOREIGN KEY ("maintenanceEventId") REFERENCES "maintenanceEvents" ("id") ON DELETE CASCADE;
--- [jooq ignore start]
-ALTER TABLE "maintenanceEventDataPoints"
-    ADD CONSTRAINT "maintenanceEventDataPointsFk2" FOREIGN KEY ("dataPointId") REFERENCES "dataPoints" ("id") ON DELETE CASCADE;
--- [jooq ignore stop]
+ALTER TABLE maintenanceEventDataPoints
+    ADD CONSTRAINT maintenanceEventDataPointsFk1 FOREIGN KEY (maintenanceEventId) REFERENCES maintenanceEvents (id) ON DELETE CASCADE;
+ALTER TABLE maintenanceEventDataPoints
+    ADD CONSTRAINT maintenanceEventDataPointsFk2 FOREIGN KEY (dataPointId) REFERENCES dataPoints (id) ON DELETE CASCADE;
 
-CREATE TABLE "maintenanceEventDataSources"
+CREATE TABLE maintenanceEventDataSources
 (
-    "maintenanceEventId" INT NOT NULL,
-    "dataSourceId"       INT NOT NULL
+    maintenanceEventId INT NOT NULL,
+    dataSourceId       INT NOT NULL
 );
-ALTER TABLE "maintenanceEventDataSources"
-    ADD CONSTRAINT "maintenanceEventDataSourcesFk1" FOREIGN KEY ("maintenanceEventId") REFERENCES "maintenanceEvents" ("id") ON DELETE CASCADE;
--- [jooq ignore start]
-ALTER TABLE "maintenanceEventDataSources"
-    ADD CONSTRAINT "maintenanceEventDataSourcesFk2" FOREIGN KEY ("dataSourceId") REFERENCES "dataSources" ("id") ON DELETE CASCADE;
--- [jooq ignore stop]
+ALTER TABLE maintenanceEventDataSources
+    ADD CONSTRAINT maintenanceEventDataSourcesFk1 FOREIGN KEY (maintenanceEventId) REFERENCES maintenanceEvents (id) ON DELETE CASCADE;
+ALTER TABLE maintenanceEventDataSources
+    ADD CONSTRAINT maintenanceEventDataSourcesFk2 FOREIGN KEY (dataSourceId) REFERENCES dataSources (id) ON DELETE CASCADE;
 
--- Watchlist/resources/com/serotonin/m2m2/watchlist/createTables-H2.sql
+++ Watchlist/resources/com/serotonin/m2m2/watchlist/createTables-H2.sql
@@ -2,48 +2,42 @@
 --    Copyright (C) 2014 Infinite Automation Systems Inc. All rights reserved.
 --    @author Matthew Lohbihler
 --
-CREATE TABLE "watchLists"
+CREATE TABLE watchLists
 (
-    "id"               INT          NOT NULL AUTO_INCREMENT,
-    "xid"              VARCHAR(100) NOT NULL,
-    "name"             VARCHAR(255),
-    "type"             VARCHAR(20),
-    "data"             LONGTEXT,
-    "readPermissionId" INT          NOT NULL,
-    "editPermissionId" INT          NOT NULL,
-    PRIMARY KEY ("id")
+    id               INT          NOT NULL AUTO_INCREMENT,
+    xid              VARCHAR(100) NOT NULL,
+    name             VARCHAR(255),
+    type             VARCHAR(20),
+    data             LONGTEXT,
+    readPermissionId INT          NOT NULL,
+    editPermissionId INT          NOT NULL,
+    PRIMARY KEY (id)
 );
-ALTER TABLE "watchLists"
-    ADD CONSTRAINT "watchListsUn1" UNIQUE ("xid");
--- [jooq ignore start]
-ALTER TABLE "watchLists"
-    ADD CONSTRAINT "watchListsFk2" FOREIGN KEY ("readPermissionId") REFERENCES "permissions" ("id") ON DELETE RESTRICT;
-ALTER TABLE "watchLists"
-    ADD CONSTRAINT "watchListsFk3" FOREIGN KEY ("editPermissionId") REFERENCES "permissions" ("id") ON DELETE RESTRICT;
--- [jooq ignore stop]
+ALTER TABLE watchLists
+    ADD CONSTRAINT watchListsUn1 UNIQUE (xid);
+ALTER TABLE watchLists
+    ADD CONSTRAINT watchListsFk2 FOREIGN KEY (readPermissionId) REFERENCES permissions (id) ON DELETE RESTRICT;
+ALTER TABLE watchLists
+    ADD CONSTRAINT watchListsFk3 FOREIGN KEY (editPermissionId) REFERENCES permissions (id) ON DELETE RESTRICT;
 
-CREATE TABLE "watchListPoints"
+CREATE TABLE watchListPoints
 (
-    "watchListId" INT NOT NULL,
-    "dataPointId" INT NOT NULL,
-    "sortOrder"   INT NOT NULL
+    watchListId INT NOT NULL,
+    dataPointId INT NOT NULL,
+    sortOrder   INT NOT NULL
 );
-ALTER TABLE "watchListPoints"
-    ADD CONSTRAINT "watchListPointsFk1" FOREIGN KEY ("watchListId") REFERENCES "watchLists" ("id") ON DELETE CASCADE;
--- [jooq ignore start]
-ALTER TABLE "watchListPoints"
-    ADD CONSTRAINT "watchListPointsFk2" FOREIGN KEY ("dataPointId") REFERENCES "dataPoints" ("id") ON DELETE CASCADE;
--- [jooq ignore stop]
+ALTER TABLE watchListPoints
+    ADD CONSTRAINT watchListPointsFk1 FOREIGN KEY (watchListId) REFERENCES watchLists (id) ON DELETE CASCADE;
+ALTER TABLE watchListPoints
+    ADD CONSTRAINT watchListPointsFk2 FOREIGN KEY (dataPointId) REFERENCES dataPoints (id) ON DELETE CASCADE;
 
-CREATE TABLE "selectedWatchList"
+CREATE TABLE selectedWatchList
 (
-    "userId"      INT NOT NULL,
-    "watchListId" INT NOT NULL,
-    PRIMARY KEY ("userId")
+    userId      INT NOT NULL,
+    watchListId INT NOT NULL,
+    PRIMARY KEY (userId)
 );
--- [jooq ignore start]
-ALTER TABLE "selectedWatchList"
-    ADD CONSTRAINT "selectedWatchListFk1" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE;
--- [jooq ignore stop]
-ALTER TABLE "selectedWatchList"
-    ADD CONSTRAINT "selectedWatchListFk2" FOREIGN KEY ("watchListId") REFERENCES "watchLists" ("id") ON DELETE CASCADE;
+ALTER TABLE selectedWatchList
+    ADD CONSTRAINT selectedWatchListFk1 FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE;
+ALTER TABLE selectedWatchList
+    ADD CONSTRAINT selectedWatchListFk2 FOREIGN KEY (watchListId) REFERENCES watchLists (id) ON DELETE CASCADE;
