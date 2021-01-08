4fa32a928082ca46dcd2eb5b7ca18ff40081cadf
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-01-08 12:33:03 -0700

    Allow anonymous access to logout rest controller

--- Mango API/src/com/infiniteautomation/mango/rest/latest/LogoutRestController.java
+++ Mango API/src/com/infiniteautomation/mango/rest/latest/LogoutRestController.java
@@ -1,17 +1,15 @@
-/**
- * Copyright (C) 2014 Infinite Automation Software. All rights reserved.
- * @author Terry Packer
+/*
+ * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
  */
 package com.infiniteautomation.mango.rest.latest;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.serotonin.m2m2.web.mvc.spring.security.permissions.AnonymousAccess;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -20,22 +18,18 @@ import io.swagger.annotations.ApiOperation;
  * @author Terry Packer
  *
  */
-@Api(value="Logout", description="Logout")
+@Api(value = "Logout")
 @RestController
 @RequestMapping("/logout")
 public class LogoutRestController {
 
-	/**
-	 * POST Logout action
-	 * @param request
-	 * @param response
-	 * @return
-	 */
-	@ApiOperation(value = "Logout", notes = "Perform logout using POST")
-	@RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> logoutPost(
-    		HttpServletRequest request, HttpServletResponse response) {
-
-	    return new ResponseEntity<>(HttpStatus.OK);
-	}
+    /**
+     * POST Logout action
+     */
+    @ApiOperation(value = "Logout", notes = "Perform logout using POST")
+    @RequestMapping(method = RequestMethod.POST)
+    @AnonymousAccess
+    public ResponseEntity<Void> logoutPost() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
