893f1f709b97a0c6d3cc7fdb0311f716d7d66e21
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-02-23 14:32:15 -0700

    Add module that loads standalone Nashorn jar

---
+++ nashorn/src/com/infiniteautomation/mango/nashorn/StandaloneNashornScriptEngineDefinition.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
+ */
+
+package com.infiniteautomation.mango.nashorn;
+
+import java.util.function.Function;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+
+import org.openjdk.nashorn.api.scripting.JSObject;
+import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
+import com.infiniteautomation.mango.spring.script.engines.NashornScriptEngineDefinition;
+import com.serotonin.m2m2.module.ConditionalDefinition;
+
+/**
+ * @author Jared Wiltshire
+ */
+@ConditionalDefinition(requireClasses = {"org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory"})
+public class StandaloneNashornScriptEngineDefinition extends NashornScriptEngineDefinition {
+
+    @Override
+    public boolean supports(ScriptEngineFactory engineFactory) {
+        return "org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory".equals(engineFactory.getClass().getName());
+    }
+
+    @Override
+    protected Object callLoad(Object load, Object source) {
+        JSObject loadFn = (JSObject) load;
+        return loadFn.call(null, source);
+    }
+
+    @Override
+    protected ScriptEngine createScriptEngine(ScriptEngineFactory engineFactory, Function<String, Boolean> filter) {
+        if (filter == null) {
+            return engineFactory.getScriptEngine();
+        }
+        return ((NashornScriptEngineFactory) engineFactory).getScriptEngine(filter::apply);
+    }
+
+}
