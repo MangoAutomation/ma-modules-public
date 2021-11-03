becbfb490be3cb5b391b7149876da8f9544acb7e
Terry Packer <terrypacker@users.noreply.github.com>
2021-11-03 07:53:45 -0500

    Move publishers without points to new endpoints
    As per RAD-1301

--- Mango API/src/com/infiniteautomation/mango/rest/latest/PublishersWithoutPointsRestController.java
+++ Mango API/src/com/infiniteautomation/mango/rest/latest/PublishersWithoutPointsRestController.java
@@ -7,17 +7,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 import javax.servlet.http.HttpServletRequest;
 import net.jazdw.rql.parser.ASTNode;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -35,7 +32,6 @@ import com.infiniteautomation.mango.rest.latest.model.RestModelMapper;
 import com.infiniteautomation.mango.rest.latest.model.StreamedArrayWithTotal;
 import com.infiniteautomation.mango.rest.latest.model.StreamedSeroJsonVORqlQuery;
 import com.infiniteautomation.mango.rest.latest.model.StreamedVORqlQueryWithTotal;
-import com.infiniteautomation.mango.rest.latest.model.publisher.AbstractPublishedPointModel;
 import com.infiniteautomation.mango.rest.latest.model.publisher.AbstractPublisherModel;
 import com.infiniteautomation.mango.rest.latest.patch.PatchVORequestBody;
 import com.infiniteautomation.mango.spring.service.PermissionService;
@@ -44,56 +40,51 @@ import com.infiniteautomation.mango.spring.service.PublisherService;
 import com.infiniteautomation.mango.util.RQLUtils;
 import com.serotonin.json.type.JsonStreamedArray;
 import com.serotonin.m2m2.vo.permission.PermissionHolder;
-import com.serotonin.m2m2.vo.publish.PublishedPointVO;
 import com.serotonin.m2m2.vo.publish.PublisherVO;
 import com.serotonin.m2m2.web.MediaTypes;
 
 /**
+ * This controller currently supports a publisher model without points in it. To access the
+ * points use the @see com.infiniteautomation.mango.rest.latest.PublishedPointsRestController
+ *
+ * TODO: This controller is temporary for the v3 REST api, when moving the v4 this should replace
+ *  the current /publishers endpoints
+ *
+ * NOTE: These endpoints are superadmin only as publishers have no permissions
  * @author Terry Packer
  *
  */
-@Api(value="Mango Publishers")
+@Api(value="Mango Publishers (points not included in model)")
 @RestController
-@RequestMapping("/publishers")
-public class PublishersRestController {
+@RequestMapping("/publishers-without-points")
+public class PublishersWithoutPointsRestController {
 
     private final PublisherService service;
     private final PublishedPointService publishedPointService;
     private final BiFunction<PublisherVO, PermissionHolder, AbstractPublisherModel<?,?>> map;
-    private final BiFunction<PublisherVO, PermissionHolder, AbstractPublisherModel<?,?>> mapWithoutPoints;
-    private final BiFunction<AbstractPublishedPointModel<?>, PermissionHolder, PublishedPointVO> unmapPoint;
     private final PermissionService permissionService;
 
     @Autowired
-    public PublishersRestController(final PublisherService service,
-                                    final PublishedPointService publishedPointService,
-                                    final RestModelMapper modelMapper, PermissionService permissionService) {
+    public PublishersWithoutPointsRestController(final PublisherService service,
+                                                 final RestModelMapper modelMapper,
+                                                 PermissionService permissionService,
+                                                 PublishedPointService publishedPointService
+                                    ) {
         this.service = service;
-        this.publishedPointService = publishedPointService;
         this.map = (vo, user) -> {
-            AbstractPublisherModel model = modelMapper.map(vo, AbstractPublisherModel.class, user);
-            List<AbstractPublishedPointModel> points = new ArrayList<>();
-            model.setPoints(points);
-            for(PublishedPointVO point : publishedPointService.getPublishedPoints(vo.getId())) {
-                points.add(modelMapper.map(point, AbstractPublishedPointModel.class, user));
-            }
-            return model;
-        };
-        this.mapWithoutPoints = (vo, user) -> {
             return modelMapper.map(vo, AbstractPublisherModel.class, user);
         };
-        this.unmapPoint = (model, user) -> {
-            return modelMapper.unMap(model, PublishedPointVO.class, user);
-        };
+
         this.permissionService = permissionService;
+        this.publishedPointService = publishedPointService;
     }
 
 
     @ApiOperation(
-            value = "Query Publishers Sources",
+            value = "Query Publishers",
             notes = "RQL Formatted Query",
             responseContainer="List"
-    )
+            )
     @RequestMapping(method = RequestMethod.GET)
     public StreamedArrayWithTotal query(
             HttpServletRequest request,
@@ -101,13 +92,13 @@ public class PublishersRestController {
             @AuthenticationPrincipal PermissionHolder user,
             UriComponentsBuilder builder) {
         ASTNode rql = RQLUtils.parseRQLtoAST(request.getQueryString());
-        return doQuery(rql, user);
+        return doQuery(rql, user, this.map);
     }
 
     @ApiOperation(
             value = "Get publisher by XID",
             notes = ""
-    )
+            )
     @RequestMapping(method = RequestMethod.GET, value="/{xid}")
     public AbstractPublisherModel<?,?> get(
             @ApiParam(value = "XID of publisher", required = true, allowMultiple = false)
@@ -120,7 +111,7 @@ public class PublishersRestController {
     @ApiOperation(
             value = "Get publisher by ID",
             notes = ""
-    )
+            )
     @RequestMapping(method = RequestMethod.GET, value="/by-id/{id}")
     public AbstractPublisherModel<?,?> getById(
             @ApiParam(value = "ID of publisher", required = true, allowMultiple = false)
@@ -130,7 +121,7 @@ public class PublishersRestController {
         return map.apply(service.get(id), user);
     }
 
-    @ApiOperation(value = "Save publisher")
+    @ApiOperation(value = "Save publisher, if points are supplied in model they will replace all existing points")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<AbstractPublisherModel<?,?>> save(
             @RequestBody(required=true) AbstractPublisherModel<?, ?> model,
@@ -139,14 +130,13 @@ public class PublishersRestController {
             HttpServletRequest request) {
 
         PublisherVO vo = this.service.insert(model.toVO());
-        maybeReplacePoints(vo, model.getPoints(), user);
         URI location = builder.path("/publishers/{xid}").buildAndExpand(new Object[]{vo.getXid()}).toUri();
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(location);
         return new ResponseEntity<>(map.apply(vo, user), headers, HttpStatus.CREATED);
     }
 
-    @ApiOperation(value = "Update publisher")
+    @ApiOperation(value = "Update publisher, if points are supplied in model they will replace all existing points")
     @RequestMapping(method = RequestMethod.PUT, value = "/{xid}")
     public ResponseEntity<AbstractPublisherModel<?,?>> update(
             @PathVariable String xid,
@@ -156,7 +146,6 @@ public class PublishersRestController {
             HttpServletRequest request) {
 
         PublisherVO vo = this.service.update(xid, model.toVO());
-        maybeReplacePoints(vo, model.getPoints(), user);
         URI location = builder.path("/publishers/{xid}").buildAndExpand(new Object[]{vo.getXid()}).toUri();
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(location);
@@ -164,42 +153,39 @@ public class PublishersRestController {
     }
 
     @ApiOperation(
-            value = "Partially update a publisher",
+            value = "Partially update a publisher, if points are supplied in model they will replace all existing points",
             notes = "Requires edit permission"
-    )
+            )
     @RequestMapping(method = RequestMethod.PATCH, value = "/{xid}")
     public ResponseEntity<AbstractPublisherModel<?,?>> partialUpdate(
             @PathVariable String xid,
-
             @ApiParam(value = "Updated data source", required = true)
             @PatchVORequestBody(
                     service=PublisherService.class,
                     modelClass=AbstractPublisherModel.class)
-                    AbstractPublisherModel<?, ?> model,
+            AbstractPublisherModel<?, ?> model,
             @AuthenticationPrincipal PermissionHolder user,
             UriComponentsBuilder builder) {
 
         PublisherVO vo = service.update(xid, model.toVO());
-        maybeReplacePoints(vo, model.getPoints(), user);
         URI location = builder.path("/publishers/{xid}").buildAndExpand(vo.getXid()).toUri();
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(location);
-
         return new ResponseEntity<>(map.apply(vo, user), headers, HttpStatus.OK);
     }
 
     @ApiOperation(
             value = "Delete a publisher",
-            notes = "",
+            notes = "Points are never returned as they don't exist after deleting a publisher",
             response=AbstractPublisherModel.class
-    )
+            )
     @RequestMapping(method = RequestMethod.DELETE, value="/{xid}")
     public AbstractPublisherModel<?,?> delete(
             @ApiParam(value = "XID of publisher to delete", required = true, allowMultiple = false)
             @PathVariable String xid,
             @AuthenticationPrincipal PermissionHolder user,
             UriComponentsBuilder builder) {
-        return mapWithoutPoints.apply(service.delete(xid), user);
+            return map.apply(service.delete(xid), user);
     }
 
     @ApiOperation(value = "Enable/disable/restart a publisher")
@@ -255,37 +241,11 @@ public class PublishersRestController {
      * @param user
      * @return
      */
-    private StreamedArrayWithTotal doQuery(ASTNode rql, PermissionHolder user) {
+    private StreamedArrayWithTotal doQuery(ASTNode rql, PermissionHolder user, BiFunction<PublisherVO, PermissionHolder, AbstractPublisherModel<?,?>> map) {
         if (permissionService.hasAdminRole(user)) {
             return new StreamedVORqlQueryWithTotal<>(service, rql, null, null, null, vo -> map.apply(vo, user));
         } else {
             return new StreamedVORqlQueryWithTotal<>(service, rql, null, null, null, vo -> service.hasReadPermission(user, vo), vo -> map.apply(vo, user));
         }
     }
-
-    /**
-     * If points are not null they will replace any points on this publisher
-     * @param publisherVO
-     * @param points
-     */
-    private void maybeReplacePoints(PublisherVO publisherVO, List<? extends AbstractPublishedPointModel<?>> points, PermissionHolder user) {
-        if(points != null) {
-            List<PublishedPointVO> pointVos = new ArrayList<>();
-            int i =0;
-            for(AbstractPublishedPointModel<?> pm : points) {
-                //Ensure publisher xid is set
-                pm.setPublisherXid(publisherVO.getXid());
-                //Ensure xid is set
-                if(StringUtils.isEmpty(pm.getXid())) {
-                    pm.setXid(publishedPointService.generateUniqueXid());
-                }
-                pm.setName(publisherVO.getName() + " point " + i);
-                pm.setEnabled(true);
-                pointVos.add(unmapPoint.apply(pm, user));
-                i++;
-            }
-            publishedPointService.replacePoints(publisherVO.getId(), pointVos);
-        }
-    }
-
 }
