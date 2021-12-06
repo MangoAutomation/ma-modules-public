36236bb755d14f039dee57c9b1641f2bed0f0e1e
MertCingoz <mertcingoz@gmail.com>
2021-12-06 21:17:56 +0300

    RAD-1508 use Data and Time units

--- Mango API/src/com/infiniteautomation/mango/rest/latest/websocket/MangoWebSocketHandshakeHandler.java
+++ Mango API/src/com/infiniteautomation/mango/rest/latest/websocket/MangoWebSocketHandshakeHandler.java
@@ -3,17 +3,20 @@
  */
 package com.infiniteautomation.mango.rest.latest.websocket;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.servlet.ServletContext;
 
 import org.eclipse.jetty.websocket.api.WebSocketBehavior;
 import org.eclipse.jetty.websocket.api.WebSocketPolicy;
 import org.eclipse.jetty.websocket.server.WebSocketServerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
 import org.springframework.web.socket.server.jetty.JettyRequestUpgradeStrategy;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-
-import com.serotonin.m2m2.Common;
 
 /**
  *
@@ -21,28 +24,27 @@ import com.serotonin.m2m2.Common;
  */
 @Component("mangoWebSocketHandshakeHandlerV2")
 public class MangoWebSocketHandshakeHandler extends DefaultHandshakeHandler {
-    private static final int KB = 1024;
-    private static final long SECOND = 1000L;
 
-    public MangoWebSocketHandshakeHandler(@Autowired ServletContext servletContext) {
-        super(new JettyRequestUpgradeStrategy(new WebSocketServerFactory(servletContext, getPolicy())));
+    @Autowired
+    public MangoWebSocketHandshakeHandler(ServletContext servletContext, Environment environment) {
+        super(new JettyRequestUpgradeStrategy(new WebSocketServerFactory(servletContext, getPolicy(environment))));
     }
 
-    static WebSocketPolicy getPolicy() {
-        int inputBufferSize = Common.envProps.getInt("web.websocket.inputBufferSize", 8 * KB);
-        int maxTextMessageSize = Common.envProps.getInt("web.websocket.maxTextMessageSize", 64 * KB);
-        int maxTextMessageBufferSize = Common.envProps.getInt("web.websocket.maxTextMessageBufferSize", 32 * KB);
-        int maxBinaryMessageSize = Common.envProps.getInt("web.websocket.maxBinaryMessageSize", 64 * KB);
-        int maxBinaryMessageBufferSize = Common.envProps.getInt("web.websocket.maxBinaryMessageBufferSize", 32 * KB);
-        long asyncWriteTimeout = Common.envProps.getLong("web.websocket.asyncWriteTimeoutMs", 60 * SECOND);
-        long idleTimeout = Common.envProps.getLong("web.websocket.idleTimeoutMs", 60 * SECOND);
+    static WebSocketPolicy getPolicy(Environment environment) {
+        long inputBufferSize = environment.getProperty("web.websocket.inputBufferSize", long.class, DataSize.of(8, DataUnit.KILOBYTES).toBytes());
+        long maxTextMessageSize = environment.getProperty("web.websocket.maxTextMessageSize", long.class, DataSize.of(64, DataUnit.KILOBYTES).toBytes());
+        long maxTextMessageBufferSize = environment.getProperty("web.websocket.maxTextMessageBufferSize", long.class, DataSize.of(32, DataUnit.KILOBYTES).toBytes());
+        long maxBinaryMessageSize = environment.getProperty("web.websocket.maxBinaryMessageSize", long.class, DataSize.of(64, DataUnit.KILOBYTES).toBytes());
+        long maxBinaryMessageBufferSize = environment.getProperty("web.websocket.maxBinaryMessageBufferSize", long.class, DataSize.of(32, DataUnit.KILOBYTES).toBytes());
+        long asyncWriteTimeout = environment.getProperty("web.websocket.asyncWriteTimeoutMs", long.class, TimeUnit.SECONDS.toMillis(60));
+        long idleTimeout = environment.getProperty("web.websocket.idleTimeoutMs", long.class, TimeUnit.SECONDS.toMillis(60));
 
         WebSocketPolicy policy = new WebSocketPolicy(WebSocketBehavior.SERVER);
-        policy.setInputBufferSize(inputBufferSize);
-        policy.setMaxTextMessageSize(maxTextMessageSize);
-        policy.setMaxTextMessageBufferSize(maxTextMessageBufferSize);
-        policy.setMaxBinaryMessageSize(maxBinaryMessageSize);
-        policy.setMaxBinaryMessageBufferSize(maxBinaryMessageBufferSize);
+        policy.setInputBufferSize((int) inputBufferSize);
+        policy.setMaxTextMessageSize((int) maxTextMessageSize);
+        policy.setMaxTextMessageBufferSize((int) maxTextMessageBufferSize);
+        policy.setMaxBinaryMessageSize((int) maxBinaryMessageSize);
+        policy.setMaxBinaryMessageBufferSize((int) maxBinaryMessageBufferSize);
         policy.setAsyncWriteTimeout(asyncWriteTimeout);
 
         // ping pong mechanism will keep socket alive, web.websocket.pingTimeoutMs should be set lower than the idle timeout
