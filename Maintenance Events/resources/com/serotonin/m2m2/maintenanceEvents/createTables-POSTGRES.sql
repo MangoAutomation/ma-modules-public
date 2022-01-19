a37e0f94c7754518b0832bf6babed5626725959a
MertCingoz <mertcingoz@gmail.com>
2022-01-19 17:07:40 +0300

    RAD-1671 Add PostgreSQL support

--- Maintenance Events/resources/com/serotonin/m2m2/maintenanceEvents/createTables-POSTGRES.sql
+++ Maintenance Events/resources/com/serotonin/m2m2/maintenanceEvents/createTables-POSTGRES.sql
@@ -2,46 +2,48 @@
 --    Copyright (C) 2021 Radix IoT LLC. All rights reserved.
 --    @author Matthew Lohbihler
 --
-create table maintenanceEvents (
-  id int not null auto_increment,
-  xid varchar(100) not null,
-  dataSourceId int,
-  alias varchar(255),
-  alarmLevel int not null,
-  scheduleType int not null,
-  disabled char(1) not null,
-  activeYear int,
-  activeMonth int,
-  activeDay int,
-  activeHour int,
-  activeMinute int,
-  activeSecond int,
-  activeCron varchar(25),
-  inactiveYear int,
-  inactiveMonth int,
-  inactiveDay int,
-  inactiveHour int,
-  inactiveMinute int,
-  inactiveSecond int,
-  inactiveCron varchar(25),
-  timeoutPeriods int,
-  timeoutPeriodType int,
-  togglePermissionId INT NOT NULL,
-  primary key (id)
+
+
+
+CREATE TABLE maintenanceEvents (
+    id serial,
+    xid varchar(100) NOT NULL,
+    alias varchar(255),
+    alarmLevel int NOT NULL,
+    scheduleType int NOT NULL,
+    disabled char(1) NOT NULL,
+    activeYear int,
+    activeMonth int,
+    activeDay int,
+    activeHour int,
+    activeMinute int,
+    activeSecond int,
+    activeCron varchar(25),
+    inactiveYear int,
+    inactiveMonth int,
+    inactiveDay int,
+    inactiveHour int,
+    inactiveMinute int,
+    inactiveSecond int,
+    inactiveCron varchar(25),
+    timeoutPeriods int,
+    timeoutPeriodType int,
+    togglePermissionId int NOT NULL,
+    PRIMARY KEY (id)
 );
-alter table maintenanceEvents add constraint maintenanceEventsUn1 unique (xid);
+ALTER TABLE maintenanceEvents ADD CONSTRAINT maintenanceEventsUn1 UNIQUE (xid);
 ALTER TABLE maintenanceEvents ADD CONSTRAINT maintenanceEventsFk1 FOREIGN KEY (togglePermissionId) REFERENCES permissions(id) ON DELETE RESTRICT;
 
 CREATE TABLE maintenanceEventDataPoints (
-  maintenanceEventId int NOT NULL,
-  dataPointId int NOT NULL
-) ;
-ALTER TABLE maintenanceEventDataPoints add constraint maintenanceEventDataPointsFk1 foreign key (maintenanceEventId) references maintenanceEvents(id) on delete cascade;
-ALTER TABLE maintenanceEventDataPoints add constraint maintenanceEventDataPointsFk2 foreign key (dataPointId) references dataPoints(id) on delete cascade;
+    maintenanceEventId int NOT NULL,
+    dataPointId int NOT NULL
+);
+ALTER TABLE maintenanceEventDataPoints ADD CONSTRAINT maintenanceEventDataPointsFk1 FOREIGN KEY (maintenanceEventId) REFERENCES maintenanceEvents(id) ON DELETE CASCADE;
+ALTER TABLE maintenanceEventDataPoints ADD CONSTRAINT maintenanceEventDataPointsFk2 FOREIGN KEY (dataPointId) REFERENCES dataPoints(id) ON DELETE CASCADE;
 
 CREATE TABLE maintenanceEventDataSources (
-  maintenanceEventId int NOT NULL,
-  dataSourceId int NOT NULL
-) ;
-ALTER TABLE maintenanceEventDataSources add constraint maintenanceEventDataSourcesFk1 foreign key (maintenanceEventId) references maintenanceEvents(id) on delete cascade;
-ALTER TABLE maintenanceEventDataSources add constraint maintenanceEventDataSourcesFk2 foreign key (dataSourceId) references dataSources(id) on delete cascade;
+    maintenanceEventId int NOT NULL,
+    dataSourceId int NOT NULL
+);
+ALTER TABLE maintenanceEventDataSources ADD CONSTRAINT maintenanceEventDataSourcesFk1 FOREIGN KEY (maintenanceEventId) REFERENCES maintenanceEvents(id) ON DELETE CASCADE;
+ALTER TABLE maintenanceEventDataSources ADD CONSTRAINT maintenanceEventDataSourcesFk2 FOREIGN KEY (dataSourceId) REFERENCES dataSources(id) ON DELETE CASCADE;
