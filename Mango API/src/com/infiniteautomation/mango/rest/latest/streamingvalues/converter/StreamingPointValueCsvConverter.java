7475c33019a34bca1ae9620f8571058f272b9261
Jared Wiltshire <jazdw@users.noreply.github.com>
2022-03-30 12:07:04 -0600

    Remove duplicate column and reorder

--- Mango API/src/com/infiniteautomation/mango/rest/latest/streamingvalues/converter/StreamingPointValueCsvConverter.java
+++ Mango API/src/com/infiniteautomation/mango/rest/latest/streamingvalues/converter/StreamingPointValueCsvConverter.java
@@ -47,15 +47,6 @@ public class StreamingPointValueCsvConverter extends StreamCsvConverter<Streamin
         if (fields.contains(PointValueField.VALUE)) {
             schemaBuilder.addColumn(PointValueField.VALUE.getFieldName(), ColumnType.NUMBER_OR_STRING);
         }
-        if (fields.contains(PointValueField.CACHED)) {
-            schemaBuilder.addColumn(PointValueField.CACHED.getFieldName(), ColumnType.BOOLEAN);
-        }
-        if (fields.contains(PointValueField.BOOKEND)) {
-            schemaBuilder.addColumn(PointValueField.BOOKEND.getFieldName(), ColumnType.BOOLEAN);
-        }
-        if (fields.contains(PointValueField.ANNOTATION)) {
-            schemaBuilder.addColumn(PointValueField.ANNOTATION.getFieldName(), ColumnType.STRING);
-        }
         if (fields.contains(PointValueField.RAW)) {
             schemaBuilder.addColumn(PointValueField.RAW.getFieldName(), ColumnType.NUMBER_OR_STRING);
         }
@@ -64,6 +55,12 @@ public class StreamingPointValueCsvConverter extends StreamCsvConverter<Streamin
         }
         if (fields.contains(PointValueField.ANNOTATION)) {
             schemaBuilder.addColumn(PointValueField.ANNOTATION.getFieldName(), ColumnType.STRING);
+        }
+        if (fields.contains(PointValueField.CACHED)) {
+            schemaBuilder.addColumn(PointValueField.CACHED.getFieldName(), ColumnType.BOOLEAN);
+        }
+        if (fields.contains(PointValueField.BOOKEND)) {
+            schemaBuilder.addColumn(PointValueField.BOOKEND.getFieldName(), ColumnType.BOOLEAN);
         }
         if (fields.contains(PointValueField.XID)) {
             schemaBuilder.addColumn(PointValueField.XID.getFieldName(), ColumnType.STRING);
