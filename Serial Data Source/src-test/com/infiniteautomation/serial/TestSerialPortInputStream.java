51404f381bf52743c98e7bf2957f33d340e8eddb
terrypacker <terrypacker@users.noreply.github.com>
2022-04-12 11:44:57 -1000

    Change JSSC Serial Port Input Stream to directly read from port
    Fix for RAD-1890

--- Serial Data Source/src-test/com/infiniteautomation/serial/TestSerialPortInputStream.java
+++ Serial Data Source/src-test/com/infiniteautomation/serial/TestSerialPortInputStream.java
@@ -14,10 +14,7 @@ import com.serotonin.util.queue.ByteQueue;
  */
 public class TestSerialPortInputStream extends SerialPortInputStream{
 	ByteQueue mockStream;
-	
-	/* (non-Javadoc)
-	 * @see com.serotonin.io.serial.SerialPortInputStream#read()
-	 */
+
 	@Override
 	public int read() throws IOException {
 		if(mockStream.size() <= 0)
@@ -25,31 +22,16 @@ public class TestSerialPortInputStream extends SerialPortInputStream{
 		return mockStream.pop();
 	}
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.io.serial.SerialPortInputStream#available()
-	 */
 	@Override
 	public int available() throws IOException {
 		return mockStream.size();
 	}
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.io.serial.SerialPortInputStream#closeImpl()
-	 */
 	@Override
 	public void closeImpl() throws IOException {
 		mockStream.popAll();
 	}
 
-	/* (non-Javadoc)
-	 * @see com.serotonin.io.serial.SerialPortInputStream#peek()
-	 */
-	@Override
-	public int peek() {
-		if(mockStream.size() <= 0)
-			return -1;
-		return mockStream.peek(0);
-	}
 	
 	public void pushToMockStream(String str) {
 		mockStream = new ByteQueue(1024);
